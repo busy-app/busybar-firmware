0d016128e4182153e3fb50f29e1aeac9f4d341d1
Georgii Surkov <georgii.surkov@outlook.com>
2025-01-29 14:07:09 +0000

    Improve furi_hal serial control, get rid of furi_hal_todo

--- targets/f20/furi_hal/furi_hal_serial_control.h
+++ targets/f20/furi_hal/furi_hal_serial_control.h
@@ -9,16 +9,10 @@
 extern "C" {
 #endif
 
-/** Initialize Serial Control
- *
- * @note    this method should be called in the early initialization stage
- */
+/** Initialize Serial Control */
 void furi_hal_serial_control_init(void);
 
-/** Deinitialize Serial Control
- *
- * @note    this method should be called in the early de-initialization stage
- */
+/** De-Initialize Serial Control */
 void furi_hal_serial_control_deinit(void);
 
 /** Suspend All Serial Interfaces
@@ -56,6 +50,15 @@ void furi_hal_serial_control_release(FuriHalSerialHandle* handle);
  * @return     true if handle is acquired by someone
  */
 bool furi_hal_serial_control_is_busy(FuriHalSerialId serial_id);
+
+/** Acquire Serial Interface Handler
+ *
+ * @param[in]  serial_id  The serial transceiver identifier. Use FuriHalSerialIdMax to disable logging.
+ * @param[in]  baud_rate  The baud rate
+ *
+ * @return     The Serial Interface Handle or null if interfaces is in use
+ */
+void furi_hal_serial_control_set_logging_config(FuriHalSerialId serial_id, uint32_t baud_rate);
 
 #ifdef __cplusplus
 }
