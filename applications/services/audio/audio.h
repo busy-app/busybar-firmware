e4d0b34bf0144376d2baa1e1f3534ee3a61cde90
Georgii Surkov <georgii.surkov@outlook.com>
2025-02-26 21:05:46 +0000

    Refactor audio service to support storage files

--- applications/services/audio/audio.h
+++ applications/services/audio/audio.h
@@ -3,19 +3,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define RECORD_AUDIO "audio"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef enum {
-    AudioCommandEnable,
-    AudioCommandDisable,
-    AudioCommandPlayClick1,
-    AudioCommandPlayClick2,
-    AudioCommandPlayBusyEnd,
-} AudioCommand;
+typedef struct Audio Audio;
 
-void audio_play(AudioCommand sound);
+bool audio_play_file(Audio* instance, const char* file_name);
 
 #ifdef __cplusplus
 }
