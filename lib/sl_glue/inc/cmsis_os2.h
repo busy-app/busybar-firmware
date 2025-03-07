161bbad45bab0319fe1468dfd764c8bc8d7f6ba2
SkorP <skorpionm@yandex.ru>
2025-03-07 19:40:32 +0400

    fuck 2

--- lib/sl_glue/inc/cmsis_os2.h
+++ lib/sl_glue/inc/cmsis_os2.h
@@ -66,6 +66,7 @@
 
 //#include <furi.h>
 #include <stdlib.h>
+#include <furi/core/base.h>
 #include <furi/core/common_defines.h>
 //#include <furi/core/check.h> //ToDo: Fix m-lib
 #include <furi/core/thread.h>
@@ -75,6 +76,8 @@
 #include <furi/core/event_flag.h>
 #include <furi/core/kernel.h>
 #include <furi/core/message_queue.h>
+#include <furi/core/timer.h>
+#include <furi/core/event_flag.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -174,13 +177,13 @@ typedef enum {
 typedef void (*osThreadFunc_t)(void* argument);
 
 // /// Timer callback function.
-// typedef void (*osTimerFunc_t) (void *argument);
+typedef void (*osTimerFunc_t)(void* argument);
 
-// /// Timer type.
-// typedef enum {
-//   osTimerOnce               = 0,          ///< One-shot timer.
-//   osTimerPeriodic           = 1           ///< Repeating timer.
-// } osTimerType_t;
+/// Timer type.
+typedef enum {
+    osTimerOnce = 0, ///< One-shot timer.
+    osTimerPeriodic = 1 ///< Repeating timer.
+} osTimerType_t;
 
 // Timeout value.
 //#define osWaitForever         0xFFFFFFFFU ///< Wait forever timeout value.
@@ -237,7 +240,7 @@ typedef enum {
 typedef void* osThreadId_t;
 
 // /// \details Timer ID identifies the timer.
-// typedef void* osTimerId_t;
+typedef void* osTimerId_t;
 
 /// \details Event Flags ID identifies the event flags.
 typedef void* osEventFlagsId_t;
@@ -285,13 +288,13 @@ static FURI_ALWAYS_INLINE int32_t furi_cmsis_thread_callback(void* context) {
     return 0;
 }
 
-// /// Attributes structure for timer.
-// typedef struct {
-//   const char                   *name;   ///< name of the timer
-//   uint32_t                 attr_bits;   ///< attribute bits
-//   void                      *cb_mem;    ///< memory for control block
-//   uint32_t                   cb_size;   ///< size of provided memory for control block
-// } osTimerAttr_t;
+/// Attributes structure for timer.
+typedef struct {
+    const char* name; ///< name of the timer
+    uint32_t attr_bits; ///< attribute bits
+    void* cb_mem; ///< memory for control block
+    uint32_t cb_size; ///< size of provided memory for control block
+} osTimerAttr_t;
 
 /// Attributes structure for event flags.
 typedef struct {
@@ -380,16 +383,25 @@ static FURI_ALWAYS_INLINE osKernelState_t osKernelGetState(void) {
 
 // /// Lock the RTOS Kernel scheduler.
 // /// \return previous lock state (1 - locked, 0 - not locked, error code if negative).
-// int32_t osKernelLock (void);
+//int32_t osKernelLock (void);
+static FURI_ALWAYS_INLINE int32_t osKernelLock(void) {
+    return furi_kernel_lock();
+}
 
 // /// Unlock the RTOS Kernel scheduler.
 // /// \return previous lock state (1 - locked, 0 - not locked, error code if negative).
-// int32_t osKernelUnlock (void);
+//int32_t osKernelUnlock (void);
+static FURI_ALWAYS_INLINE int32_t osKernelUnlock(void) {
+    return furi_kernel_unlock();
+}
 
 // /// Restore the RTOS Kernel scheduler lock state.
 // /// \param[in]     lock          lock state obtained by \ref osKernelLock or \ref osKernelUnlock.
 // /// \return new lock state (1 - locked, 0 - not locked, error code if negative).
-// int32_t osKernelRestoreLock (int32_t lock);
+//int32_t osKernelRestoreLock (int32_t lock);
+static FURI_ALWAYS_INLINE int32_t osKernelRestoreLock(int32_t lock) {
+    return furi_kernel_restore_lock(lock);
+}
 
 // /// Suspend the RTOS Kernel scheduler.
 // /// \return time in ticks, for how long the system can sleep or power-down.
@@ -514,7 +526,14 @@ static FURI_ALWAYS_INLINE osStatus_t osThreadYield(void) {
 // /// Wait for specified thread to terminate.
 // /// \param[in]     thread_id     thread ID obtained by \ref osThreadNew or \ref osThreadGetId.
 // /// \return status code that indicates the execution status of the function.
-// osStatus_t osThreadJoin (osThreadId_t thread_id);
+//osStatus_t osThreadJoin (osThreadId_t thread_id);
+static FURI_ALWAYS_INLINE osStatus_t osThreadJoin(osThreadId_t thread_id) {
+    FuriThreadAdapter* adapter = (FuriThreadAdapter*)thread_id;
+    furi_thread_join(adapter->thread);
+    //furi_thread_free(adapter->thread);
+    //free(adapter);
+    return osOK;
+}
 
 // /// Terminate execution of current running thread.
 // __NO_RETURN void osThreadExit (void);
@@ -548,6 +567,10 @@ static FURI_ALWAYS_INLINE osStatus_t osThreadTerminate(osThreadId_t thread_id) {
 // /// \param[in]     flags         specifies the flags of the thread that shall be set.
 // /// \return thread flags after setting or error code if highest bit set.
 // uint32_t osThreadFlagsSet (osThreadId_t thread_id, uint32_t flags);
+static FURI_ALWAYS_INLINE uint32_t osThreadFlagsSet(osThreadId_t thread_id, uint32_t flags) {
+    FuriThreadAdapter* adapter = (FuriThreadAdapter*)thread_id;
+    return furi_thread_flags_set(adapter->thread, flags);
+}
 
 // /// Clear the specified Thread Flags of current running thread.
 // /// \param[in]     flags         specifies the flags of the thread that shall be cleared.
@@ -563,7 +586,11 @@ static FURI_ALWAYS_INLINE osStatus_t osThreadTerminate(osThreadId_t thread_id) {
 // /// \param[in]     options       specifies flags options (osFlagsXxxx).
 // /// \param[in]     timeout       \ref CMSIS_RTOS_TimeOutValue or 0 in case of no time-out.
 // /// \return thread flags before clearing or error code if highest bit set.
-// uint32_t osThreadFlagsWait (uint32_t flags, uint32_t options, uint32_t timeout);
+//uint32_t osThreadFlagsWait (uint32_t flags, uint32_t options, uint32_t timeout);
+static FURI_ALWAYS_INLINE uint32_t
+    osThreadFlagsWait(uint32_t flags, uint32_t options, uint32_t timeout) {
+    return furi_thread_flags_wait(flags, options, timeout);
+}
 
 // //  ==== Generic Wait Functions ====
 
@@ -593,6 +620,12 @@ static FURI_ALWAYS_INLINE osStatus_t osDelayUntil(uint32_t ticks) {
 // /// \param[in]     attr          timer attributes; NULL: default values.
 // /// \return timer ID for reference by other functions or NULL in case of error.
 // osTimerId_t osTimerNew (osTimerFunc_t func, osTimerType_t type, void *argument, const osTimerAttr_t *attr);
+static FURI_ALWAYS_INLINE osTimerId_t
+    osTimerNew(osTimerFunc_t func, osTimerType_t type, void* argument, const osTimerAttr_t* attr) {
+    UNUSED(attr);
+    //furi_check((attr == NULL), "osTimerNew: attr must be NULL");
+    return (osTimerId_t)furi_timer_alloc((FuriTimerCallback)func, (FuriTimerType)type, argument);
+}
 
 // /// Get name of a timer.
 // /// \param[in]     timer_id      timer ID obtained by \ref osTimerNew.
@@ -603,17 +636,28 @@ static FURI_ALWAYS_INLINE osStatus_t osDelayUntil(uint32_t ticks) {
 // /// \param[in]     timer_id      timer ID obtained by \ref osTimerNew.
 // /// \param[in]     ticks         \ref CMSIS_RTOS_TimeOutValue "time ticks" value of the timer.
 // /// \return status code that indicates the execution status of the function.
-// osStatus_t osTimerStart (osTimerId_t timer_id, uint32_t ticks);
+//osStatus_t osTimerStart (osTimerId_t timer_id, uint32_t ticks);
+static FURI_ALWAYS_INLINE osStatus_t osTimerStart(osTimerId_t timer_id, uint32_t ticks) {
+    FuriStatus status = furi_timer_start((FuriTimer*)timer_id, ticks);
+    return (osStatus_t)status;
+}
 
 // /// Stop a timer.
 // /// \param[in]     timer_id      timer ID obtained by \ref osTimerNew.
 // /// \return status code that indicates the execution status of the function.
-// osStatus_t osTimerStop (osTimerId_t timer_id);
+//osStatus_t osTimerStop (osTimerId_t timer_id);
+static FURI_ALWAYS_INLINE osStatus_t osTimerStop(osTimerId_t timer_id) {
+    FuriStatus status = furi_timer_stop((FuriTimer*)timer_id);
+    return (osStatus_t)status;
+}
 
 // /// Check if a timer is running.
 // /// \param[in]     timer_id      timer ID obtained by \ref osTimerNew.
 // /// \return 0 not running, 1 running.
-// uint32_t osTimerIsRunning (osTimerId_t timer_id);
+//uint32_t osTimerIsRunning (osTimerId_t timer_id);
+static FURI_ALWAYS_INLINE uint32_t osTimerIsRunning(osTimerId_t timer_id) {
+    return furi_timer_is_running((FuriTimer*)timer_id);
+}
 
 // /// Delete a timer.
 // /// \param[in]     timer_id      timer ID obtained by \ref osTimerNew.
