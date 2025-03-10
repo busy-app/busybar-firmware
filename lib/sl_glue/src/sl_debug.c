a3c1835cc780b48908a3b90888e85e34566bc112
hedger <hedger@nanode.su>
2025-03-10 18:19:15 +0000

    lib: matter&si: config and build fixes

--- lib/sl_glue/src/sl_debug.c
+++ lib/sl_glue/src/sl_debug.c
@@ -14,3 +14,25 @@ void sl_debug_log(const char* format, ...) {
 
     va_end(args);
 }
+
+int __wrap_fprintf(FILE* stream, const char* format, ...) {
+    UNUSED(stream);
+
+    va_list args;
+    va_start(args, format);
+
+    FuriString* message = furi_string_alloc_vprintf(format, args);
+    furi_string_trim(message);
+
+    FURI_LOG_D(TAG, "%s", furi_string_get_cstr(message));
+    furi_string_free(message);
+
+    va_end(args);
+    return 0;
+}
+
+int __wrap__fflush_r(struct _reent* reent, FILE* stream) {
+    UNUSED(reent);
+    UNUSED(stream);
+    return 0;
+}
