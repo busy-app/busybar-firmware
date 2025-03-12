ddc0c2e15b66d7f4a6c87e90706a10ed76808b3b
hedger <hedger@nanode.su>
2025-03-12 20:44:33 +0000

    code formatting

--- applications/services/desktop/desktop.c
+++ applications/services/desktop/desktop.c
@@ -12,7 +12,7 @@
 #define TAG "Desktop"
 
 // Time to wait for the rotary switch steady state
-#define SWITCH_DELAY_MS (300)
+#define SWITCH_DELAY_MS   (300)
 // Maximum and initial counts for synchronisation primitives
 #define INPUT_QUEUE_COUNT (8)
 #define START_QUEUE_COUNT (3)
