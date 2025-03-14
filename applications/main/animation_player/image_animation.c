7d27b6df3cba232784a78acd968e64a0de113697
Georgii Surkov <georgii.surkov@outlook.com>
2025-03-14 17:34:14 +0000

    Refactor and clean up ImageAnimation code

--- applications/main/animation_player/image_animation.c
+++ applications/main/animation_player/image_animation.c
@@ -10,6 +10,8 @@
 #define IMAGE_ANIMATION_FILE_MAGIC     (0x69)
 #define IMAGE_ANIMATION_FORMAT_VERSION (0x00)
 
+#define ANIM_IMAGE_MAX_FPS (100)
+
 #define MY_CLASS (&anim_image_lvgl_class)
 
 typedef struct {
@@ -115,6 +117,62 @@ static void image_animation_timer_callback(lv_timer_t* timer) {
     lv_obj_invalidate(instance->canvas);
 }
 
+static bool
+    image_animation_validate_header(const ImageAnimationFileHeader* header, size_t file_size) {
+    bool validated = false;
+
+    do {
+        if(header->magic != IMAGE_ANIMATION_FILE_MAGIC) {
+            FURI_LOG_D(TAG, "Invalid magic num: %ld", header->magic);
+            break;
+        }
+
+        if(header->format_version != IMAGE_ANIMATION_FORMAT_VERSION) {
+            FURI_LOG_D(TAG, "Invalid  format version: %ld", header->format_version);
+            break;
+        }
+
+        if(header->fps > ANIM_IMAGE_MAX_FPS) {
+            FURI_LOG_D(
+                TAG, "Unsupported fps: %ld. Must be less than %d", header->fps, ANIM_IMAGE_MAX_FPS);
+            break;
+        }
+
+        if(header->bytes_per_pixel != 3 && header->bytes_per_pixel != 1) {
+            FURI_LOG_E(
+                TAG,
+                "Unsupported bytes per pixel: %lu. Must be 3 (RGB888) or 1 (L8)",
+                header->bytes_per_pixel);
+            break;
+        }
+
+        const size_t frames_data_size =
+            header->frames * header->bytes_per_pixel * header->width * header->height;
+
+        if((frames_data_size + sizeof(ImageAnimationFileHeader)) != file_size) {
+            FURI_LOG_D(
+                TAG,
+                "Frames: %ld, BPP: %ld, Width: %ld, Height: %ld",
+                header->frames,
+                header->bytes_per_pixel,
+                header->width,
+                header->height);
+
+            FURI_LOG_D(
+                TAG,
+                "Incorrect file size: %zu. Expected size: %zu",
+                file_size,
+                frames_data_size + sizeof(ImageAnimationFileHeader));
+
+            break;
+        }
+
+        validated = true;
+    } while(false);
+
+    return validated;
+}
+
 // Public API
 
 ImageAnimation* image_animation_alloc(Widget* parent) {
@@ -135,81 +193,37 @@ void image_animation_free(ImageAnimation* instance) {
 bool image_animation_set_source(ImageAnimation* instance, const char* file_path) {
     furi_check(instance);
 
+    bool success = false;
+
     ImageAnimationFileHeader* header = &instance->header;
 
-    bool parsed = false;
-
     do {
-        FileInfo file_info = {};
-        if(storage_common_stat(instance->storage, file_path, &file_info) != FSE_OK) {
-            FURI_LOG_D(TAG, "File not exist: %s", file_path);
-            break;
-        }
-        if(file_info.size < sizeof(ImageAnimationFileHeader)) {
-            FURI_LOG_D(TAG, "Incorrect file size: %lld", file_info.size);
-            break;
+        if(storage_file_is_open(instance->file)) {
+            storage_file_close(instance->file);
         }
 
         if(!storage_file_open(instance->file, file_path, FSAM_READ, FSOM_OPEN_EXISTING)) {
-            storage_file_close(instance->file);
             FURI_LOG_D(TAG, "Failed to open file %s", file_path);
             break;
         }
 
         size_t bytes_read =
             storage_file_read(instance->file, &instance->header, sizeof(ImageAnimationFileHeader));
+
         if(bytes_read != sizeof(ImageAnimationFileHeader)) {
-            storage_file_close(instance->file);
             FURI_LOG_D(TAG, "Failed to read file header");
             break;
         }
 
-        if(header->magic != IMAGE_ANIMATION_FILE_MAGIC) {
-            FURI_LOG_D(TAG, "Invalid magic num: %ld", header->magic);
+        if(!image_animation_validate_header(&instance->header, storage_file_size(instance->file))) {
             break;
         }
 
-        if(header->format_version != IMAGE_ANIMATION_FORMAT_VERSION) {
-            FURI_LOG_D(TAG, "Invalid  format version: %ld", header->format_version);
-            break;
-        }
-
-        if(header->fps > 100) {
-            FURI_LOG_D(TAG, "Unsupported fps: %ld. Must be less than 100", header->fps);
-            break;
-        }
-
-        if(header->bytes_per_pixel != 3 && header->bytes_per_pixel != 1) {
-            FURI_LOG_E(
-                TAG,
-                "Unsupported bytes per pixel: %lu. Must be 3 (RGB888) or 1 (L8)",
-                header->bytes_per_pixel);
-            break;
-        }
-
-        uint32_t frames_data_size =
-            header->frames * header->bytes_per_pixel * header->width * header->height;
-        if((frames_data_size + sizeof(ImageAnimationFileHeader)) != file_info.size) {
-            FURI_LOG_D(
-                TAG,
-                "Frames: %ld, BPP: %ld, Width: %ld, Height: %ld",
-                header->frames,
-                header->bytes_per_pixel,
-                header->width,
-                header->height);
-            FURI_LOG_D(
-                TAG,
-                "Incorrect file size: %lld. Expected size: %ld",
-                file_info.size,
-                frames_data_size + sizeof(ImageAnimationFileHeader));
-            break;
-        }
-
-        parsed = true;
+        success = true;
 
     } while(false);
 
-    if(parsed) {
+    if(success) {
         instance->frame_size = header->height * header->width * header->bytes_per_pixel;
         instance->frame_total = header->frames;
         instance->frame_idx = 0;
@@ -229,7 +243,7 @@ bool image_animation_set_source(ImageAnimation* instance, const char* file_path)
         image_animation_update_canvas_buffer(instance);
     }
 
-    return parsed;
+    return success;
 }
 
 void image_animation_start(ImageAnimation* instance) {
