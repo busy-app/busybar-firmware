0bbe351a4880ab20dbc2f45756519fd18af2f9b3
Georgii Surkov <georgii.surkov@outlook.com>
2025-03-14 15:13:12 +0000

    Update the Message app to use widgets

--- applications/system/message/message.c
+++ applications/system/message/message.c
@@ -1,20 +1,21 @@
 #include <furi.h>
+
 #include <gui/gui.h>
+#include <gui/modules/label.h>
 
 typedef struct {
     FuriEventLoop* event_loop;
     Gui* gui;
-    lv_obj_t* label;
+    Label* label;
 } MessageApp;
 
-static void message_app_key_callback(lv_event_t* event) {
-    const lv_event_code_t code = lv_event_get_code(event);
+static void message_app_input_callback(const InputEvent* event, void* context) {
+    furi_assert(event);
+    furi_assert(context);
+    MessageApp* instance = context;
 
-    if(code == LV_EVENT_KEY) {
-        const uint32_t key = *(uint32_t*)lv_event_get_param(event);
-
-        if(key == LV_KEY_ESC) {
-            MessageApp* instance = lv_event_get_user_data(event);
+    if(event->type == InputTypeShort) {
+        if(event->key == InputKeyBack) {
             furi_event_loop_stop(instance->event_loop);
         }
     }
@@ -25,25 +26,20 @@ static MessageApp* message_app_alloc(const char* message) {
     instance->event_loop = furi_event_loop_alloc();
     instance->gui = furi_record_open(RECORD_GUI);
 
-    with_gui_layer(instance->gui, GuiDisplayIdFront, GuiLayerIdActive, {
-        instance->label = lv_label_create(layer);
+    with_gui(instance->gui, {
+        Widget* root = gui_get_root_widget(gui, GuiDisplayIdFront, GuiLayerIdActive);
+        instance->label = label_alloc(root);
+        label_set_text(instance->label, message ? message : "Hello There");
 
-        lv_label_set_text(instance->label, message ? message : "Hello There");
-        lv_label_set_long_mode(instance->label, LV_LABEL_LONG_MODE_SCROLL_CIRCULAR);
-        lv_obj_set_style_text_font(instance->label, &lv_font_tiny5_8, LV_PART_MAIN);
-        lv_obj_set_style_text_color(instance->label, lv_color_white(), LV_PART_MAIN);
-        lv_obj_set_width(instance->label, lv_obj_get_width(layer));
-        lv_obj_center(instance->label);
-
-        lv_group_add_obj(lv_group_get_default(), instance->label);
-        lv_obj_add_event_cb(instance->label, message_app_key_callback, LV_EVENT_KEY, instance);
+        widget_set_input_callback((Widget*)instance->label, message_app_input_callback, instance);
+        gui_set_active_widget(gui, (Widget*)instance->label);
     });
 
     return instance;
 }
 
 static void message_app_free(MessageApp* instance) {
-    with_gui(instance->gui, { lv_obj_delete(instance->label); });
+    with_gui(instance->gui, { label_free(instance->label); });
 
     furi_record_close(RECORD_GUI);
 
