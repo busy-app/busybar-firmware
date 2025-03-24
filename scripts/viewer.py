b34662b3e46944122a71d32804677f9fef18612a
Georgii Surkov <georgii.surkov@outlook.com>
2025-03-24 16:25:58 +0000

    Add minimal viewer implementation

---
+++ scripts/viewer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import tempfile
+
+from flipper.app import App
+from seq2anim import BusyBarAnimation
+from flipper.storage_socket import FlipperStorage, FlipperStorageOperations
+
+
+class Main(App):
+    ANIM_FILE_NAME = "temp.anim"
+    ANIM_DEVICE_PATH = "/ext/animations"
+
+    def init(self):
+        self.subparsers = self.parser.add_subparsers(help="sub-command help")
+        self.parser_anim = self.subparsers.add_parser(
+            "anim", help="Start an animation on the device"
+        )
+        self.parser_anim.add_argument("source_dir", help="Source directory")
+        self.parser_anim.set_defaults(func=self.anim)
+
+    def anim(self):
+        with tempfile.TemporaryDirectory(prefix="bsb_anim") as temp_dir_path:
+            local_anim_path = os.path.join(temp_dir_path, self.ANIM_FILE_NAME)
+            device_anim_path = os.path.join(self.ANIM_DEVICE_PATH, self.ANIM_FILE_NAME)
+
+            animation = BusyBarAnimation(self.args.source_dir, 60, local_anim_path)
+            animation.process_images()
+
+            with FlipperStorage(self._get_port()) as storage:
+                FlipperStorageOperations(storage).recursive_send(
+                    device_anim_path, local_anim_path, True
+                )
+
+                self.logger.info("Starting animation player")
+
+                # TODO: Common CLI library for everything that uses it
+                storage.send_and_wait_prompt(
+                    f"loader open animation_player {device_anim_path}\r"
+                )
+
+        return 0
+
+    def _get_port(self):
+        return ("10.0.4.20", 23)
+
+
+if __name__ == "__main__":
+    Main()()
