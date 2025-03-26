94f5ee9d77cf7b5750d246c227a27e9e962dacac
SG <who.just.the.doctor@gmail.com>
2025-03-26 18:01:42 +0000

    back display service: cleanup code a little bit

--- applications/services/back_display/back_display.c
+++ applications/services/back_display/back_display.c
@@ -3,6 +3,8 @@
 #include <ssd1320/ssd1320.h>
 #include "back_display.h"
 
+#define CANARY_SIZE 512
+
 typedef enum {
     BackDisplayEventDraw = 1 << 0,
     BackDisplayEventTearing = 1 << 1,
@@ -26,25 +28,23 @@ static void buffer_l8_to_l4(uint8_t* dst_l4, const uint8_t* src_l8) {
     }
 }
 
-static void back_display_swap_buffers(BackDisplaySrv* instance) {
-    furi_check(instance);
-
-    furi_mutex_acquire(instance->buffers_mutex, FuriWaitForever);
-    uint8_t* tmp = instance->send_buffer;
-    instance->send_buffer = instance->draw_buffer;
-    instance->draw_buffer = tmp;
-    furi_mutex_release(instance->buffers_mutex);
-}
-
 static void back_display_event_callback(uint32_t events, void* context) {
     BackDisplaySrv* instance = context;
     furi_check(instance);
 
     if(events & BackDisplayEventDraw) {
-        back_display_swap_buffers(instance);
+        // swap buffers
+        furi_mutex_acquire(instance->buffers_mutex, FuriWaitForever);
+        uint8_t* tmp = instance->send_buffer;
+        instance->send_buffer = instance->draw_buffer;
+        instance->draw_buffer = tmp;
+        furi_mutex_release(instance->buffers_mutex);
+
+        // mark as dirty, to be drawn on next tearing event
         instance->dirty = true;
     }
 
+    // draw on tearing event, if needed
     if(instance->dirty && (events & BackDisplayEventTearing)) {
         ssd1320_draw(instance->draw_buffer);
         instance->dirty = false;
@@ -72,23 +72,23 @@ static BackDisplaySrv* back_display_alloc(void) {
     furi_event_loop_set_custom_event_callback(
         instance->event_loop, back_display_event_callback, instance);
 
-    furi_record_create(RECORD_BACK_DISPLAY, instance);
-
     ssd1320_init();
     furi_hal_gpio_init_simple(&gpio_oled_fr, GpioModeInterruptRise);
     furi_hal_gpio_add_int_callback(&gpio_oled_fr, back_display_tearing_callback, instance);
 
     furi_thread_set_current_priority(FuriThreadPriorityHigh);
 
+    furi_record_create(RECORD_BACK_DISPLAY, instance);
+
     return instance;
 }
 
-void back_display_draw(BackDisplaySrv* instance, const uint8_t* buf) {
+void back_display_draw(BackDisplaySrv* instance, const uint8_t* data) {
     furi_check(instance);
-    furi_check(buf);
+    furi_check(data);
 
     furi_mutex_acquire(instance->buffers_mutex, FuriWaitForever);
-    buffer_l8_to_l4(instance->draw_buffer, buf);
+    buffer_l8_to_l4(instance->draw_buffer, data);
     furi_mutex_release(instance->buffers_mutex);
 
     furi_event_loop_set_custom_event(instance->event_loop, BackDisplayEventDraw);
