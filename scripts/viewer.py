e60cb0e6d85c2680f907dde26ae7d28e6b1e331c
Georgii Surkov <georgii.surkov@outlook.com>
2025-04-08 17:54:37 +0100

    Python: add lottie support to viewer.py

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -16,12 +16,19 @@ class Main(App):
         self.parser.add_argument("-a", "--address", help="IP address or hostname", default="auto")
 
         self.subparsers = self.parser.add_subparsers(help="sub-command help")
+
         self.parser_anim = self.subparsers.add_parser(
             "anim", help="Start an animation on the device"
         )
         self.parser_anim.add_argument("source_dir", help="Source directory")
         self.parser_anim.add_argument("-f", "--fps", help="Animation FPS", type=int, default=60)
         self.parser_anim.set_defaults(func=self.anim)
+
+        self.parser_lottie = self.subparsers.add_parser(
+            "lottie", help="Start a lottie animation on the device"
+        )
+        self.parser_lottie.add_argument("lottie_file", help="Lottie JSON file")
+        self.parser_lottie.set_defaults(func=self.lottie)
 
     def anim(self):
         with tempfile.TemporaryDirectory(prefix="bsb_anim") as temp_dir_path:
@@ -46,6 +53,28 @@ class Main(App):
                 storage.send_and_wait_prompt(
                     f"loader open animation_player {device_anim_path}\r"
                 )
+
+        return 0
+
+    def lottie(self):
+        with FlipperStorage(self._get_address()) as storage:
+            lottie_file_path = self.args.lottie_file
+            lottie_file_name = os.path.basename(lottie_file_path)
+            device_anim_path = os.path.join(self.ANIM_DEVICE_PATH, lottie_file_name)
+
+            # TODO: Common CLI library for everything that uses it
+            self.logger.info("Closing currently running app")
+            storage.send_and_wait_prompt("loader kill\r")
+
+            self.logger.info("Uploading animation")
+            FlipperStorageOperations(storage).recursive_send(
+                device_anim_path, lottie_file_path, True
+            )
+
+            self.logger.info("Starting player app")
+            storage.send_and_wait_prompt(
+                f"loader open lottie_player {device_anim_path}\r"
+            )
 
         return 0
 
