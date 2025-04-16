9c86b462415b82d106cf1e06142b56cf8fd54cf1
Georgii Surkov <georgii.surkov@outlook.com>
2025-04-16 16:10:39 +0100

    Gui: Move fonts out of src folder

--- lib/lvgl_addons/fonts/lv_font_cubic_12.c
+++ lib/lvgl_addons/fonts/lv_font_cubic_12.c
@@ -589,7 +589,7 @@ lv_font_t lv_font_cubic_12 = {
 #endif
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
-    .fallback = &lv_font_ark_pixel_12,
+    .fallback = &lv_font_tiny5_8,
 #endif
     .user_data = NULL,
 };
