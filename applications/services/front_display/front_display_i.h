f0f1d2b45477760f07ad5033cac0c4277690f6c0
nminaylov <nm29719@gmail.com>
2025-04-24 19:26:02 +0300

    Display index LUT generation in runtime

--- applications/services/front_display/front_display_i.h
+++ applications/services/front_display/front_display_i.h
@@ -8,10 +8,10 @@
 #include <furi_hal_gpio.h>
 #include <furi_hal_dma.h>
 
-// LED current per color channel (HC | Gain)
-// #define CUR_GAIN_R (0x20 | 5)
-// #define CUR_GAIN_G (0x20 | 0)
-// #define CUR_GAIN_B (0x00 | 18)
+// LED current per color channel
+#define CUR_GAIN_R (255)
+#define CUR_GAIN_G (255)
+#define CUR_GAIN_B (255)
 
 #define DISPLAY_GAMMA (0.35f) // Default gamma value
 
@@ -21,6 +21,7 @@
 #define LED_DRIVER_CHAIN (3)
 #define PIXEL_BUF_LEN    (3 * 2 * 2) // Tx buffer len for 1 pixel (RGB * uint16_t * 2(Dual SPI))
 #define DISPLAY_BLOCKS   (8 * 3) // Scan blocks number (24)
+#define TRANSFER_COUNT   (FRONT_DISPLAY_H * FRONT_DISPLAY_W / LED_DRIVER_CHAIN)
 
 typedef struct FrontDisplayDriver FrontDisplayDriver;
 typedef struct FrontDisplayScan FrontDisplayScan;
