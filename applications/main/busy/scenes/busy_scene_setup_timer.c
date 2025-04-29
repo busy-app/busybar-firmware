bf9080ccc51eecc1170121c84ae88410f943a3eb
Georgii Surkov <georgii.surkov@outlook.com>
2025-04-29 11:36:10 +0100

    Busy: add timer setup menu to the front display

--- applications/main/busy/scenes/busy_scene_setup_timer.c
+++ applications/main/busy/scenes/busy_scene_setup_timer.c
@@ -6,6 +6,7 @@
 #include <gui/modules/flex_layout.h>
 
 typedef struct {
+    VarItemList* front_list;
     FlexLayout* back_layout;
     NavHeader* back_header;
     VarItemList* back_list;
@@ -52,6 +53,55 @@ static void busy_scene_setup_timer_speed_changed_callback(VarItem* item, void* c
     data->timer_config.enable_speed = var_item_get_value(item);
 }
 
+static void
+    busy_scene_setup_fill_var_item_list(VarItemList* list, BusySceneSetupTimer* data, bool set_cb) {
+    VarItem* item;
+
+    item = var_item_list_add_timebox(
+        list,
+        "WORK",
+        BUSY_TIMER_WORK_TIME_MIN_MN,
+        BUSY_TIMER_WORK_TIME_MAX_MN,
+        BUSY_TIMER_TIME_INCREMENT_MN,
+        set_cb ? busy_scene_setup_timer_work_changed_callback : NULL,
+        data);
+
+    var_item_set_value(item, data->timer_config.work_time_mn);
+
+    item = var_item_list_add_timebox(
+        list,
+        "REST",
+        BUSY_TIMER_REST_TIME_MIN_MN,
+        BUSY_TIMER_REST_TIME_MAX_MN,
+        BUSY_TIMER_TIME_INCREMENT_MN,
+        set_cb ? busy_scene_setup_timer_rest_changed_callback : NULL,
+        data);
+
+    var_item_set_value(item, data->timer_config.rest_time_mn);
+
+    item = var_item_list_add_spinbox(
+        list,
+        "CYCLES",
+        NULL,
+        BUSY_TIMER_CYCLE_COUNT_MIN,
+        BUSY_TIMER_CYCLE_COUNT_MAX,
+        BUSY_TIMER_CYCLE_INCREMENT,
+        set_cb ? busy_scene_setup_timer_cycles_changed_callback : NULL,
+        data);
+
+    var_item_set_value(item, data->timer_config.cycle_count);
+
+    item = var_item_list_add_switch(
+        list, "A.START", set_cb ? busy_scene_setup_timer_autostart_changed_callback : NULL, data);
+
+    var_item_set_value(item, data->timer_config.enable_autostart);
+
+    item = var_item_list_add_switch(
+        list, "HI.SPEED", set_cb ? busy_scene_setup_timer_speed_changed_callback : NULL, data);
+
+    var_item_set_value(item, data->timer_config.enable_speed);
+}
+
 static void busy_scene_setup_timer_on_enter(void* context) {
     furi_assert(context);
 
@@ -61,6 +111,9 @@ static void busy_scene_setup_timer_on_enter(void* context) {
     busy_timer_get_config(instance->busy_timer, &data->timer_config);
 
     with_gui(instance->gui, {
+        data->front_list = var_item_list_alloc(instance->front_window);
+        busy_scene_setup_fill_var_item_list(data->front_list, data, true);
+
         data->back_layout = flex_layout_alloc(instance->back_window, FlexLayoutTypeColumn);
 
         data->back_header = nav_header_alloc(flex_layout_get_base(data->back_layout));
@@ -71,52 +124,7 @@ static void busy_scene_setup_timer_on_enter(void* context) {
         data->back_list = var_item_list_alloc(flex_layout_get_base(data->back_layout));
         // TODO: Fix the layout to set appropriate sizes for children
         widget_set_height(var_item_list_get_base(data->back_list), 56);
-
-        VarItem* item;
-
-        item = var_item_list_add_timebox(
-            data->back_list,
-            "WORK",
-            BUSY_TIMER_WORK_TIME_MIN_MN,
-            BUSY_TIMER_WORK_TIME_MAX_MN,
-            BUSY_TIMER_TIME_INCREMENT_MN,
-            busy_scene_setup_timer_work_changed_callback,
-            data);
-
-        var_item_set_value(item, data->timer_config.work_time_mn);
-
-        item = var_item_list_add_timebox(
-            data->back_list,
-            "REST",
-            BUSY_TIMER_REST_TIME_MIN_MN,
-            BUSY_TIMER_REST_TIME_MAX_MN,
-            BUSY_TIMER_TIME_INCREMENT_MN,
-            busy_scene_setup_timer_rest_changed_callback,
-            data);
-
-        var_item_set_value(item, data->timer_config.rest_time_mn);
-
-        item = var_item_list_add_spinbox(
-            data->back_list,
-            "CYCLES",
-            NULL,
-            BUSY_TIMER_CYCLE_COUNT_MIN,
-            BUSY_TIMER_CYCLE_COUNT_MAX,
-            BUSY_TIMER_CYCLE_INCREMENT,
-            busy_scene_setup_timer_cycles_changed_callback,
-            data);
-
-        var_item_set_value(item, data->timer_config.cycle_count);
-
-        item = var_item_list_add_switch(
-            data->back_list, "A.START", busy_scene_setup_timer_autostart_changed_callback, data);
-
-        var_item_set_value(item, data->timer_config.enable_autostart);
-
-        item = var_item_list_add_switch(
-            data->back_list, "HI.SPEED", busy_scene_setup_timer_speed_changed_callback, data);
-
-        var_item_set_value(item, data->timer_config.enable_speed);
+        busy_scene_setup_fill_var_item_list(data->back_list, data, false);
     });
 }
 
