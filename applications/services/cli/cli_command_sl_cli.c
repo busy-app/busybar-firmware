0d0507e328e392307219401f1cb60dea79707f65
RebornedBrain <RebornedBrain@gmail.com>
2025-04-29 13:12:13 +0300

    Added function which invokes command on 917 side and waits for the response

--- applications/services/cli/cli_command_sl_cli.c
+++ applications/services/cli/cli_command_sl_cli.c
@@ -42,6 +42,28 @@ static void cli_command_sl_cli_free(CliCommandSlCli* instance) {
     free(instance);
 }
 
+void cli_command_sl_cli_send_command_get_response(Cli* cli, const char* command) {
+    CliCommandSlCli* instance = cli_command_sl_cli_alloc();
+
+    FuriString* buf = furi_string_alloc_printf("%s\r", command);
+    size_t sz = furi_string_size(buf);
+
+    const size_t tx_size = intercom_tx(
+        instance->intercom, IntercomChannelCli, furi_string_get_cstr(buf), sz, FuriWaitForever);
+    furi_assert(tx_size == sz);
+
+    while(true) {
+        const size_t rx_size = furi_stream_buffer_receive(
+            instance->rx_buffer, instance->rx_data, sizeof(instance->rx_data), 100);
+
+        if(!rx_size) break;
+        cli_write(cli, instance->rx_data, rx_size);
+    }
+
+    cli_command_sl_cli_free(instance);
+    furi_string_free(buf);
+}
+
 void cli_command_sl_cli(Cli* cli, FuriString* args, void* context) {
     UNUSED(args);
     UNUSED(context);
--- applications/services/cli/cli_command_sl_cli.h
+++ applications/services/cli/cli_command_sl_cli.h
@@ -2,4 +2,5 @@
 
 #include "cli_i.h"
 
+void cli_command_sl_cli_send_command_get_response(Cli* cli, const char* command);
 void cli_command_sl_cli(Cli* cli, FuriString* args, void* context);
