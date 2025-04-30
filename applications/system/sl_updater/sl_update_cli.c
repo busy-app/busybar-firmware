18be1bb0a4a2f00ffb3955a453b7e312f5d7f4d4
RebornedBrain <RebornedBrain@gmail.com>
2025-04-30 15:06:26 +0300

    New option and logic added to updater_cli

--- applications/system/sl_updater/sl_update_cli.c
+++ applications/system/sl_updater/sl_update_cli.c
@@ -7,11 +7,12 @@
 #define SL_UPDATE_M4_COMM_TIMEOUT_S  (15)
 #define SL_UPDATE_NWP_COMM_TIMEOUT_S (30)
 
-#define SL_UPDATE_RETRIES (3)
+#define SL_UPDATE_RETRIES  (3)
+#define SL_PROBING_RETRIES (3)
 
 static void updater_cli_command_print_usage(void) {
     printf("Usage:\r\n");
-    printf("update <u5|917|917_ta> path\r\n");
+    printf("update <u5|917|917_ta|917_probe> path\r\n");
 }
 
 static bool
@@ -25,6 +26,26 @@ static bool
         baud_throttle_ratio);
     sl_updater_free(instance);
     return success;
+}
+
+static void updater_cli_probe_excute() {
+    SlUpdater* instance = sl_updater_alloc();
+    FuriString* version = furi_string_alloc();
+    for(int i = 0; i < SL_PROBING_RETRIES; i++) {
+        printf("Probing...\r\n");
+        if(sl_update_probe(instance, i, version)) {
+            printf("Success\r\n%s", furi_string_get_cstr(version));
+            break;
+        } else {
+            if(i == SL_PROBING_RETRIES - 1) {
+                printf("Probe failed\r\n");
+                break;
+            }
+            printf("Probing failed, retrying (%d/%d)\r\n", i + 1, SL_PROBING_RETRIES);
+        }
+    }
+    furi_string_free(version);
+    sl_updater_free(instance);
 }
 
 static void updater_cli(Cli* cli, FuriString* args, void* context) {
@@ -41,6 +62,11 @@ static void updater_cli(Cli* cli, FuriString* args, void* context) {
 
         if(furi_string_equal_str(cmd, "u5")) {
             printf("Not yet implemented\r\n");
+            break;
+        }
+
+        if(furi_string_equal_str(cmd, "917_probe")) {
+            updater_cli_probe_excute();
             break;
         }
 
