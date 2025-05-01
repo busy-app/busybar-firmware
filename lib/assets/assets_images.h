c2f85ea97dd5e983aaa68af2cfdc7f1cbc8e5e6b
Georgii Surkov <georgii.surkov@outlook.com>
2025-05-01 14:08:27 +0100

    Gui: show a placeholder image if a file load fails

--- lib/assets/assets_images.h
+++ lib/assets/assets_images.h
@@ -3,6 +3,7 @@
 // TODO: Wrap built-in images into a type that does not require LVGL headers
 #include <lvgl.h>
 
+extern const lv_image_dsc_t I_load_error_9x9;
 extern const lv_image_dsc_t I_pause_5x5;
 extern const lv_image_dsc_t I_setup_12x12;
 extern const lv_image_dsc_t I_start_12x12;
