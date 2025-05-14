770562c79eadd4591935a99a67ee0becc7e890a8
RebornedBrain <RebornedBrain@gmail.com>
2025-05-14 20:03:03 +0300

    Moved previous font back

--- targets/f20/config/lv_conf.h
+++ targets/f20/config/lv_conf.h
@@ -594,7 +594,8 @@
     LV_FONT_DECLARE(lv_font_somybmp_7) \
     LV_FONT_DECLARE(lv_font_ark_numerals_small_10) \
     LV_FONT_DECLARE(lv_font_ark_numerals_regular_10) \
-    LV_FONT_DECLARE(lv_font_ark_numerals_condensed_10)
+    LV_FONT_DECLARE(lv_font_ark_numerals_condensed_10) \
+    LV_FONT_DECLARE(lv_font_haxrcorp4089_16)
 
 /** Always set a default font */
 #define LV_FONT_DEFAULT &lv_font_tiny5_8
