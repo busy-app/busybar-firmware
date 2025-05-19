eef95a2fd8f19013c0146590e6d30204d17a02ea
RebornedBrain <RebornedBrain@gmail.com>
2025-05-19 15:10:00 +0300

    Raw http implementation for image showing

--- applications/services/web_server/http_api/api_root.c
+++ applications/services/web_server/http_api/api_root.c
@@ -1,7 +1,8 @@
 #include "http_api.h"
 #include <version.h>
+#include <loader/loader.h>
+#include <desktop/desktop.h>
 #include <gui/gui.h>
-#include <gui/modules/image.h>
 
 typedef struct {
     bool led_state;
@@ -102,6 +103,7 @@ static bool
     char file_path[32];
     GuiDisplayId display_id;
     bool success = false;
+    UNUSED(display_id);
 
     do {
         if(msg->query.len == 0) {
@@ -113,13 +115,7 @@ static bool
             break;
         }
 
-        Storage* storage = furi_record_open(RECORD_STORAGE);
-        bool file_exists = storage_file_exists(storage, file_path);
-        furi_record_close(RECORD_STORAGE);
-        if(!file_exists) {
-            FURI_LOG_E("HTTP display", "Image file not exists");
-            break;
-        }
+        //TODO: place some file saving logic in here
 
         char display_id_str[8];
         var_len = mg_http_get_var(&msg->query, "display", display_id_str, sizeof(display_id_str));
@@ -142,35 +138,34 @@ static bool
         return true;
     }
 
-    FURI_LOG_I(
-        "HTTP display",
-        "Showing %s on %s display",
-        file_path,
-        (display_id == GuiDisplayIdBack) ? "back" : "front");
-
-    Gui* gui = furi_record_open(RECORD_GUI);
-
-    Image* image;
-
-    bool result = false;
-    with_gui(gui, {
-        GuiLayer* system_layer = gui_get_layer(gui, GuiLayerIdSystem);
-        Widget* root = gui_layer_get_root_widget(system_layer, display_id);
-        image = image_alloc(root);
-        result = image_set_source(image, file_path);
-    });
-
-    if(!result) {
-        FURI_LOG_I("HTTP display", "Unable to load image");
-        mg_http_reply(conn, 400, "", "Image load error");
-    } else {
-        mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"result\":\"OK\"}\n");
+    Desktop* desktop = furi_record_open(RECORD_DESKTOP);
+    if(!desktop_replace_current_app(desktop, "http_viewer", "")) {
+        mg_http_reply(conn, 400, "", "Failed to load app");
+        success = false;
     }
 
-    // TODO: with_gui(gui, { image_free(image); });
+    if(success)
+        mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"result\":\"OK\"}\n");
 
-    furi_record_close(RECORD_GUI);
+    furi_record_close(RECORD_DESKTOP);
 
+    return true;
+}
+
+static bool http_api_display_delete_callback(
+    struct mg_connection* conn,
+    struct mg_http_message* msg,
+    void* ctx) {
+    UNUSED(conn);
+    UNUSED(msg);
+    UNUSED(ctx);
+    FURI_LOG_I("HTTP display", "DELETE");
+
+    Loader* loader = furi_record_open(RECORD_LOADER);
+    loader_stop(loader);
+    furi_record_close(RECORD_LOADER);
+    // mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"result\":\"OK\"}\n");
+    mg_http_reply(conn, 200, "", "OK");
     return true;
 }
 
@@ -191,9 +186,15 @@ static const HttpHandler handlers_api_root[] = {
     },
     {
         .uri = "/*/display",
-        .method = "GET",
+        .method = "POST",
         .type = HttpHandlerCustom,
         .callback = http_api_display_callback,
+    },
+    {
+        .uri = "/*/display",
+        .method = "DELETE",
+        .type = HttpHandlerCustom,
+        .callback = http_api_display_delete_callback,
     },
 };
 
