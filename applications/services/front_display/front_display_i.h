2748b41d6c03164c35801ef1ec49d6a7254162a2
nminaylov <nm29719@gmail.com>
2025-05-28 19:32:41 +0300

    Front display GPIO high speed

--- applications/services/front_display/front_display_i.h
+++ applications/services/front_display/front_display_i.h
@@ -23,6 +23,8 @@
 #define DISPLAY_BLOCKS   (8 * 3) // Scan blocks number (24)
 #define TRANSFER_COUNT   (FRONT_DISPLAY_H * FRONT_DISPLAY_W / LED_DRIVER_CHAIN)
 
+#define DISPLAY_GPIO_SPEED GpioSpeedHigh
+
 typedef struct FrontDisplayDriver FrontDisplayDriver;
 typedef struct FrontDisplayScan FrontDisplayScan;
 
