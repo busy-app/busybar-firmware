5be540910dcea1e169c8fef083ec5bf553c7fe80
Georgii Surkov <georgii.surkov@outlook.com>
2025-05-30 12:01:25 +0100

    Busy: divide various presets to separate file

---
+++ applications/main/busy/busy_presets.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "busy.h"
+
+typedef struct {
+    Color color;
+    const char* mask_path;
+    TransitionOverlayColorMode color_mode;
+    TransitionOverlayMaskMode mask_mode;
+    struct {
+        uint32_t in_ms;
+        uint32_t out_ms;
+    } timings;
+    bool enable_press;
+} BusyTransition;
+
+extern const BusyTransition busy_transitions[BusyTransitionTypeMax];
+
+extern const StatusLightsCommand busy_status_lights[BusyStatusLightsTypeMax];
