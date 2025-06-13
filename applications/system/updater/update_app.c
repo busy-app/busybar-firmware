0665d14b85d03b813f797d617e79f318466e3a4b
hedger <hedger@nanode.su>
2025-06-13 19:22:40 +0100

    updater: progress on main display

--- applications/system/updater/update_app.c
+++ applications/system/updater/update_app.c
@@ -10,6 +10,7 @@
 #include <gui/gui.h>
 #include <gui/modules/flex_layout.h>
 #include <gui/modules/label.h>
+#include <gui/modules/canvas.h>
 
 #include <toolbox/path.h>
 #include <toolbox/update_lib/dfu_file.h>
@@ -28,10 +29,16 @@ typedef struct {
     // Back display
     FlexLayout* flex;
     Label *stage_label, *progress_label;
+    // Front display
+    Canvas* canvas;
 
     UpdateTask* update_task;
     FuriString* startup_arg;
 } Updater;
+
+static const Color ColorGreen = {.r = 0, .g = 140, .b = 20};
+static const Color ColorRed = {.r = 140, .g = 0, .b = 10};
+static const Color ColorWhite = {.r = 255, .g = 255, .b = 255};
 
 static void updater_task_on_progress(
     const char* status,
@@ -40,6 +47,7 @@ static void updater_task_on_progress(
     void* state) {
     furi_check(state);
     Updater* updater = state;
+    FuriString* progress_pct_str = furi_string_alloc_printf("%d%%", stage_pct);
 
     with_gui(updater->gui, {
         if(failed) {
@@ -50,55 +58,99 @@ static void updater_task_on_progress(
             label_set_text_fmt(updater->stage_label, "%s", status ? status : "Unknown stage");
             label_set_text_fmt(updater->progress_label, "Update progress: %d%%", stage_pct);
         }
+
+        Canvas* canvas = updater->canvas;
+        canvas_draw_begin(canvas);
+        canvas_clear(canvas);
+        canvas_set_fill_color(canvas, failed ? ColorRed : ColorGreen);
+        canvas_set_line_color(
+            canvas, failed ? ColorRed : ColorGreen); // Though likely unused for filled rects
+        canvas_set_line_width(canvas, 1);
+
+        uint8_t bar_x = 1;
+        uint8_t bar_y = 7;
+        uint8_t bar_h = 8;
+        uint8_t bar_w = (70 * stage_pct) / 100;
+        const uint8_t radius = 1;
+
+        if(bar_w > 0) { // Only draw if width is positive
+            if(bar_w < (2 * radius + 1)) { // If bar is too thin for 1px rounding (i.e., width < 3)
+                // Draw a sharp-cornered rectangle
+                canvas_draw_rect(canvas, bar_x, bar_y, bar_w, bar_h, true);
+            } else {
+                // Draw the main horizontal part of the rounded rectangle
+                // (width will be bar_w - 2*radius, which is bar_w - 2)
+                canvas_draw_rect(canvas, bar_x + radius, bar_y, bar_w - 2 * radius, bar_h, true);
+                // Draw the main vertical part of the rounded rectangle
+                // (height will be bar_h - 2*radius, which is bar_h - 2)
+                canvas_draw_rect(canvas, bar_x, bar_y + radius, bar_w, bar_h - 2 * radius, true);
+            }
+        }
+
+        canvas_set_fill_color(canvas, ColorWhite);
+        canvas_draw_text(canvas, 16, 0, furi_string_get_cstr(progress_pct_str));
+
+        canvas_draw_end(canvas);
     });
+
+    furi_string_free(progress_pct_str);
 }
 
 Updater* updater_alloc(const char* arg) {
-    Updater* updater = malloc(sizeof(Updater));
+    Updater* instance = malloc(sizeof(Updater));
 
-    updater->storage = furi_record_open(RECORD_STORAGE);
-    updater->startup_arg = furi_string_alloc_set(arg ? arg : "");
-    updater->update_task = update_task_alloc();
+    instance->storage = furi_record_open(RECORD_STORAGE);
+    instance->startup_arg = furi_string_alloc_set(arg ? arg : "");
+    instance->update_task = update_task_alloc();
 
-    updater->gui = furi_record_open(RECORD_GUI);
-    updater->event_loop = furi_event_loop_alloc();
+    instance->gui = furi_record_open(RECORD_GUI);
+    instance->event_loop = furi_event_loop_alloc();
 
-    with_gui(updater->gui, {
-        GuiLayer* main_layer = gui_get_layer(updater->gui, GuiLayerIdMain);
-        updater->flex = flex_layout_alloc(
+    with_gui(instance->gui, {
+        GuiLayer* main_layer = gui_get_layer(instance->gui, GuiLayerIdMain);
+        instance->flex = flex_layout_alloc(
             gui_layer_get_root_widget(main_layer, GuiDisplayIdBack), FlexLayoutTypeColumn);
 
-        Widget* flex_base = flex_layout_get_base(updater->flex);
-        updater->stage_label = label_alloc(flex_base);
-        label_set_text(updater->stage_label, "Updater is starting...");
+        Widget* flex_base = flex_layout_get_base(instance->flex);
+        instance->stage_label = label_alloc(flex_base);
+        label_set_text(instance->stage_label, "Updater is starting...");
 
-        updater->progress_label = label_alloc(flex_base);
-        label_set_text(updater->progress_label, "Progress: 0%%");
+        instance->progress_label = label_alloc(flex_base);
+        label_set_text(instance->progress_label, "Waiting for update...");
+
+        // Front display
+        Widget* root = gui_layer_get_root_widget(main_layer, GuiDisplayIdFront);
+
+        instance->canvas = canvas_alloc(root, 72, 16);
     });
 
-    update_task_set_progress_cb(updater->update_task, updater_task_on_progress, updater);
-    update_task_start(updater->update_task);
+    update_task_set_progress_cb(instance->update_task, updater_task_on_progress, instance);
+    update_task_start(instance->update_task);
 
-    return updater;
+    return instance;
 }
 
-void updater_free(Updater* updater) {
-    furi_check(updater);
+void updater_free(Updater* instance) {
+    furi_check(instance);
 
-    if(updater->update_task) {
-        update_task_set_progress_cb(updater->update_task, NULL, NULL);
-        update_task_free(updater->update_task);
+    if(instance->update_task) {
+        update_task_set_progress_cb(instance->update_task, NULL, NULL);
+        update_task_free(instance->update_task);
     }
 
-    flex_layout_free(updater->flex);
-    label_free(updater->stage_label);
-    label_free(updater->progress_label);
-    furi_event_loop_free(updater->event_loop);
-    furi_string_free(updater->startup_arg);
+    with_gui(instance->gui, {
+        flex_layout_free(instance->flex);
+        label_free(instance->stage_label);
+        label_free(instance->progress_label);
+        canvas_free(instance->canvas);
+    });
+
+    furi_event_loop_free(instance->event_loop);
+    furi_string_free(instance->startup_arg);
 
     furi_record_close(RECORD_GUI);
     furi_record_close(RECORD_STORAGE);
-    free(updater);
+    free(instance);
 }
 
 int32_t updater_srv(void* arg) {
