80a01b44a9124c689fd6f61bbae1e0a575570bed
nminaylov <nm29719@gmail.com>
2025-06-25 14:48:43 +0300

    sdmmc unaligned write fix, mongoose io size change

--- targets/f20/config/mongoose_config.h
+++ targets/f20/config/mongoose_config.h
@@ -14,7 +14,10 @@
 #define MG_ENABLE_LOG               1
 #define MG_ENABLE_POSIX_FS          0
 #define MG_ENABLE_DIRLIST           1
-#define MG_DATA_SIZE                32
-#define MG_IO_SIZE                  1460 * 4
+
+#define MG_DATA_SIZE 32
+
+#define MG_MAX_RECV_SIZE (256 * 1024)
+#define MG_IO_SIZE       1460
 
 // #define MG_ENABLE_TCPIP_PRINT_DEBUG_STATS 1
