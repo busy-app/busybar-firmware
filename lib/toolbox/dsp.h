7455fe7156e4bd645ce5d7db20536c3ed93eaced
SG <who.just.the.doctor@gmail.com>
2025-07-17 13:12:51 +0300

    dsp lib: fix formatting

--- lib/toolbox/dsp.h
+++ lib/toolbox/dsp.h
@@ -1,3 +1,11 @@
 #pragma once
 
+/**
+ * @brief Applies a one pole low-pass filter to the input signal.
+ *
+ * @param input The current input sample.
+ * @param prev_output The previous output sample.
+ * @param alpha The smoothing factor (0 < alpha < 1).
+ * @return The filtered output sample.
+ */
 float dsp_low_pass(float input, float prev_output, float alpha);
