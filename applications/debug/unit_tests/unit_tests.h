692152fe630cc164aab17df99fa9637f9b9ef8c6
SG <who.just.the.doctor@gmail.com>
2025-07-17 17:43:37 +0300

    unit tests: comment about file guarantee

--- applications/debug/unit_tests/unit_tests.h
+++ applications/debug/unit_tests/unit_tests.h
@@ -2,5 +2,8 @@
 #include <storage/storage.h>
 #include "minunit.h"
 
-#define UNIT_TESTS_FOLDER     EXT_PATH("unit_tests")
+#define UNIT_TESTS_FOLDER EXT_PATH("unit_tests")
+
+// This macro is used to create a path for unit tests files
+// We guarantee that the unit_tests folder is always exists
 #define UNIT_TESTS_PATH(path) UNIT_TESTS_FOLDER "/" path
