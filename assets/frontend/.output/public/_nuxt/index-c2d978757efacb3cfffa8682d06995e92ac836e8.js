669e3bb55864e3d4e793a856e105a6ae8d5eaab2
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-07-22 13:39:22 +0300

    feat: Add cloud login link

--- assets/frontend/.output/public/_nuxt/index-c2d978757efacb3cfffa8682d06995e92ac836e8.js
+++
@@ -1,1 +0,0 @@
-import{f as e,u as t,c as n,o}from"./entry-c2d978757efacb3cfffa8682d06995e92ac836e8.js";const i=e({__name:"index",setup(a){return t({title:"BUSY Bar Control Panel",meta:[{name:"description",content:"Set up and control your Busy Bar in the browser"}]}),(r,s)=>(o(),n("div"))}});export{i as default};
