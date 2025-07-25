8170c80d4f7f3f9d60be385273e274082102235e
Ivan Shestakov <vanyww2@gmail.com>
2025-07-25 13:58:18 +0400

    Implement battery status live update

--- applications/services/status_bar/status_bar.c
+++ applications/services/status_bar/status_bar.c
@@ -1,11 +1,15 @@
 #include "status_bar.h"
 #include "storage_macros.h"
 
+#include "widgets/battery_status_indicator.h"
+
 #include <lvgl.h>
 
 #include <gui/gui.h>
 #include <gui/modules/image.h>
 #include <gui/modules/flex_layout.h>
+
+#include <power/power_service/power.h>
 
 #define TAG "StatusBar"
 
@@ -14,13 +18,75 @@
 struct StatusBar {
     FuriEventLoop* event_loop;
     Gui* gui;
+    Power* power;
+    BatteryStatusIndicator* battery_status_indicator;
 };
+
+typedef enum {
+    StatusBarUpdateEventPowerChargingState = 1 << 0,
+    StatusBarUpdateEventPowerCharge = 1 << 1,
+
+    StatusBarUpdateEventAnyPower = StatusBarUpdateEventPowerChargingState |
+                                   StatusBarUpdateEventPowerCharge
+} StatusBarUpdateEvent;
+
+static void power_events_callback(const void* message, void* context) {
+    furi_assert(message);
+    furi_assert(context);
+
+    PowerEvent* event = (PowerEvent*)message;
+    StatusBar* instance = context;
+    StatusBarUpdateEvent update_event;
+
+    switch(event->type) {
+    case PowerEventChargingStateUpdate:
+        update_event = StatusBarUpdateEventPowerChargingState;
+        break;
+
+    case PowerEventChargeUpdate:
+        update_event = StatusBarUpdateEventPowerCharge;
+        break;
+
+    default:
+        return;
+    }
+
+    furi_event_loop_set_custom_event(instance->event_loop, update_event);
+}
+
+static void status_bar_custom_event_callback(uint32_t events, void* context) {
+    StatusBar* instance = context;
+    PowerInfo* power_info = NULL;
+
+    if(READ_BIT(events, StatusBarUpdateEventAnyPower)) {
+        power_info = alloca(sizeof(*power_info));
+        power_get_info(instance->power, power_info);
+    }
+
+    with_gui(instance->gui, {
+        if(READ_BIT(events, StatusBarUpdateEventPowerChargingState)) {
+            battery_status_indicator_set_charging_state(
+                instance->battery_status_indicator, power_info->is_charging);
+        }
+
+        if(READ_BIT(events, StatusBarUpdateEventPowerCharge)) {
+            battery_status_indicator_set_charge_amount(
+                instance->battery_status_indicator,
+                (power_info->is_full_charged) ? BATTERY_STATUS_INDICATOR_MAX_CHARGE :
+                                                power_info->charge);
+        }
+    })
+}
 
 static StatusBar* status_bar_alloc(void) {
     StatusBar* instance = malloc(sizeof(*instance));
 
     instance->event_loop = furi_event_loop_alloc();
     instance->gui = furi_record_open(RECORD_GUI);
+    instance->power = furi_record_open(RECORD_POWER);
+
+    PowerInfo power_info;
+    power_get_info(instance->power, &power_info);
 
     with_gui(instance->gui, {
         GuiLayer* system_layer = gui_get_layer(instance->gui, GuiLayerIdSystem);
@@ -52,13 +118,24 @@ static StatusBar* status_bar_alloc(void) {
         widget_set_margin(image_get_base(usb), 0, 0, 2, 2);
         widget_set_size(image_get_base(usb), LV_SIZE_CONTENT, LV_SIZE_CONTENT);
 
-        Image* battery = image_alloc(flex_layout_get_base(status_bar));
-        widget_add_flag(image_get_base(battery), LV_OBJ_FLAG_IGNORE_LAYOUT);
-        widget_set_align(image_get_base(battery), AlignBottomMid);
-        image_set_source(battery, STATUS_BAR_IMG_PATH("battery_8x18.bin"));
-        widget_set_margin(image_get_base(battery), 0, 0, 2, 2);
-        widget_set_size(image_get_base(battery), LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+        instance->battery_status_indicator =
+            battery_status_indicator_alloc(flex_layout_get_base(status_bar));
+        widget_add_flag(
+            battery_status_indicator_get_base(instance->battery_status_indicator),
+            LV_OBJ_FLAG_IGNORE_LAYOUT);
+        widget_set_align(
+            battery_status_indicator_get_base(instance->battery_status_indicator), AlignBottomMid);
+        battery_status_indicator_set_error_state(instance->battery_status_indicator, false);
+        battery_status_indicator_set_charging_state(
+            instance->battery_status_indicator, power_info.is_charging);
+        battery_status_indicator_set_charge_amount(
+            instance->battery_status_indicator, power_info.charge);
     });
+
+    furi_event_loop_set_custom_event_callback(
+        instance->event_loop, status_bar_custom_event_callback, instance);
+
+    furi_pubsub_subscribe(power_get_pubsub(instance->power), power_events_callback, instance);
 
     furi_record_create(RECORD_STATUS_BAR, instance);
     return instance;
