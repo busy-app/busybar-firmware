54d0010922ef7f63ca701baf1295816fde1e8602
Georgii Surkov <georgii.surkov@outlook.com>
2025-07-31 19:56:40 +0100

    Wifi: set intercom callback inside netif init fn

--- applications/services/wifi/wifi_net.c
+++ applications/services/wifi/wifi_net.c
@@ -70,20 +70,6 @@ static void wifi_disable_netif_callback(void* arg) {
     netif_set_link_down(netif);
 }
 
-static void wifi_add_netif_callback(void* arg) {
-    furi_assert(arg);
-    Wifi* instance = arg;
-
-    const ip_addr_t ip = {0};
-    const ip_addr_t netmask = {0};
-    const ip4_addr_t gateway = {0};
-
-    struct netif* netif = &instance->netif;
-
-    netif_add(netif, &ip, &netmask, &gateway, instance, wifi_init_netif_callback, tcpip_input);
-    netif_set_default(netif);
-}
-
 static void wifi_net_intercom_rx_callback(const void* data, size_t data_size, void* context) {
     furi_assert(context);
     Wifi* instance = context;
@@ -125,14 +111,26 @@ static void wifi_net_intercom_rx_callback(const void* data, size_t data_size, vo
     }
 }
 
-void wifi_net_init(Wifi* instance) {
-    furi_record_open(RECORD_USB_NETWORK);
-    tcpip_callback(wifi_add_netif_callback, instance);
-    // TODO: semaphore or something
-    furi_delay_ms(1000);
+static void wifi_add_netif_callback(void* arg) {
+    furi_assert(arg);
+    Wifi* instance = arg;
+
+    const ip_addr_t ip = {0};
+    const ip_addr_t netmask = {0};
+    const ip4_addr_t gateway = {0};
+
+    struct netif* netif = &instance->netif;
+
+    netif_add(netif, &ip, &netmask, &gateway, instance, wifi_init_netif_callback, tcpip_input);
+    netif_set_default(netif);
 
     intercom_set_rx_callback(
         instance->intercom, IntercomChannelWifiData, wifi_net_intercom_rx_callback, instance);
+}
+
+void wifi_net_init(Wifi* instance) {
+    furi_record_open(RECORD_USB_NETWORK);
+    tcpip_callback(wifi_add_netif_callback, instance);
 }
 
 void wifi_net_set_hw_address(Wifi* instance, const WifiHardwareAddress* addr) {
