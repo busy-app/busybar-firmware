1bfd9d407efea0b329ddd82972947121e0d787f3
Anna Antonenko <portasynthinca3@gmail.com>
2025-08-01 18:44:07 +0400

    change device type to on/off

--- applications/system/matter/helpers/matter_app.cpp
+++ applications/system/matter/helpers/matter_app.cpp
@@ -4,25 +4,14 @@
 #include <furi.h>
 #include <MatterConfig.h>
 #include <BaseApplication.h>
-// #include <app/clusters/occupancy-sensor-server/occupancy-sensor-server.h>
-#include <app/clusters/occupancy-sensor-server/occupancy-hal.h>
 #include <app-common/zap-generated/attributes/Accessors.h>
-// #include <app/PluginApplicationCallbacks.h>
 
 #define TAG "MatterApp"
 
 using namespace chip;
 using namespace chip::app::Clusters;
 
-constexpr EndpointId kModeEndpoint = 1;
-constexpr EndpointId kOccupancyEndpoint = 2;
-
-HalOccupancySensorType halOccupancyGetSensorType(EndpointId endpoint) {
-    FURI_LOG_D(TAG, "Initializing occupancy HAL");
-    furi_check(endpoint == kOccupancyEndpoint);
-    return HAL_OCCUPANCY_SENSOR_TYPE_PHYSICAL;
-}
-
+constexpr EndpointId kOnOffEndpoint = 1;
 void matter_app_init(void) {
     //sl_mbedtls_init();
     SilabsMatterConfig::AppInit();
@@ -35,23 +24,17 @@ void matter_factory_reset(void) {
 void matter_button_press(void) {
     FURI_LOG_D(TAG, "BUSY mode");
 
-    DeviceLayer::PlatformMgr().LockChipStack();
-    halOccupancyStateChangedCallback(kOccupancyEndpoint, HAL_OCCUPANCY_STATE_OCCUPIED);
-    DeviceLayer::PlatformMgr().UnlockChipStack();
-
-    // using namespace OccupancySensing;
-    // Attributes::Occupancy::Set(kOccupancyEndpoint, OccupancyBitmap::kOccupied);
+    // DeviceLayer::PlatformMgr().LockChipStack();
+    // halOccupancyStateChangedCallback(kOnOffEndpoint, HAL_OCCUPANCY_STATE_OCCUPIED);
+    // DeviceLayer::PlatformMgr().UnlockChipStack();
 }
 
 void matter_button_release(void) {
     FURI_LOG_D(TAG, "REST mode");
 
-    DeviceLayer::PlatformMgr().LockChipStack();
-    halOccupancyStateChangedCallback(kOccupancyEndpoint, HAL_OCCUPANCY_STATE_UNOCCUPIED);
-    DeviceLayer::PlatformMgr().UnlockChipStack();
-
-    // using namespace OccupancySensing;
-    // Attributes::Occupancy::Set(kOccupancyEndpoint, 0);
+    // DeviceLayer::PlatformMgr().LockChipStack();
+    // halOccupancyStateChangedCallback(kOnOffEndpoint, HAL_OCCUPANCY_STATE_UNOCCUPIED);
+    // DeviceLayer::PlatformMgr().UnlockChipStack();
 }
 
 void matter_basic_commissioning_window(void) {
