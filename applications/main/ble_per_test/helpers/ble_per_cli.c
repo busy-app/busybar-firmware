294ccc13d54f8c47d771dde3a5248d9a336758ac
SkorP <skorpionm@yandex.ru>
2025-08-06 10:28:42 +0400

    fix CLI

--- applications/main/ble_per_test/helpers/ble_per_cli.c
+++ applications/main/ble_per_test/helpers/ble_per_cli.c
@@ -1,5 +1,8 @@
 #include "ble_per_cli.h"
-#include <intercom/intercom.h>
+
+#include <cli_intercom/cli_intercom.h>
+#include <containers/pipe_util.h>
+
 #include <furi_hal_cortex.h>
 #include <cli/args.h>
 #include <strint.h>
@@ -8,14 +11,20 @@
 #define CLI_BUFFER_SIZE       (1024U)
 #define CLI_READ_TIMEOUT      (10U)
 #define CLI_START_APP_TIMEOUT (5000000U) // 5 seconds
+#define TRANSFER_BATCH_SIZE   512UL
 
 typedef struct {
-    Intercom* intercom;
     FuriStreamBuffer* rx_buffer;
     uint8_t rx_data[CLI_BUFFER_SIZE];
     FuriThread* thread;
+    FuriThread* thread_event_loop;
     FuriString* rx_msg;
     BlePerTest* app_handle;
+
+    CliIntercom* cli_intercom;
+    PipeSide* own_pipe;
+    PipeSide* shell_pipe;
+    FuriEventLoop* event_loop;
 } CliCommandSlCli;
 
 typedef enum {
@@ -65,7 +74,7 @@ typedef struct {
 
 static const BlePerCliCmd ble_per_cli_cmd[BlePerCliCmdTypeMax] = {
     [BlePerCliCmdTypeStartApp] = {"ble_per_test"},
-    [BlePerCliCmdTypeEndApp] = {"^C"}, //Ctrl+C
+    [BlePerCliCmdTypeEndApp] = {"exit"},
     [BlePerCliCmdTypeModeTx] = {"tx"},
     [BlePerCliCmdTypeModeRx] = {"rx"},
     [BlePerCliCmdTypeModeTxRxStop] = {"stop"},
@@ -103,30 +112,25 @@ BlePerStats ble_per_cli_stats[BlePerCliStatsCmdMax] = {
 
 CliCommandSlCli* ble_per_cli_instance = NULL;
 
-static void cli_command_917_rx_callback(const void* data, size_t data_size, void* context) {
-    furi_check(data);
-    furi_check(context);
-
+static void cli_uart_data_from_pipe(PipeSide* pipe, void* context) {
     CliCommandSlCli* instance = context;
-    UNUSED(instance);
-    UNUSED(data_size);
-    for(size_t i = 0; i < data_size; i++) {
-        FURI_LOG_RAW_E("%c", ((uint8_t*)data)[i]);
-    }
+
+    size_t bytes_in_pipe = pipe_bytes_available(pipe);
+    size_t to_transfer = MIN(bytes_in_pipe, TRANSFER_BATCH_SIZE);
+
+    uint8_t buffer[to_transfer];
+    furi_check(pipe_receive(pipe, buffer, to_transfer) == to_transfer);
     furi_check(
-        furi_stream_buffer_send(instance->rx_buffer, data, data_size, FuriWaitForever) ==
-        data_size);
-    furi_thread_flags_set(
-        furi_thread_get_id(ble_per_cli_instance->thread), BlePerCliThreadEventRxData);
+        furi_stream_buffer_send(instance->rx_buffer, buffer, sizeof(buffer), FuriWaitForever) ==
+        sizeof(buffer));
+    furi_thread_flags_set(furi_thread_get_id(instance->thread), BlePerCliThreadEventRxData);
 }
 
 void ble_per_cli_data_tx(uint8_t* data, size_t data_size) {
     furi_check(data);
     furi_check(data_size);
 
-    const size_t tx_size = intercom_tx(
-        ble_per_cli_instance->intercom, IntercomChannelCli, data, data_size, FuriWaitForever);
-    furi_assert(tx_size == data_size);
+    furi_check(pipe_send(ble_per_cli_instance->own_pipe, data, data_size) == data_size);
 }
 
 bool ble_per_cli_parse_msg(FuriString* args, const char* suffix) {
@@ -172,16 +176,17 @@ static int32_t ble_per_cli_worker_thread(void* context) {
     while(1) {
         uint32_t events =
             furi_thread_flags_wait(BLE_PER_CLI_THREAD_EVENT, FuriFlagWaitAny, FuriWaitForever);
-
         if(events & BlePerCliThreadEventRxData) {
             uint8_t data[CLI_BUFFER_SIZE];
             const size_t rx_size =
                 furi_stream_buffer_receive(instance->rx_buffer, data, sizeof(data), 0);
             for(size_t i = 0; i < rx_size; i++) {
-                if(data[i] != '\n' && data[i] != '\r') {
+                if(data[i] == '\r') {
+                } else if(data[i] != '\n' && data[i] != 0x1B) {
                     furi_string_push_back(instance->rx_msg, data[i]);
                 } else {
-                    if(ble_per_cli_parse_msg(instance->rx_msg, ">: ")) {
+                    //FURI_LOG_I(TAG, "Parsed args: %s", furi_string_get_cstr(instance->rx_msg));
+                    if(ble_per_cli_parse_msg(instance->rx_msg, " ")) {
                         ble_per_test_update(
                             instance->app_handle,
                             ble_per_cli_stats[BlePerCliStatsCmdTypeTxDones].value,
@@ -195,11 +200,57 @@ static int32_t ble_per_cli_worker_thread(void* context) {
         }
 
         if(events & BlePerCliThreadEventStop) {
+            furi_event_loop_stop(instance->event_loop);
             break;
         }
     }
     FURI_LOG_I(TAG, "Stop");
 
+    return 0;
+}
+
+static void ble_per_cli_thread_event_loop_state_callback(
+    FuriThread* thread,
+    FuriThreadState state,
+    void* context) {
+    furi_assert(thread);
+    UNUSED(context);
+
+    if(state == FuriThreadStateStopped) {
+        furi_thread_free(thread);
+        FURI_LOG_D(TAG, "Stop event loop");
+    }
+}
+
+static int32_t ble_per_cli_thread_event_loop_callback(void* context) {
+    furi_assert(context);
+    CliCommandSlCli* instance = context;
+
+    instance->event_loop = furi_event_loop_alloc();
+    PipeSideBundle temp_bundle = pipe_alloc(CLI_BUFFER_SIZE, 1);
+    instance->own_pipe = temp_bundle.alices_side;
+    instance->shell_pipe = temp_bundle.bobs_side;
+
+    CliIntercom* cli_intercom = furi_record_open(RECORD_CLI_INTERCOM);
+    furi_check(cli_intercom_spawn(cli_intercom, instance->shell_pipe) == CliIntercomSpawnStatusOk);
+
+    pipe_attach_to_event_loop(instance->own_pipe, instance->event_loop);
+    pipe_set_callback_context(instance->own_pipe, instance);
+    pipe_set_data_arrived_callback(instance->own_pipe, cli_uart_data_from_pipe, 0);
+
+    FURI_LOG_D(TAG, "Start event loop");
+    furi_event_loop_run(instance->event_loop);
+    FURI_LOG_D(TAG, "Stopping event loop");
+
+    pipe_set_data_arrived_callback(instance->own_pipe, NULL, 0);
+    pipe_set_broken_callback(instance->own_pipe, NULL, 0);
+    pipe_detach_from_event_loop(instance->own_pipe);
+    pipe_free(instance->own_pipe);
+    cli_intercom_join(cli_intercom);
+    pipe_free(instance->shell_pipe);
+
+    furi_event_loop_free(ble_per_cli_instance->event_loop);
+    furi_record_close(RECORD_CLI_INTERCOM);
     return 0;
 }
 
@@ -219,19 +270,24 @@ bool ble_per_cli_start(BlePerTest* app_handle, BlePerCliSettings settings) {
     for(size_t i = 0; i < BlePerCliStatsCmdMax; i++) {
         ble_per_cli_stats[i].value = 0;
     }
-    ble_per_cli_instance->intercom = furi_record_open(RECORD_INTERCOM);
-
-    intercom_set_rx_callback(
-        ble_per_cli_instance->intercom,
-        IntercomChannelCli,
-        cli_command_917_rx_callback,
-        ble_per_cli_instance);
 
     ble_per_cli_instance->rx_buffer = furi_stream_buffer_alloc(CLI_BUFFER_SIZE, 1);
     ble_per_cli_instance->rx_msg = furi_string_alloc();
     ble_per_cli_instance->thread = furi_thread_alloc_ex(
         "BLE_CLI_Worker", CLI_BUFFER_SIZE * 2, ble_per_cli_worker_thread, ble_per_cli_instance);
     furi_thread_start(ble_per_cli_instance->thread);
+
+    // Start event loop thread
+    ble_per_cli_instance->thread_event_loop = furi_thread_alloc_ex(
+        "BLE_CLI_event_loop",
+        CLI_BUFFER_SIZE * 2,
+        ble_per_cli_thread_event_loop_callback,
+        ble_per_cli_instance);
+    furi_thread_set_state_callback(
+        ble_per_cli_instance->thread_event_loop, ble_per_cli_thread_event_loop_state_callback);
+    furi_thread_start(ble_per_cli_instance->thread_event_loop);
+
+    furi_delay_ms(100); // wait for the thread to start
 
     //Start App
     furi_string_printf(msg, "%s\r\n", ble_per_cli_cmd[BlePerCliCmdTypeStartApp].cmd);
@@ -304,17 +360,14 @@ void ble_per_cli_stop(void) {
     ble_per_cli_data_tx((uint8_t*)furi_string_get_cstr(msg), furi_string_utf8_length(msg));
     FURI_LOG_D(TAG, "%s", furi_string_get_cstr(msg));
 
-    furi_delay_ms(1000); //wait for the app to stop
+    furi_delay_ms(500); //wait for the app to stop
 
-    furi_string_printf(msg, "%c\r\n", 0x03); //Ctrl+C
+    furi_string_printf(msg, "%s\r\n", ble_per_cli_cmd[BlePerCliCmdTypeEndApp].cmd);
     ble_per_cli_data_tx((uint8_t*)furi_string_get_cstr(msg), furi_string_utf8_length(msg));
-    furi_delay_ms(1000); //wait for the app to stop
+    furi_delay_ms(500); //wait for the app to stop
     FURI_LOG_D(TAG, "End APP");
 
     furi_string_free(msg);
-
-    intercom_set_rx_callback(ble_per_cli_instance->intercom, IntercomChannelCli, NULL, NULL);
-    furi_record_close(RECORD_INTERCOM);
 
     furi_thread_flags_set(
         furi_thread_get_id(ble_per_cli_instance->thread), BlePerCliThreadEventStop);
