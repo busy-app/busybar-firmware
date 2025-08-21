18544251347bba8c5fcff831ac90a9545f32af9d
Anna Antonenko <portasynthinca3@gmail.com>
2025-08-21 23:25:00 +0400

    settings: list debug apps

--- applications/system/settings/scenes/settings_scenes.h
+++ applications/system/settings/scenes/settings_scenes.h
@@ -7,6 +7,7 @@ typedef enum {
     SettingsAppSceneIdMain,
     SettingsAppSceneIdSound,
     SettingsAppSceneIdBrightness,
+    SettingsAppSceneIdDebugApps,
 
     SettingsAppSceneIdsCount
 } SettingsAppSceneId;
