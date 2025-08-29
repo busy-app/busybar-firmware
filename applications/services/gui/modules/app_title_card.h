2f4fbb688aa5f27841325e0b1f0c9457e8ffec8b
Ivan Shestakov <vanyww2@gmail.com>
2025-08-29 14:59:58 +0400

    Rework gui app_title_card to implement animations interface

--- applications/services/gui/modules/app_title_card.h
+++ applications/services/gui/modules/app_title_card.h
@@ -8,7 +8,7 @@ extern "C" {
 
 typedef struct AppTitleCard AppTitleCard;
 
-AppTitleCard* app_title_card_alloc(Widget* parent);
+AppTitleCard* app_title_card_alloc(Widget* parent, bool use_anim_image);
 
 void app_title_card_free(AppTitleCard* instance);
 
@@ -16,7 +16,17 @@ Widget* app_title_card_get_base(AppTitleCard* instance);
 
 void app_title_card_set_image(AppTitleCard* instance, const char* file_path);
 
+void app_title_card_set_anim_image(AppTitleCard* instance, const char* file_path);
+
 void app_title_card_set_text(AppTitleCard* instance, const char* text);
+
+void app_title_card_run_image_anim(AppTitleCard* instance, uint32_t start, uint32_t stop);
+
+void app_title_card_run_text_anim(
+    AppTitleCard* instance,
+    int32_t start,
+    int32_t stop,
+    uint32_t duration);
 
 #ifdef __cplusplus
 }
