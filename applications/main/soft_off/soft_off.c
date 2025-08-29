8ef59c450bcafd990a88c21e34411e9fff8a988a
Ivan Shestakov <vanyww2@gmail.com>
2025-08-29 16:52:57 +0400

    Implement soft off animation

--- applications/main/soft_off/soft_off.c
+++ applications/main/soft_off/soft_off.c
@@ -1,4 +1,9 @@
+#include "soft_off/storage_macros.h"
+
 #include <furi.h>
+
+#include <gui/gui.h>
+#include <gui/modules/anim_image.h>
 
 #include <back_display/back_display.h>
 
@@ -20,6 +25,7 @@ static bool soft_off_signal_callback(uint32_t signal, void* arg, void* context) 
 int32_t soft_off_app(void* arg) {
     UNUSED(arg);
 
+    Gui* gui = furi_record_open(RECORD_GUI);
     BackDisplaySrv* back_display = furi_record_open(RECORD_BACK_DISPLAY);
 
     back_display_sleep_mode(back_display, true);
@@ -27,11 +33,25 @@ int32_t soft_off_app(void* arg) {
     FuriSemaphore* exit_semaphore = furi_semaphore_alloc(1, 0);
     furi_thread_set_signal_callback(
         furi_thread_get_current(), soft_off_signal_callback, exit_semaphore);
+
+    AnimImage* anim_image;
+    with_gui(gui, {
+        GuiLayer* layer = gui_get_layer(gui, GuiLayerIdMain);
+        Widget* root = gui_layer_get_root_widget(layer, GuiDisplayIdFront);
+
+        anim_image = anim_image_alloc(root);
+        anim_image_set_source(anim_image, SOFT_OFF_ANIM_PATH("turn_off_72x16.anim"));
+        anim_image_set_loop(anim_image, false);
+    });
+
     furi_check(furi_semaphore_acquire(exit_semaphore, FuriWaitForever) == FuriStatusOk);
+
+    with_gui(gui, { anim_image_free(anim_image); });
 
     back_display_sleep_mode(back_display, false);
 
     furi_record_close(RECORD_BACK_DISPLAY);
+    furi_record_close(RECORD_GUI);
 
     return 0;
 }
