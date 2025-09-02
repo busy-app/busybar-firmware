2dd63caa463ef7d21970e9902694181ef76e4d6a
Ivan Shestakov <vanyww2@gmail.com>
2025-09-02 19:05:58 +0400

    Lower settings standby animation delay on initial run

--- applications/system/settings/scenes/settings_scene_start.c
+++ applications/system/settings/scenes/settings_scene_start.c
@@ -19,6 +19,9 @@
 #define EXIT_TEXT_ANIM_END         -8
 #define EXIT_TEXT_ANIM_DURATION_MS 135
 
+#define STANDBY_ANIM_INITIAL_DELAY_MS 2000
+#define STANDBY_ANIM_DELAY_MS         5000
+
 typedef struct {
     AnimTitleCard* front_card;
     TitleCard* back_card;
@@ -26,6 +29,7 @@ typedef struct {
     FuriEventLoopTimer* timer;
 
     bool is_not_first_enter;
+    bool is_timer_initial_run;
 } SettingsSceneStart;
 
 static bool settings_scene_start_input_callback(const InputEvent* event, void* context) {
@@ -65,6 +69,10 @@ void settings_scene_start_timer_callback(void* context) {
     SettingsSceneStart* data = scene_manager_get_current_scene_data(instance->scene_manager);
 
     anim_title_card_run_background_anim(data->front_card);
+
+    if(data->is_timer_initial_run) {
+        furi_event_loop_timer_start(data->timer, STANDBY_ANIM_DELAY_MS);
+    }
 }
 
 static void settings_scene_start_on_enter(void* context) {
@@ -104,13 +112,14 @@ static void settings_scene_start_on_enter(void* context) {
         widget_set_visible(nav_bar_get_base(instance->back_nav_bar), false);
     });
 
+    data->is_timer_initial_run = true;
     data->timer = furi_event_loop_timer_alloc(
         instance->event_loop,
         settings_scene_start_timer_callback,
         FuriEventLoopTimerTypePeriodic,
         instance);
 
-    furi_event_loop_timer_start(data->timer, 5000);
+    furi_event_loop_timer_start(data->timer, STANDBY_ANIM_INITIAL_DELAY_MS);
 }
 
 static void settings_scene_start_on_exit(void* context) {
