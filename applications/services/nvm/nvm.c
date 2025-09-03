2bd5512906da10a917be174c7885d8a6fa3dbd46
Georgii Surkov <georgii.surkov@outlook.com>
2025-09-03 16:54:22 +0100

    Nvm: Add comments and more checks

--- applications/services/nvm/nvm.c
+++ applications/services/nvm/nvm.c
@@ -27,21 +27,29 @@ bool nvm_exists(Nvm* instance, uint32_t key, size_t* len) {
 
 bool nvm_read(Nvm* instance, uint32_t key, void* data, size_t len) {
     furi_check(instance);
+    furi_check(data);
+
     return nvm3_readData(nvm3_defaultHandle, key, data, len) == SL_STATUS_OK;
 }
 
 bool nvm_read_partial(Nvm* instance, uint32_t key, void* data, size_t offset, size_t len) {
     furi_check(instance);
+    furi_check(data);
+
     return nvm3_readPartialData(nvm3_defaultHandle, key, data, offset, len) == SL_STATUS_OK;
 }
 
 bool nvm_write(Nvm* instance, uint32_t key, const void* data, size_t len) {
     furi_check(instance);
+    furi_check(data);
+
     return nvm3_writeData(nvm3_defaultHandle, key, data, len) == SL_STATUS_OK;
 }
 
 bool nvm_read_counter(Nvm* instance, uint32_t key, uint32_t* value) {
     furi_check(instance);
+    furi_check(value);
+
     return nvm3_readCounter(nvm3_defaultHandle, key, value) == SL_STATUS_OK;
 }
 
@@ -81,6 +89,8 @@ void nvm_on_system_start(void) {
 
     furi_check(nvm3_initDefault() == SL_STATUS_OK);
     furi_record_create(RECORD_NVM, &nvm_instance);
+
+    FURI_LOG_I(TAG, "Init OK");
 }
 
 // Redefinitions of weak SDK functions
--- applications/services/nvm/nvm.h
+++ applications/services/nvm/nvm.h
@@ -1,3 +1,7 @@
+/**
+ * @file nvm.h
+ * @brief Non-volatile flash memory API.
+ */
 #pragma once
 
 #include <stddef.h>
@@ -8,39 +12,150 @@
 extern "C" {
 #endif
 
+/**
+ * @brief The string key for Nvm instance access.
+ *
+ * Get the instance pointer by calling `furi_record_open(RECORD_NVM)`
+ */
 #define RECORD_NVM "nvm"
 
+/**
+ * @brief Enumeration of available Nvm key ranges.
+ */
 typedef enum {
-    NvmKeyMin = 0xFFFFUL,
-    NvmKeyUser1Min,
+    NvmKeyMin = 0xFFFFUL, /**< Keys 0x0-0xFFFF (inclusive) are reserved for Matter */
+    NvmKeyUser1Min, /**< Free to use key range 1 start (inclusive) */
     /* Add more key ranges here */
-    NvmKeyMatterMin = 0x87200UL,
-    NvmKeyMatterMax = 0x87FFFUL,
-    NvmKeyUser2Min,
+    NvmKeyMatterMin = 0x87200UL, /**< Additional key range reserved for Matter (start) */
+    NvmKeyMatterMax = 0x87FFFUL, /**< Additional key range reserved for Matter (end, inclusive) */
+    NvmKeyUser2Min, /**< Free to use key range 2 start (inclusive) */
     /* Add more key ranges here */
-    NvmKeyMax = 0x1000000UL,
+    NvmKeyMax = 0x1000000UL, /**< Maximum key value (non-inclusive) */
 } NvmKey;
 
 typedef struct Nvm Nvm;
 
+/**
+ * @brief Check if a record exists under a given key and optionally get its length.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the record to be checked
+ * @param[out] len optional pointer to the variable to contain the record length (may be NULL)
+ *
+ * @returns true if the record exists, false otherwise
+ */
 bool nvm_exists(Nvm* instance, uint32_t key, size_t* len);
 
+/**
+ * @brief Read the record data under a given key.
+ *
+ * @note Value of @p len MUST match the data length given during writing,
+ *       otherwise an error will occur.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the record to be read
+ * @param[out] data pointer to the buffer to hold the data (must be large enough)
+ * @param[in] len length of the data to be read (see note), in bytes
+ *
+ * @returns true if the record could be read, false otherwise
+ */
 bool nvm_read(Nvm* instance, uint32_t key, void* data, size_t len);
 
+/**
+ * @brief Read the record data under a given key partially.
+ *
+ * @note Value of @p len - @p offset MUST match the data length given during writing,
+ *       otherwise an error will occur.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the record to be read
+ * @param[out] data pointer to the buffer to hold the data (must be large enough)
+ * @param[in] offset offset from the beginning of the data, in bytes
+ * @param[in] len length of the data to be read (see note), in bytes
+ *
+ * @returns true if the record could be read, false otherwise
+ */
 bool nvm_read_partial(Nvm* instance, uint32_t key, void* data, size_t offset, size_t len);
 
+/**
+ * @brief Write data to a record under a given key.
+ *
+ * @note If a record under the given key already exists,
+ *       it will be overwritten with the new data.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the record to be written
+ * @param[in] data pointer to the data to be written
+ * @param[in] len length of the data to be written
+ *
+ * @returns true if the record could be written, false otherwise
+ */
 bool nvm_write(Nvm* instance, uint32_t key, const void* data, size_t len);
 
+/**
+ * @brief Read the counter value under a given key.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the counter to be read
+ * @param[out] value pointer to the variable to hold the counter value
+ *
+ * @returns true if the counter could be read, false otherwise
+ */
 bool nvm_read_counter(Nvm* instance, uint32_t key, uint32_t* value);
 
+/**
+ * @brief Write the counter value under a given key.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the counter to be written
+ * @param[in] value counter value to be written
+ *
+ * @returns true if the counter could be written, false otherwise
+ */
 bool nvm_write_counter(Nvm* instance, uint32_t key, uint32_t value);
 
+/**
+ * @brief Increment the counter under a given key by 1 and optionally get its value.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the counter to be incremented
+ * @param[out] value optional pointer to the variable to hold the new counter value (may be NULL)
+ *
+ * @returns true if the counter could be incremented, false otherwise
+ */
 bool nvm_increment_counter(Nvm* instance, uint32_t key, uint32_t* value);
 
+/**
+ * @brief Delete the record under a given key.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ * @param[in] key key associated with the record to be deleted
+ *
+ * @returns true if the record could be deleted, false otherwise
+ */
 bool nvm_delete(Nvm* instance, uint32_t key);
 
+/**
+ * @brief Repack the storage to optimise space.
+ *
+ * @note The operation will only be performed if the driver
+ *       logic decides that it is needed.
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ *
+ * @returns true if the storage could be repacked, false otherwise
+ */
 bool nvm_repack(Nvm* instance);
 
+/**
+ * @brief Erase all data from the storage.
+ *
+ * @warning This will ERASE ALL THE DATA on the storage, use with caution
+ *
+ * @param[in,out] instance pointer to the Nvm instance
+ *
+ * @returns true if the storage could be erased, false otherwise.
+ */
 bool nvm_erase_all(Nvm* instance);
 
 #ifdef __cplusplus
