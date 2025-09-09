950e7efea33865f5ae62e2aee21b2145386bccf5
Anna Antonenko <portasynthinca3@gmail.com>
2025-09-09 19:16:50 +0400

    matter: added service to f20, intercom between services

---
+++ applications/services/matter/matter_common_i.h
@@ -0,0 +1,60 @@
+/**
+ * @file matter_common_i.h
+ * Intercom protocol definitions.
+ */
+
+#pragma once
+
+#include <furi.h>
+#include "matter_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ===============
+// Specific frames
+// ===============
+
+/**
+ * @brief Notification about updated state
+ */
+typedef struct {
+    MatterVirtualDeviceState new_state;
+} MatterIntercomUpdateFrame;
+
+/**
+ * @brief Request to change state
+ */
+typedef struct {
+    MatterVirtualDeviceState req_state;
+} MatterIntercomRequestFrame;
+
+/**
+ * @brief Request to wipe all Matter-related data (factory reset)
+ */
+typedef struct {
+} MatterIntercomResetFrame;
+
+// =============
+// Generic frame
+// =============
+
+typedef enum {
+    MatterIntercomFrameTypeUpdate, //<! Notification about updated state. Direction: f64->f20
+    MatterIntercomFrameTypeRequest, //<! Request to change state. Direction: f20->f64
+    MatterIntercomFrameTypeReset, //<! Factory reset. Direction: f20->f64
+} MatterIntercomFrameType;
+
+typedef struct {
+    MatterIntercomFrameType type;
+    union {
+        MatterIntercomUpdateFrame update;
+        MatterIntercomRequestFrame request;
+        MatterIntercomResetFrame reset;
+    };
+} MatterIntercomFrame;
+
+#ifdef __cplusplus
+}
+#endif
