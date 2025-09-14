e91a13f127d73121a3d9f4d9a00092931b480da0
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-09-14 03:47:29 +0300

    feat: Preview status bar, circular progress

--- assets/frontend/components/DefaultLayout/Preview.vue
+++ assets/frontend/components/DefaultLayout/Preview.vue
@@ -2,15 +2,34 @@
   <div class="w-full flex flex-col items-center">
     <ScreenStream class="py-6" />
 
-    <div class="text-muted">
-      Firmware Version
-      <span class="text-neutral-800 dark:text-neutral-100 ml-2">
-        {{ deviceStore.version.version === 'unknown' ? deviceStore.version.commit_hash : deviceStore.version.version }}
-      </span>
+    <div class="flex items-center justify-center gap-6">
+      <UpdateButton />
+
+      <div class="flex items-center gap-1.5">
+        <div class="relative flex">
+          <UIcon
+            name="i-busy-battery-charging"
+            class="size-6"
+          />
+          <UIcon
+            name="i-busy-charging-lightning"
+            class="absolute size-6"
+          />
+        </div>
+        <div>95%</div>
+      </div>
+
+      <div class="flex items-center gap-1.5">
+        <CircularProgress
+          :model-value="3.2 / 8 * 100"
+          color="#6A7282"
+          track-color="#99A1AF26"
+        />
+        <div>3.2 / 8 GB</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const deviceStore = useDeviceStore();
 </script>
---
+++ assets/frontend/components/UpdateButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <UButton
+    variant="subtle"
+    color="neutral"
+    :ui="{
+      base: 'flex items-center justify-center gap-2 ring-1 ring-glass rounded-full pl-3 pr-2 py-1'
+    }"
+    @click="tabStore.currentTab = 'firmware'"
+  >
+    <div class="relative size-2.5 rounded-full bg-green-500" />
+    <div class="flex items-center">
+      Update available
+      <div class="size-6 flex items-center justify-end">
+        <UIcon
+          name="i-tabler-chevron-right"
+          class="size-4"
+        />
+      </div>
+    </div>
+  </UButton>
+</template>
+
+<script setup lang="ts">
+const tabStore = useTabStore();
+</script>
