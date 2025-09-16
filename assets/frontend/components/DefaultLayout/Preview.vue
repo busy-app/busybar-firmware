154c48e862ee920c8e3987442458554f5b309033
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-09-16 06:00:02 +0300

    feat: Add battery info

--- assets/frontend/components/DefaultLayout/Preview.vue
+++ assets/frontend/components/DefaultLayout/Preview.vue
@@ -6,18 +6,22 @@
       <div class="w-fit flex items-center justify-center gap-6 px-4">
         <UpdateButton />
 
-        <div class="flex items-center gap-1.5">
+        <div
+          v-if="power"
+          class="flex items-center gap-1.5"
+        >
           <div class="relative flex">
             <UIcon
-              name="i-busy-battery-charging"
+              :name="power?.state === 'charging' ? 'i-busy-battery-charging' : batteryIcon()"
               class="size-6"
             />
             <UIcon
+              v-if="power?.state === 'charging'"
               name="i-busy-charging-lightning"
               class="absolute size-6"
             />
           </div>
-          <div>95%</div>
+          <div>{{ power?.battery_charge }}%</div>
         </div>
 
         <div class="flex items-center gap-1.5">
@@ -34,4 +38,22 @@
 </template>
 
 <script setup lang="ts">
+const deviceStore = useDeviceStore();
+
+const power = computed(() => deviceStore.deviceStatus?.power);
+
+function batteryIcon (): string {
+  const charge = power.value?.battery_charge || 0;
+  if (charge >= 75) {
+    return 'i-ri-battery-fill';
+  }
+  if (charge >= 30) {
+    return 'i-ri-battery-low-line';
+  }
+  return 'i-ri-battery-line';
+}
+
+onMounted(async () => {
+  await deviceStore.getDeviceStatus();
+});
 </script>
