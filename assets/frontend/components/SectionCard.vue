7b4f93929af6240a0e534de40c8a6089cc06d3fe
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-09-16 03:59:02 +0300

    fix: Ensure empty slots don't show up

--- assets/frontend/components/SectionCard.vue
+++ assets/frontend/components/SectionCard.vue
@@ -75,6 +75,11 @@ const props = defineProps<{
   icon?: string;
 }>();
 
-const headerExists = !!(props.title || props.subtitle || props.icon || useSlots().subtitle || useSlots().actions);
-const bodyExists = !!(useSlots().default || useSlots()['raw-body']);
+const headerExists = !!(props.title || props.subtitle || props.icon || isEmptySlot('subtitle') || isEmptySlot('leading-actions') || isEmptySlot('actions'));
+const bodyExists = computed(() => isEmptySlot('default') === false || isEmptySlot('raw-body') === false);
+
+function isEmptySlot (slotName: string): boolean {
+  const slot = useSlots()[slotName]?.();
+  return !(slot && slot.length && slot.some(vnode => vnode.children && vnode.children !== 'v-if' && vnode.children.length));
+}
 </script>
