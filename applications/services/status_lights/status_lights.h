28b1c9872dd3c20f35f5213acc106d9e344173cf
Ivan Shestakov <vanyww2@gmail.com>
2025-09-19 14:32:26 +0400

    Implement missing status lights documentation

--- applications/services/status_lights/status_lights.h
+++ applications/services/status_lights/status_lights.h
@@ -21,9 +21,31 @@ extern "C" {
 #define STATUS_LIGHTS_BRIGHTNESS_MAX  (100)
 #define STATUS_LIGHTS_BRIGHTNESS_AUTO (255)
 
+/**
+ * @brief Run a preset animation with the specified color.
+ *
+ * @param instance status lights service instance
+ * @param preset the preset animation to execute
+ * @param color the color to use for the animation
+ */
 void status_lights_run_preset(StatusLights* instance, StatusLightsPreset preset, Color color);
 
+/**
+ * @brief Set the status lights brightness
+ *
+ * @param instance Pointer to the StatusLights instance
+ * @param brightness Brightness value (STATUS_LIGHTS_BRIGHTNESS_MIN to STATUS_LIGHTS_BRIGHTNESS_MAX),
+ * or STATUS_LIGHTS_BRIGHTNESS_AUTO to enable automatic brightness
+ */
 void status_lights_set_brightness(StatusLights* instance, uint8_t brightness);
+
+/**
+ * @brief Get the status lights brightness
+ *
+ * @param instance Pointer to the StatusLights instance
+ * @return Brightness value (STATUS_LIGHTS_BRIGHTNESS_MIN to STATUS_LIGHTS_BRIGHTNESS_MAX),
+ * or STATUS_LIGHTS_BRIGHTNESS_AUTO if automatic brightness is enabled
+ */
 uint8_t status_lights_get_brightness(StatusLights* instance);
 
 #ifdef __cplusplus
