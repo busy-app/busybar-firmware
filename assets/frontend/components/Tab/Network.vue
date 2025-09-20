15aff884d6595e7d2a304e1b1cd7b4da7a6d3b08
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-09-21 01:01:36 +0300

    feat: Add http api control flow

--- assets/frontend/components/Tab/Network.vue
+++ assets/frontend/components/Tab/Network.vue
@@ -320,6 +320,7 @@
   </ModalGeneric>
 
   <SectionCard
+    v-if="deviceStore.httpAPIAccess"
     title="HTTP API"
     icon="i-ri-exchange-2-line"
   >
@@ -328,8 +329,10 @@
       <UButton
         variant="link"
         class="px-0"
+        href="http://10.0.4.20/docs"
+        target="_blank"
       >
-        <span class="underline">http://10.04.20/docs</span>
+        <span class="underline">http://10.0.4.20/docs</span>
         <UIcon
           name="i-ri-external-link-line"
           class="size-4"
@@ -337,8 +340,162 @@
       </UButton>
     </div>
     <div class="flex flex-col gap-1">
-      <div>Over Wi-Fi</div>
-      <div class="text-sm text-muted">Connect to a Wi-Fi network to enable access to HTTP API over Wi-Fi</div>
+      <div class="flex justify-between items-center">
+        <div>Over Wi-Fi</div>
+        <UButton
+          v-if="connected"
+          variant="link"
+          class="px-0"
+          :href="`http://${wifiStore.wifi?.ip_config?.address}/docs`"
+          target="_blank"
+        >
+          <span class="underline">http://{{ wifiStore.wifi?.ip_config?.address }}/docs</span>
+          <UIcon
+            name="i-ri-external-link-line"
+            class="size-4"
+          />
+        </UButton>
+      </div>
+      <div
+        v-if="!connected"
+        class="text-sm text-muted"
+      >
+        Connect to a Wi-Fi network to enable access to HTTP API over Wi-Fi
+      </div>
+      <template v-else>
+        <div class="flex justify-between items-center mt-3">
+          <div class="flex flex-col gap-1">
+            <div>HTTP API access</div>
+            <div class="text-sm text-muted">Send requests and control BUSY Bar when connected via Wi-Fi</div>
+          </div>
+
+          <USwitch
+            v-model="httpApiSwitchModel"
+            :loading="loading.access"
+            @update:model-value="handleHttpApiToggle"
+          />
+        </div>
+
+        <div
+          v-if="deviceStore.httpAPIAccess.mode !== 'disabled'"
+          class="flex justify-between items-center gap-y-4 gap-x-6 mt-3"
+        >
+          <div class="flex flex-col gap-1">
+            <div class="flex items-center gap-2">
+              <div>Security</div>
+              <UBadge
+                v-if="deviceStore.httpAPIAccess.mode === 'key'"
+                icon="i-ri-lock-fill"
+                label="Secure"
+                color="success"
+                size="sm"
+                class="rounded-full"
+              />
+              <UBadge
+                v-else-if="deviceStore.httpAPIAccess.mode === 'enabled'"
+                icon="i-ri-alert-fill"
+                label="Insecure"
+                color="warning"
+                size="sm"
+                class="rounded-full"
+              />
+            </div>
+            <div
+              v-if="deviceStore.httpAPIAccess.mode === 'key'"
+              class="text-sm text-muted"
+            >
+              A password will be asked each time this page is opened with a BUSY Bar connected via Wi-Fi
+            </div>
+            <div
+              v-else-if="deviceStore.httpAPIAccess.mode === 'enabled'"
+              class="text-sm text-muted"
+            >
+              Anyone on the same Wi-Fi network will be able to access the device via this page. We recommend setting a password that will be asked each time this page is opened with a BUSY Bar connected via Wi-Fi.
+            </div>
+          </div>
+
+          <UDropdownMenu
+            v-if="deviceStore.httpAPIAccess.mode === 'key'"
+            :items="[
+              {
+                label: 'Change',
+                icon: 'i-ri-pencil-line',
+                onSelect: () => {
+                  pms.passwordModel.current = '';
+                  pms.passwordModel.currentWrong = false;
+                  pms.passwordModel.new = '';
+                  pms.showUpdatePasswordModal = true;
+                }
+              },
+              {
+                label: 'Remove',
+                icon: 'i-ri-lock-unlock-line',
+                onSelect: () => {
+                  pms.passwordModel.current = '';
+                  pms.passwordModel.currentWrong = false;
+                  pms.showRemovePasswordModal = true;
+                }
+              }
+            ]"
+            :content="{
+              align: 'end',
+              side: 'bottom',
+              sideOffset: 8
+            }"
+            :ui="{
+            }"
+          >
+            <UButton
+              icon="i-ri-more-fill"
+              variant="ghost"
+              color="neutral"
+              square
+            />
+          </UDropdownMenu>
+
+          <UButton
+            v-else
+            label="Set password"
+            variant="soft"
+            color="primary"
+            @click="() => {
+              pms.passwordModel.current = '';
+              pms.passwordModel.currentWrong = false;
+              pms.passwordModel.new = '';
+              pms.showSetPasswordModal = true;
+            }"
+          />
+        </div>
+      </template>
+
+      <ModalGeneric
+        v-model:open="showEnableHttpApiModal"
+        title="Enable access to HTTP API?"
+        description="Anyone on the same Wi-Fi network will be able to access the device via this page. We recommend setting a password that will be asked each time you open this page with a BUSY Bar connected via Wi-Fi."
+        show-close-button
+        wide
+        :primary-action-props="{
+          label: 'Set password and enable',
+          loading: loading.access,
+          onClick: async () => {
+            showEnableHttpApiModal = false;
+            pms.passwordModel.current = '';
+            pms.passwordModel.currentWrong = false;
+            pms.passwordModel.new = '';
+            pms.showSetPasswordModal = true;
+          }
+        }"
+        :secondary-action-props="{
+          label: 'Continue without password',
+          loading: loading.access,
+          onClick: async () => {
+            loading.access = true;
+            await deviceStore.setHttpAPIAccess('enabled');
+            loading.access = false;
+            showEnableHttpApiModal = false;
+          }
+        }"
+      />
     </div>
   </SectionCard>
 </template>
@@ -347,13 +504,16 @@
 import { vMaska } from 'maska/vue';
 
 const wifiStore = useWifiStore();
+const deviceStore = useDeviceStore();
+const pms = usePasswordModalStore();
 
 const loading = ref({
   state: false,
   enable: false,
   list: false,
   connect: false,
-  forget: false
+  forget: false,
+  access: false
 });
 
 async function refreshWifiState () {
@@ -445,6 +605,35 @@ function wifiIconByRssi (rssi: number): string {
     return 'i-ri-signal-wifi-2-fill';
   }
   return 'i-ri-signal-wifi-1-fill';
+}
+
+watch(() => deviceStore.httpAPIAccess, access => {
+  if (!access) {
+    return;
+  }
+  if (access.mode === 'key' || access.mode === 'enabled') {
+    httpApiSwitchModel.value = true;
+  } else {
+    httpApiSwitchModel.value = false;
+  }
+}, { immediate: true, deep: true });
+
+const httpApiSwitchModel = ref(false);
+const showEnableHttpApiModal = ref(false);
+
+async function handleHttpApiToggle (value: boolean) {
+  if (!value) {
+    loading.value.access = true;
+    await deviceStore.setHttpAPIAccess('disabled');
+    loading.value.access = false;
+  } else {
+    if (deviceStore.httpAPIAccess?.mode === 'key') {
+      httpApiSwitchModel.value = true;
+    } else {
+      showEnableHttpApiModal.value = true;
+      httpApiSwitchModel.value = false;
+    }
+  }
 }
 
 onMounted(async () => {
