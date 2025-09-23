f5382e268614d91789f3ed9acdf017b04c2bb187
Ivan Shestakov <vanyww2@gmail.com>
2025-09-23 12:27:21 +0400

    Implement input f64 side interface: input_get_absolute_state, event pubsub

--- applications/services/input/input_f64.c
+++ applications/services/input/input_f64.c
@@ -1,8 +1,9 @@
-#include <furi.h>
+#include "input_f64.h"
 
+#include <furi.h>
 #include <furi_hal_qei.h>
 #include <furi_hal_resources.h>
-#include "input_common_i.h"
+#include <toolbox/api_lock.h>
 
 #ifdef SRV_INTERCOM
 #include <intercom/intercom.h>
@@ -13,6 +14,7 @@
 #define INPUT_DEBOUNCE_TIMEOUT 2
 #define INPUT_DEBOUNCE_TICKS   10
 #define INPUT_QUEUE_SIZE       32
+#define REQUEST_QUEUE_SIZE     4
 
 #ifdef INPUT_DEBUG
 #define INPUT_LOG(...) FURI_LOG_D(TAG, __VA_ARGS__)
@@ -30,7 +32,20 @@ typedef struct {
     bool level;
 } InputKeyState;
 
+typedef enum {
+    InputRequestTypeGetAbsState,
+} InputRequestType;
+
 typedef struct {
+    InputRequestType type;
+    FuriApiLock lock;
+    union {
+        InputAbsoluteState* abs_state;
+    };
+} InputRequest;
+
+struct Input {
+    FuriPubSub* event_pubsub;
     FuriEventLoop* event_loop;
     FuriMessageQueue* input_queue;
     FuriEventLoopTimer* debounce_timer;
@@ -38,16 +53,18 @@ typedef struct {
 #ifdef SRV_INTERCOM
     Intercom* intercom;
 #endif
-} InputSrv;
+    FuriMessageQueue* request_queue;
+    InputAbsoluteState absolute_state;
+};
 
 static void input_isr_key(void* context) {
-    InputSrv* instance = context;
+    Input* instance = context;
     furi_event_loop_set_custom_event(instance->event_loop, InputEventFlagActivity);
 }
 
 static void input_custom_event_callback(uint32_t events, void* context) {
     furi_assert(context);
-    InputSrv* instance = context;
+    Input* instance = context;
 
     if(events & InputEventFlagActivity) {
         if(!furi_event_loop_timer_is_running(instance->debounce_timer)) {
@@ -56,19 +73,48 @@ static void input_custom_event_callback(uint32_t events, void* context) {
     }
 }
 
-static void input_send(InputSrv* instance, const InputPin* pin, InputAction input_action) {
+static void input_request_callback(FuriEventLoopObject* object, void* context) {
+    UNUSED(object);
+    furi_assert(context);
+
+    Input* instance = context;
+
+    InputRequest request;
+    furi_check(furi_message_queue_get(instance->request_queue, &request, 0) == FuriStatusOk);
+
+    switch(request.type) {
+    case InputRequestTypeGetAbsState:
+        *request.abs_state = instance->absolute_state;
+        break;
+    default:
+        furi_crash();
+    }
+
+    if(request.lock) {
+        api_lock_unlock(request.lock);
+    }
+}
+
+static void input_send(Input* instance, const InputPin* pin, InputAction input_action) {
     InputCommonEvent event;
     event.device = pin->device;
 
     if(pin->device == InputDeviceSwitch) {
         if(input_action == InputActionPress) {
             event.switch_position = pin->pos;
+            instance->absolute_state.switch_position = pin->pos;
             furi_check(furi_message_queue_put(instance->input_queue, &event, 0) == FuriStatusOk);
         }
-
     } else {
         event.button_event.button = pin->button;
         event.button_event.action = input_action;
+
+        if(input_action == InputActionPress) {
+            instance->absolute_state.buttons |= (1 << pin->button);
+        } else {
+            instance->absolute_state.buttons &= ~(1 << pin->button);
+        }
+
         furi_check(furi_message_queue_put(instance->input_queue, &event, 0) == FuriStatusOk);
     }
 }
@@ -80,7 +126,7 @@ static FURI_ALWAYS_INLINE bool input_get_pin_level(const InputPin* input_pin) {
 static void input_debounce_timer_callback(void* context) {
     furi_assert(context);
 
-    InputSrv* instance = context;
+    Input* instance = context;
     bool is_changing = false;
 
     for(size_t i = 0; i < input_pins_count; i++) {
@@ -111,7 +157,7 @@ static void input_debounce_timer_callback(void* context) {
 }
 
 static void input_qei_callback(int16_t delta_pos, void* context) {
-    InputSrv* instance = context;
+    Input* instance = context;
 
     InputCommonEvent event = {
         .device = InputDeviceEncoder,
@@ -123,11 +169,13 @@ static void input_qei_callback(int16_t delta_pos, void* context) {
 
 static void input_queue_callback(FuriEventLoopObject* object, void* context) {
     furi_assert(context);
-    InputSrv* instance = context;
+    Input* instance = context;
     furi_assert(object == instance->input_queue);
 
     InputCommonEvent event;
     furi_check(furi_message_queue_get(instance->input_queue, &event, 0) == FuriStatusOk);
+
+    furi_pubsub_publish(instance->event_pubsub, &event);
 
 #ifdef INPUT_DEBUG
 
@@ -171,8 +219,9 @@ int32_t input_srv(void* p) {
 
     INPUT_LOG("Starting");
 
-    InputSrv* instance = malloc(sizeof(InputSrv));
+    Input* instance = malloc(sizeof(Input));
 
+    instance->event_pubsub = furi_pubsub_alloc();
     instance->input_queue = furi_message_queue_alloc(INPUT_QUEUE_SIZE, sizeof(InputCommonEvent));
     instance->event_loop = furi_event_loop_alloc();
     instance->debounce_timer = furi_event_loop_timer_alloc(
@@ -180,11 +229,11 @@ int32_t input_srv(void* p) {
         input_debounce_timer_callback,
         FuriEventLoopTimerTypePeriodic,
         instance);
-
     instance->key_states = malloc(sizeof(InputKeyState) * input_pins_count);
 #ifdef SRV_INTERCOM
     instance->intercom = furi_record_open(RECORD_INTERCOM);
 #endif
+    instance->request_queue = furi_message_queue_alloc(REQUEST_QUEUE_SIZE, sizeof(InputRequest));
 
     for(size_t i = 0; i < input_pins_count; i++) {
         const InputPin* pin = &input_pins[i];
@@ -208,6 +257,15 @@ int32_t input_srv(void* p) {
         FuriEventLoopEventIn,
         input_queue_callback,
         instance);
+    furi_event_loop_subscribe_message_queue(
+        instance->event_loop,
+        instance->request_queue,
+        FuriEventLoopEventIn,
+        input_request_callback,
+        instance);
+
+    furi_record_create(RECORD_INPUT, instance);
+    furi_record_create(RECORD_INPUT_EVENTS, instance->event_pubsub);
 
     furi_hal_qei_init();
     furi_hal_qei_set_delta_pos_callback(input_qei_callback, instance);
@@ -216,4 +274,22 @@ int32_t input_srv(void* p) {
     furi_event_loop_run(instance->event_loop);
 
     return 0;
+}
+
+InputAbsoluteState input_get_absolute_state(Input* instance) {
+    furi_assert(instance);
+
+    InputAbsoluteState abs_state;
+    InputRequest request = {
+        .type = InputRequestTypeGetAbsState,
+        .lock = api_lock_alloc_locked(),
+        .abs_state = &abs_state,
+    };
+
+    furi_check(
+        furi_message_queue_put(instance->request_queue, &request, FuriWaitForever) ==
+        FuriStatusOk);
+    api_lock_wait_unlock_and_free(request.lock);
+
+    return *request.abs_state;
 }
