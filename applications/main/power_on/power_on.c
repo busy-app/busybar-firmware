d6b3f03df2e96c4a6b991ddf581af1635d624915
RebornedBrain <RebornedBrain@gmail.com>
2025-09-25 17:07:47 +0300

    Hello renamed to power_on

--- applications/main/power_on/power_on.c
+++ applications/main/power_on/power_on.c
@@ -9,23 +9,23 @@
 #include <front_display/front_display.h>
 #include <power/power_service/power.h>
 
-#define HELLO_APP_TIMEOUT_MIN (15)
+#define POWER_ON_APP_TIMEOUT_MIN (15)
 
 #define MIN_TO_MS(minutes) (minutes * 60U * 1000U)
 
-#define HELLO_ANIMATION_LOOP_START_FRAME (90)
-#define HELLO_ANIMATION_LOOP_END_FRAME   (630)
+#define POWER_ON_ANIMATION_LOOP_START_FRAME (90)
+#define POWER_ON_ANIMATION_LOOP_END_FRAME   (630)
 
-#define HELLO_ASSETS_PATH(path) EXT_PATH("apps_assets/power_on") "/" path
-#define HELLO_ANIM_PATH(path)   HELLO_ASSETS_PATH("animations") "/" path
-#define HELLO_DONE_PATH         HELLO_ASSETS_PATH("done.txt")
+#define POWER_ON_ANIM_PATH(path) APP_ASSETS_PATH("animations") "/" path
+#define POWER_ON_DONE_PATH       APP_DATA_PATH("done.txt")
 
 typedef enum {
-    HelloAppThreadFlagExitToMenu = 1 << 0,
-    HelloAppThreadFlagExitToTransportMode = 1 << 1,
-} HelloAppThreadFlag;
+    PowerOnAppThreadFlagExitToMenu = 1 << 0,
+    PowerOnAppThreadFlagExitToTransportMode = 1 << 1,
+} PowerOnAppThreadFlag;
 
-#define HELLO_APP_FLAGS (HelloAppThreadFlagExitToMenu | HelloAppThreadFlagExitToTransportMode)
+#define POWER_ON_APP_FLAGS \
+    (PowerOnAppThreadFlagExitToMenu | PowerOnAppThreadFlagExitToTransportMode)
 
 typedef struct {
     Gui* gui;
@@ -37,21 +37,21 @@ typedef struct {
 
     FuriThread* thread;
     FuriTimer* back_to_transport_timer;
-} HelloApp;
+} PowerOnApp;
 
-static bool hello_input_callback(const InputEvent* event, void* context) {
+static bool power_on_input_callback(const InputEvent* event, void* context) {
     furi_assert(event);
     furi_assert(context);
-    HelloApp* instance = context;
+    PowerOnApp* instance = context;
 
     bool consumed = false;
     if(event->type == InputTypeShort) {
         if(event->key == InputKeyStart)
-            furi_thread_flags_set(instance->thread, HelloAppThreadFlagExitToMenu);
+            furi_thread_flags_set(instance->thread, PowerOnAppThreadFlagExitToMenu);
         consumed = true;
     } else if(event->type == InputTypeLong) {
         if(event->key == InputKeyBack)
-            furi_thread_flags_set(instance->thread, HelloAppThreadFlagExitToTransportMode);
+            furi_thread_flags_set(instance->thread, PowerOnAppThreadFlagExitToTransportMode);
         consumed = true;
     }
 
@@ -59,12 +59,12 @@ static bool hello_input_callback(const InputEvent* event, void* context) {
 }
 
 static void back_to_transport_timer_callback(void* ctx) {
-    HelloApp* instance = ctx;
-    furi_thread_flags_set(instance->thread, HelloAppThreadFlagExitToTransportMode);
+    PowerOnApp* instance = ctx;
+    furi_thread_flags_set(instance->thread, PowerOnAppThreadFlagExitToTransportMode);
 }
 
-static HelloApp* hello_app_alloc() {
-    HelloApp* instance = malloc(sizeof(HelloApp));
+static PowerOnApp* power_on_app_alloc() {
+    PowerOnApp* instance = malloc(sizeof(PowerOnApp));
 
     instance->gui = furi_record_open(RECORD_GUI);
     instance->front_display = furi_record_open(RECORD_FRONT_DISPLAY);
@@ -77,11 +77,11 @@ static HelloApp* hello_app_alloc() {
     instance->back_to_transport_timer =
         furi_timer_alloc(back_to_transport_timer_callback, FuriTimerTypeOnce, instance);
     furi_timer_start(
-        instance->back_to_transport_timer, furi_ms_to_ticks(MIN_TO_MS(HELLO_APP_TIMEOUT_MIN)));
+        instance->back_to_transport_timer, furi_ms_to_ticks(MIN_TO_MS(POWER_ON_APP_TIMEOUT_MIN)));
     return instance;
 }
 
-static void hello_app_free(HelloApp* instance) {
+static void power_on_app_free(PowerOnApp* instance) {
     furi_record_close(RECORD_STORAGE);
     furi_record_close(RECORD_POWER);
     furi_record_close(RECORD_INPUT);
@@ -93,42 +93,46 @@ static void hello_app_free(HelloApp* instance) {
     free(instance);
 }
 
-int32_t hello_app(void* arg) {
+int32_t power_on_app(void* arg) {
     UNUSED(arg);
 
-    HelloApp* instance = hello_app_alloc();
+    PowerOnApp* instance = power_on_app_alloc();
 
     do {
-        if(storage_file_exists(instance->storage, HELLO_DONE_PATH)) break;
+        if(storage_file_exists(instance->storage, POWER_ON_DONE_PATH)) break;
 
         back_display_sleep_mode(instance->back_display, true);
 
         AnimImage* anim_image;
         with_gui(instance->gui, {
             GuiLayer* layer = gui_get_layer(instance->gui, GuiLayerIdMain);
-            gui_layer_add_input_callback(layer, hello_input_callback, instance);
+            gui_layer_add_input_callback(layer, power_on_input_callback, instance);
             Widget* root = gui_layer_get_root_widget(layer, GuiDisplayIdFront);
 
             anim_image = anim_image_alloc(root);
-            anim_image_set_source(anim_image, HELLO_ANIM_PATH("power_on_72x16.anim"));
+            anim_image_set_source(anim_image, POWER_ON_ANIM_PATH("power_on_72x16.anim"));
             anim_image_set_range(
                 anim_image,
-                HELLO_ANIMATION_LOOP_START_FRAME,
-                HELLO_ANIMATION_LOOP_END_FRAME,
+                POWER_ON_ANIMATION_LOOP_START_FRAME,
+                POWER_ON_ANIMATION_LOOP_END_FRAME,
                 true,
                 true);
         });
 
-        uint32_t flags = furi_thread_flags_wait(HELLO_APP_FLAGS, FuriFlagWaitAny, FuriWaitForever);
-        if(flags & HelloAppThreadFlagExitToTransportMode) {
+        uint32_t flags =
+            furi_thread_flags_wait(POWER_ON_APP_FLAGS, FuriFlagWaitAny, FuriWaitForever);
+        if(flags & PowerOnAppThreadFlagExitToTransportMode) {
             power_off(instance->power);
         }
 
-        if(flags & HelloAppThreadFlagExitToMenu) {
+        if(flags & PowerOnAppThreadFlagExitToMenu) {
             furi_timer_stop(instance->back_to_transport_timer);
 
             File* file = storage_file_alloc(instance->storage);
-            storage_file_open(file, HELLO_DONE_PATH, FSAM_WRITE, FSOM_CREATE_ALWAYS);
+
+            if(!storage_file_open(file, POWER_ON_DONE_PATH, FSAM_WRITE, FSOM_CREATE_ALWAYS))
+                FURI_LOG_W("Power on", "Failed to create file");
+
             storage_file_close(file);
             storage_file_free(file);
         }
@@ -136,12 +140,12 @@ int32_t hello_app(void* arg) {
         with_gui(instance->gui, {
             anim_image_free(anim_image);
             GuiLayer* layer = gui_get_layer(instance->gui, GuiLayerIdMain);
-            gui_layer_remove_input_callback(layer, hello_input_callback);
+            gui_layer_remove_input_callback(layer, power_on_input_callback);
         });
 
         back_display_sleep_mode(instance->back_display, false);
     } while(false);
 
-    hello_app_free(instance);
+    power_on_app_free(instance);
     return 0;
 }
