9b345735d7040c7b78be590bb9addcc5f9bbbaf0
Anna Antonenko <portasynthinca3@gmail.com>
2025-09-29 17:09:10 +0400

    custom: matter integration

--- applications/main/custom/scenes/custom_scene_timer.c
+++ applications/main/custom/scenes/custom_scene_timer.c
@@ -53,6 +53,7 @@ static void custom_scene_timer_on_enter(void* context) {
     });
 
     custom_set_status_lights(instance, CustomStatusLightsTypeWork);
+    custom_set_matter(instance, true);
     timer_indicator_set_state(data->timer_indicator, TimerIndicatorStateWorkBig);
 
     custom_start_transition(instance);
@@ -64,6 +65,7 @@ static void custom_scene_timer_on_exit(void* context) {
     CustomApp* instance = context;
 
     custom_set_status_lights(instance, CustomStatusLightsTypeOff);
+    custom_set_matter(instance, false);
 
     CustomSceneTimer* data = scene_manager_get_current_scene_data(instance->scene_manager);
 
