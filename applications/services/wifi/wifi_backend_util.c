82d073a35b4e7b05672def268de065db075a6160
Georgii Surkov <georgii.surkov@outlook.com>
2025-10-22 19:25:38 +0100

    Wifi: remove redundant code

--- applications/services/wifi/wifi_backend_util.c
+++ applications/services/wifi/wifi_backend_util.c
@@ -71,20 +71,6 @@ sl_wifi_security_t wifi_encode_security_mode(WifiSecurityMode security_mode) {
     return ret;
 }
 
-sl_ip_address_type_t wifi_encode_ip_version(WifiIpType type) {
-    sl_ip_address_type_t ret;
-
-    if(type == WifiIpTypeV4) {
-        ret = SL_IPV4;
-    } else if(type == WifiIpTypeV6) {
-        ret = SL_IPV6;
-    } else {
-        furi_crash("Invalid WifiIpAddressType value");
-    }
-
-    return ret;
-}
-
 void wifi_encode_ssid(sl_wifi_ssid_t* sl_ssid, const char* ssid) {
     char* sl_ssid_str = (char*)sl_ssid->value;
     const size_t sl_ssid_capacity = sizeof(sl_ssid->value);
@@ -125,55 +111,4 @@ WifiSecurityMode wifi_decode_security_mode(sl_wifi_security_t sl_security) {
 void wifi_decode_ssid(char* ssid, const sl_wifi_ssid_t* sl_ssid) {
     strncpy(ssid, (char*)sl_ssid->value, sl_ssid->length);
     ssid[sl_ssid->length] = '\0';
-}
-
-static WifiIpManagement wifi_decode_ip_management(sl_ip_management_t sl_mgmt) {
-    WifiIpManagement ret;
-
-    if(sl_mgmt == SL_IP_MANAGEMENT_STATIC_IP) {
-        ret = WifiIpManagementStatic;
-    } else if(sl_mgmt == SL_IP_MANAGEMENT_DHCP) {
-        ret = WifiIpManagementDynamic;
-    } else {
-        furi_crash("Invalid sl_ip_management_t value");
-    }
-
-    return ret;
-}
-
-static WifiIpType wifi_decode_ip_version(sl_ip_address_type_t sl_type) {
-    WifiIpType ret;
-
-    if(sl_type == SL_IPV4) {
-        ret = WifiIpTypeV4;
-    } else if(sl_type == SL_IPV6) {
-        ret = WifiIpTypeV6;
-    } else {
-        furi_crash("Invalid sl_ip_address_type_t value");
-    }
-
-    return ret;
-}
-
-void wifi_decode_ip_config(WifiIpConfig* config, const sl_net_ip_configuration_t* sl_config) {
-    config->mgmt = wifi_decode_ip_management(sl_config->mode);
-    config->type = wifi_decode_ip_version(sl_config->type);
-
-    const uint8_t* ip_ptr;
-    uint8_t* dest;
-    size_t ip_length;
-
-    if(config->type == WifiIpTypeV4) {
-        ip_ptr = sl_config->ip.v4.ip_address.bytes;
-        ip_length = sizeof(sl_config->ip.v4.ip_address.bytes);
-        dest = config->ip4.address.bytes;
-    } else if(config->type == WifiIpTypeV6) {
-        ip_ptr = sl_config->ip.v6.global_address.bytes;
-        ip_length = sizeof(sl_config->ip.v6.global_address.bytes);
-        dest = config->ip6.global.bytes;
-    } else {
-        furi_crash("Invalid WifiIpType value");
-    }
-
-    memcpy(dest, ip_ptr, ip_length);
 }
