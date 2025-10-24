3a59f327275b68d29f637327d3706f9d93ba8984
Georgii Surkov <georgii.surkov@outlook.com>
2025-10-24 18:12:57 +0100

    StatusBar: display Wifi status

--- applications/services/status_bar/widgets/wifi_status_indicator.h
+++ applications/services/status_bar/widgets/wifi_status_indicator.h
@@ -6,6 +6,14 @@
 extern "C" {
 #endif
 
+typedef enum {
+    WifiStatusIndicatorStateUnknown,
+    WifiStatusIndicatorStateDisconnected,
+    WifiStatusIndicatorStateConnecting,
+    WifiStatusIndicatorStateConnected,
+    WifiStatusIndicatorStateMax,
+} WifiStatusIndicatorState;
+
 typedef struct WifiStatusIndicator WifiStatusIndicator;
 
 WifiStatusIndicator* wifi_status_indicator_alloc(Widget* parent);
@@ -13,6 +21,8 @@ WifiStatusIndicator* wifi_status_indicator_alloc(Widget* parent);
 void wifi_status_indicator_free(WifiStatusIndicator* instance);
 
 Widget* wifi_status_indicator_get_base(WifiStatusIndicator* instance);
+
+void wifi_status_indicator_set_state(WifiStatusIndicator* instance, WifiStatusIndicatorState state);
 
 #ifdef __cplusplus
 }
