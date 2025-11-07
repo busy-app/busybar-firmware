c707454cf9794a5097f7534b0ffa4d3e016e5af7
RebornedBrain <RebornedBrain@gmail.com>
2025-11-07 13:33:10 +0300

    Reworked flags to use enum instead of bitfield, simplified subsctiption and adding more flags in future

--- applications/services/ble/service/device_events/ble_service_device_events.c
+++ applications/services/ble/service/device_events/ble_service_device_events.c
@@ -5,14 +5,17 @@
 
 #define TAG "BleDevEvents"
 
-typedef union {
-    uint32_t raw;
-    struct {
-        bool name_changed : 1;
-        bool test_flag_1  : 1;
-        bool test_flag_2  : 1;
-    } flags;
-} BleServiceDeviceEvents;
+typedef uint32_t BleServiceDeviceEvents;
+
+typedef enum {
+    BleServiceDeviceEventFlagBitNameChange = 0,
+    BleServiceDeviceEventFlagBitCount
+} BleServiceDeviceEventFlagBit;
+
+typedef struct {
+    BleServiceDeviceEventFlagBit flag_bit;
+    BleServiceObject* instance;
+} BleDeviceEventSubscriptionContext;
 
 typedef struct {
     FuriMutex* lock;
@@ -33,18 +36,6 @@ typedef enum {
 #define EVENT_SERVICE_FLAGS_CHAR_UUID UUID_VALUE(1)
 
 //==========================================================
-static const BleCharacteristicDescriptor device_events_service_characteristics[] = {
-    {
-        .intercom_index = BleSrvDeviceEventsCharacterIndexFlags,
-        .name = "Event Flags",
-#if defined(SI917)
-        .uuid = {.Char_UUID_128 = EVENT_SERVICE_FLAGS_CHAR_UUID},
-        .uuid_size = 16,
-        ///TODO: maybe BLE_ATT_PROPERTY_READ is optional in current context, and can be removed
-        .char_properties = BLE_ATT_PROPERTY_READ | BLE_ATT_PROPERTY_INDICATE,
-#endif
-    },
-};
 
 #if defined(SI917)
 static bool ble_service_device_events_init_917(void* object) {
@@ -54,18 +45,25 @@ static bool ble_service_device_events_init_917(void* object) {
 }
 #else
 
-static void ble_device_event_on_name_changed(const void* message, void* context) {
-    UNUSED(message);
-    BleServiceObject* instance = context;
+static void
+    ble_device_events_set_flag(BleServiceObject* instance, BleServiceDeviceEventFlagBit flag_bit) {
+    furi_assert(flag_bit < BleServiceDeviceEventFlagBitCount);
+
     if(ble_service_lock(instance)) {
         BleServiceDeviceEventContext* ctx = instance->context;
         furi_mutex_acquire(ctx->lock, FuriWaitForever);
-        ctx->shadow.flags.name_changed = true;
+        ctx->shadow |= (1 << flag_bit);
         furi_mutex_release(ctx->lock);
 
         ble_service_enqueue_run(instance);
         ble_service_unlock(instance);
     }
+}
+
+static void ble_device_events_callback(const void* message, void* context) {
+    UNUSED(message);
+    BleDeviceEventSubscriptionContext* ctx = context;
+    ble_device_events_set_flag(ctx->instance, ctx->flag_bit);
 }
 
 static void ble_service_device_events_tx_done(void* context) {
@@ -84,13 +82,13 @@ void timer_cb(void* context) {
     if(ble_service_lock(instance)) {
         BleServiceDeviceEventContext* ctx = instance->context;
         furi_mutex_acquire(ctx->lock, FuriWaitForever);
+
         if(ctx->cout == 0)
-            ctx->shadow.flags.test_flag_1 = true;
+            ctx->shadow |= (1 << 1);
         else if(ctx->cout == 1)
-            ctx->shadow.flags.test_flag_2 = true;
+            ctx->shadow |= (1 << 2);
         else if(ctx->cout == 2) {
-            ctx->shadow.flags.test_flag_1 = true;
-            ctx->shadow.flags.test_flag_2 = true;
+            ctx->shadow |= ((1 << 3) | (1 << 2) | (1 << 1));
         }
         furi_mutex_release(ctx->lock);
 
@@ -100,6 +98,20 @@ void timer_cb(void* context) {
     }
 }
 
+typedef FuriPubSub* (*BleServiceDeviceEventFlagGetPubSubCallback)(void);
+
+static FuriPubSub* ble_service_device_event_flag_get_pubsub() {
+    DeviceName* device_name = furi_record_open(RECORD_DEVICE_NAME);
+    FuriPubSub* pubsub = device_name_get_pubsub(device_name);
+    furi_record_close(RECORD_DEVICE_NAME);
+    return pubsub;
+}
+
+static const BleServiceDeviceEventFlagGetPubSubCallback
+    pubsub_callbacks[BleServiceDeviceEventFlagBitCount] = {
+        [BleServiceDeviceEventFlagBitNameChange] = ble_service_device_event_flag_get_pubsub,
+};
+
 static bool ble_service_device_events_init_u5(void* object) {
     BLE_LOG_I("device_events_init_u5");
     BleServiceObject* instance = object;
@@ -107,20 +119,23 @@ static bool ble_service_device_events_init_u5(void* object) {
     BleServiceDeviceEventContext* ctx = malloc(sizeof(BleServiceDeviceEventContext));
     ctx->lock = furi_mutex_alloc(FuriMutexTypeNormal);
     ctx->timer = furi_timer_alloc(timer_cb, FuriTimerTypeOnce, instance);
-    ctx->shadow.raw = 0;
+    ctx->shadow = 0;
     instance->context = ctx;
 
-    DeviceName* device_name = furi_record_open(RECORD_DEVICE_NAME);
-    FuriPubSub* pub_sub = device_name_get_on_change_pub_sub(device_name);
-    furi_pubsub_subscribe(pub_sub, ble_device_event_on_name_changed, instance);
-    furi_record_close(RECORD_DEVICE_NAME);
+    for(uint8_t i = 0; i < BleServiceDeviceEventFlagBitCount; i++) {
+        BleServiceDeviceEventFlagGetPubSubCallback get_pubsub = pubsub_callbacks[i];
+        FuriPubSub* pubsub = get_pubsub();
+        BleDeviceEventSubscriptionContext* sub_context =
+            malloc(sizeof(BleDeviceEventSubscriptionContext));
+        sub_context->flag_bit = i;
+        sub_context->instance = instance;
+        furi_pubsub_subscribe(pubsub, ble_device_events_callback, sub_context);
+    }
+
+    // FuriPubSub* pubsub = ble_service_device_event_flag_get_pubsub();
+    // furi_pubsub_subscribe(pubsub, ble_device_event_on_name_changed, instance);
 
     BleCharacteristicObject* ch = instance->chars[BleSrvDeviceEventsCharacterIndexFlags];
-
-    furi_mutex_acquire(ctx->lock, FuriWaitForever);
-    ble_characteristic_set_data(ch, &ctx->shadow, sizeof(BleServiceDeviceEvents));
-    ctx->shadow.raw = 0;
-    furi_mutex_release(ctx->lock);
 
     ble_characteristic_register_tx_done_callback(ch, ble_service_device_events_tx_done, instance);
 
@@ -134,16 +149,29 @@ static bool ble_service_device_events_run(void* object) {
     BleCharacteristicObject* ch = instance->chars[BleSrvDeviceEventsCharacterIndexFlags];
 
     furi_mutex_acquire(ctx->lock, FuriWaitForever);
-    if(ctx->shadow.raw != 0) {
+    if(ctx->shadow != 0) {
         ble_characteristic_set_data(ch, &ctx->shadow, sizeof(BleServiceDeviceEvents));
-        ctx->shadow.raw = 0;
+        ctx->shadow = 0;
     }
     furi_mutex_release(ctx->lock);
 
     return true;
 }
-
 #endif
+
+static const BleCharacteristicDescriptor device_events_service_characteristics[] = {
+    {
+        .intercom_index = BleSrvDeviceEventsCharacterIndexFlags,
+        .name = "Event Flags",
+        .initial_data_size = sizeof(BleServiceDeviceEvents),
+#if defined(SI917)
+        .uuid = {.Char_UUID_128 = EVENT_SERVICE_FLAGS_CHAR_UUID},
+        .uuid_size = 16,
+        ///TODO: maybe BLE_ATT_PROPERTY_READ is optional in current context, and can be removed
+        .char_properties = BLE_ATT_PROPERTY_READ | BLE_ATT_PROPERTY_INDICATE,
+#endif
+    },
+};
 
 const BleServiceDescriptor ble_service_config_device_events = {
     .name = "Device Events",
