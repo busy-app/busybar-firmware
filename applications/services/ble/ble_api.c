2cd78228c0a7238338b81e6f009d0821da6b73bd
RebornedBrain <RebornedBrain@gmail.com>
2025-11-26 15:33:09 +0300

    Api cleanup, now state will be checked inside

--- applications/services/ble/ble_api.c
+++ applications/services/ble/ble_api.c
@@ -43,31 +43,13 @@ static void ble_send_message(
     furi_mutex_release(instance->current_message_lock);
 }
 
-void ble_set_name(Ble* ble) {
-    bool result = false;
-    ble_send_message(ble, BleCommandSetDeviceName, NULL, 0, NULL, 0, &result);
-}
-
 bool ble_init(Ble* ble) {
     furi_assert(ble);
 
-    BleServiceState state = ble_get_state(ble);
+    // BleServiceState state = ble_get_state(ble);
 
     bool result = false;
-    if(state == BleServiceStateReset) {
-        ble_send_message(ble, BleCommandInit, NULL, 0, NULL, 0, &result);
-        ble_set_name(ble);
-
-    } else if(
-        state == BleServiceStateReady || state == BleServiceStateAdvertising ||
-        state == BleServiceStateConnected) {
-        ///TODO: possibly this should be done by actually executing command and
-        /// if state is Ready then just do nothing and return true;
-        /// But for now let's left it as it is
-        result = true;
-    } else {
-        BLE_LOG_W("No init, wrong state: %d", state);
-    }
+    ble_send_message(ble, BleCommandInit, NULL, 0, NULL, 0, &result);
 
     return result;
 }
@@ -86,27 +68,27 @@ BleServiceState ble_get_state(Ble* ble) {
 
 bool ble_start(Ble* ble) {
     furi_assert(ble);
-    ble_init(ble);
-
-    BleServiceState state = ble_get_state(ble);
 
     bool result = false;
-    if(state == BleServiceStateReady) {
+    do {
+        if(!ble_init(ble)) break;
+
         ble_send_message(ble, BleCommandEnable, NULL, 0, NULL, 0, &result);
-    } else {
-        BLE_LOG_W("No start, wrong state: %d", state);
-    }
+    } while(false);
 
     return result;
 }
 
 bool ble_stop(Ble* ble) {
     furi_assert(ble);
-    bool result = false;
-    ble_init(ble);
 
-    BLE_LOG_I("ble_stop");
-    ble_send_message(ble, BleCommandDisable, NULL, 0, NULL, 0, &result);
+    bool result = false;
+    do {
+        if(!ble_init(ble)) break;
+
+        ble_send_message(ble, BleCommandDisable, NULL, 0, NULL, 0, &result);
+    } while(false);
+
     return result;
 }
 
