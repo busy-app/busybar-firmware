35102e97ec1c53c038ec13f20931bd80ff92b8ac
RebornedBrain <RebornedBrain@gmail.com>
2025-12-10 17:19:13 +0300

    Use UNDIR_CONN advertise during testing

--- applications/services/ble/worker/ble_worker.c
+++ applications/services/ble/worker/ble_worker.c
@@ -651,12 +651,11 @@ static int32_t ble_worker_thread_callback(void* context) {
             //! start advertising
             const rsi_bt_event_le_security_keys_t* rpa =
                 ble_security_get_rpa_data(ble_worker_instance->security_data);
-            instance->state = ble_worker_start_advertising(
-                                  ble_worker_instance->pairing_info_available,
-                                  rpa,
-                                  ble_worker_instance->advertise) ?
-                                  BleWorkerStateAdvertising :
-                                  BleWorkerStateError;
+            ///TODO: move ble_worker_instance->pairing_info_available instead of false, when debug done
+            instance->state =
+                ble_worker_start_advertising(false, rpa, ble_worker_instance->advertise) ?
+                    BleWorkerStateAdvertising :
+                    BleWorkerStateError;
 
             memset(ble_worker_instance->str_remote_address, 0, BLE_REMOTE_ADDRESS_STRING_SIZE);
             ble_worker_instance->on_connection_changed_cb(
@@ -1129,8 +1128,8 @@ void ble_worker_start() {
 
         const rsi_bt_event_le_security_keys_t* rpa =
             ble_security_get_rpa_data(ble_worker_instance->security_data);
-        ble_worker_start_advertising(
-            ble_worker_instance->pairing_info_available, rpa, ble_worker_instance->advertise);
+        ///TODO: move ble_worker_instance->pairing_info_available instead of false, when debug done
+        ble_worker_start_advertising(false, rpa, ble_worker_instance->advertise);
 
         ble_worker_instance->state = BleWorkerStateAdvertising;
         furi_thread_start(ble_worker_instance->thread);
