50f2ce28fb363e31ecd76822e802618bab8aa712
Anna Antonenko <portasynthinca3@gmail.com>
2025-12-26 16:31:48 +0400

    format code

--- lib/matter_glue/platform/bsb/ConfigurationManagerImpl.cpp
+++ lib/matter_glue/platform/bsb/ConfigurationManagerImpl.cpp
@@ -134,7 +134,7 @@ exit:
     return err;
 }
 
-CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersionString(char * buf, size_t bufSize) {
+CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersionString(char* buf, size_t bufSize) {
     const Version* version = version_get();
     const char* version_str = version_get_version(version);
 
@@ -144,7 +144,7 @@ CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersionString(char * buf, size_t
     return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersion(uint32_t & softwareVer) {
+CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersion(uint32_t& softwareVer) {
     softwareVer = MATTER_SOFTWARE_VER_NUM;
     return CHIP_NO_ERROR;
 }
