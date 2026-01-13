455fbeaedef1a8f37eeea25584bd3270db1b89ef
RebornedBrain <RebornedBrain@gmail.com>
2026-01-13 15:28:23 +0300

    Added back screen layout for pairing mode scene

--- applications/settings/ble_settings/scenes/scene_pairing_mode.c
+++ applications/settings/ble_settings/scenes/scene_pairing_mode.c
@@ -2,18 +2,25 @@
 #include <settings_helpers/gui_params.h>
 
 #include <gui/modules/anim_image.h>
+#include <gui/modules/image.h>
 #include <gui/modules/label.h>
 
 #include <ble/ble.h>
+#include <device_name/device_name.h>
 
 typedef enum {
     SceneEventBlePairingEvent = AppEventSceneEventsStart,
 } SceneEvent;
 
 typedef struct {
-    FlexLayout* flex;
+    FlexLayout* front_flex;
     AnimImage* front_anim;
     Label* front_label;
+
+    FlexLayout* back_flex;
+    Image* back_image;
+    Label* back_label;
+    Label* name_label;
     FuriPubSubSubscription* pubsub_subscription;
 } BleSettingsPairingSceneData;
 
@@ -21,6 +28,12 @@ static void scene_pairing_ble_pairing_done_callback(const void* message, void* c
     UNUSED(message);
     BleSettings* instance = context;
     ble_settings_send_custom_event(instance, SceneEventBlePairingEvent);
+}
+
+static void scene_pairing_get_device_name(FuriString* output) {
+    DeviceName* device_name = furi_record_open(RECORD_DEVICE_NAME);
+    device_name_get(device_name, output);
+    furi_record_close(RECORD_DEVICE_NAME);
 }
 
 static void scene_pairing_mode_on_enter(void* context) {
@@ -36,22 +49,52 @@ static void scene_pairing_mode_on_enter(void* context) {
     ///TODO: Rework this to enable BLE on start according to previous state, and move it out of here
     ble_start(instance->ble);
 
-    with_gui(instance->gui, {
-        data->flex = flex_layout_alloc(instance->front_scene_window, FlexLayoutTypeRow);
-        flex_layout_set_spacing(data->flex, 2);
-        widget_set_height_content(flex_layout_get_base(data->flex));
-        widget_set_align(flex_layout_get_base(data->flex), AlignLeftMid);
+    FuriString* name = furi_string_alloc();
+    scene_pairing_get_device_name(name);
+    FuriString* name_label_text =
+        furi_string_alloc_printf("Device name: %s", furi_string_get_cstr(name));
+    furi_string_free(name);
 
-        data->front_anim = anim_image_alloc(flex_layout_get_base(data->flex));
+    with_gui(instance->gui, {
+        const char* pairing_text = "Pairing mode...";
+        //Front screen
+        data->front_flex = flex_layout_alloc(instance->front_scene_window, FlexLayoutTypeRow);
+        flex_layout_set_spacing(data->front_flex, 2);
+        widget_set_height_content(flex_layout_get_base(data->front_flex));
+        widget_set_align(flex_layout_get_base(data->front_flex), AlignLeftMid);
+
+        data->front_anim = anim_image_alloc(flex_layout_get_base(data->front_flex));
         anim_image_set_source(data->front_anim, ANIM_PATH("ble_pairing_8x8.anim"));
         anim_image_set_range(data->front_anim, 0, 3, true, false);
         widget_set_height_content(anim_image_get_base(data->front_anim));
 
-        data->front_label = label_alloc(flex_layout_get_base(data->flex));
-        label_set_text(data->front_label, "Pairing mode...");
+        data->front_label = label_alloc(flex_layout_get_base(data->front_flex));
+        label_set_text(data->front_label, pairing_text);
         widget_set_height_content(label_get_base(data->front_label));
+
+        //Back screen
+        data->back_flex = flex_layout_alloc(instance->back_scene_window, FlexLayoutTypeColumn);
+        flex_layout_set_align(
+            data->back_flex, FlexLayoutAlignCenter, FlexLayoutAlignCenter, FlexLayoutAlignCenter);
+
+        data->back_image = image_alloc(flex_layout_get_base(data->back_flex));
+        image_set_source(data->back_image, IMG_PATH("ble_back_white_11x11.bin"));
+        widget_set_width_content(image_get_base(data->back_image));
+        widget_set_margin(image_get_base(data->back_image), 0, 0, 0, 6);
+
+        data->back_label = label_alloc(flex_layout_get_base(data->back_flex));
+        widget_set_width_content(label_get_base(data->back_label));
+        label_set_text(data->back_label, pairing_text);
+        widget_set_margin(label_get_base(data->back_label), 0, 0, 0, 2);
+
+        data->name_label = label_alloc(flex_layout_get_base(data->back_flex));
+        label_set_text(data->name_label, furi_string_get_cstr(name_label_text));
+        widget_set_width_content(label_get_base(data->name_label));
+        Color color = COLOR_MAKE_HEX(0x444444);
+        label_set_text_color(data->name_label, color);
     });
 
+    furi_string_free(name_label_text);
     Color color = COLOR_MAKE_RGB(0, 0, 0xFF);
     status_lights_set_brightness(instance->status_lights, STATUS_LIGHTS_BRIGHTNESS_MAX);
     status_lights_run_preset(instance->status_lights, StatusLightsPresetFade, color);
@@ -69,7 +112,12 @@ static void scene_pairing_mode_on_exit(void* context) {
     with_gui(instance->gui, {
         anim_image_free(data->front_anim);
         label_free(data->front_label);
-        flex_layout_free(data->flex);
+        flex_layout_free(data->front_flex);
+
+        image_free(data->back_image);
+        label_free(data->back_label);
+        label_free(data->name_label);
+        flex_layout_free(data->back_flex);
     });
 
     status_lights_run_preset(instance->status_lights, StatusLightsPresetOff, (Color){});
