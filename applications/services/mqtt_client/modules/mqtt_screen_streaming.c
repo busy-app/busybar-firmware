b8bf0206d137eaa07d67d17e723c3dd8f957419c
Georgii Surkov <georgii.surkov@outlook.com>
2026-01-29 12:35:53 +0000

    Mqtt: refactor screen streaming to a permanent service

--- applications/services/mqtt_client/modules/mqtt_screen_streaming.c
+++ applications/services/mqtt_client/modules/mqtt_screen_streaming.c
@@ -2,9 +2,9 @@
 #include <mqtt_client/mqtt_client.h>
 #include <front_display/front_display.h>
 
-#define TAG "MqttScreenStreamingSrv"
+#include <time.h>
 
-#define THREAD_STACK_SIZE (1024)
+#define TAG "MqttScreenStreamSrv"
 
 #define SUB_QOS (MqttQosAtLeastOnce)
 #define PUB_QOS (MqttQosAtMostOnce)
@@ -12,14 +12,28 @@
 #define SUB_TOPIC "stream-request"
 #define PUB_TOPIC "displays/front"
 
+#define API_QUEUE_SIZE  (4)
 #define FRAME_PERIOD_MS (500)
 
 typedef enum {
-    MqttScreenStreamingThreadFlagStop = 1UL << 0,
-} MqttScreenStreamingThreadFlag;
+    MqttScreenStreamingApiMessageTypeStart,
+    MqttScreenStreamingApiMessageTypeStop,
+    MqttScreenStreamingApiMessageTypeMax,
+} MqttScreenStreamingApiMessageType;
 
 typedef struct {
-    FuriThread* thread;
+    MqttScreenStreamingApiMessageType type;
+    union {
+        time_t timestamp;
+    };
+} MqttScreenStreamingApiMessage;
+
+typedef struct {
+    FuriEventLoop* event_loop;
+    FuriEventLoopTimer* timer;
+    FuriMessageQueue* api_queue;
+    MqttClient* mqtt;
+    Gui* gui;
     uint8_t* buffer;
 } MqttScreenStreaming;
 
@@ -28,30 +42,23 @@ static void mqtt_screen_streaming_message_callback(const MqttMessage* message, v
     furi_assert(context);
 
     MqttScreenStreaming* instance = context;
-    UNUSED(instance);
 
     size_t data_size;
     const char* data = mqtt_message_get_data(message, &data_size);
     UNUSED(data);
     // TODO: Parse timestamp & calculate timeout
 
-    const FuriThreadState thread_state = furi_thread_get_state(instance->thread);
+    MqttScreenStreamingApiMessage api_msg;
 
     if(data_size) {
-        if(thread_state == FuriThreadStateStopping) {
-            furi_thread_join(instance->thread);
-        }
-        if(thread_state == FuriThreadStateStopped) {
-            furi_thread_start(instance->thread);
-        }
+        api_msg.type = MqttScreenStreamingApiMessageTypeStart;
+        api_msg.timestamp = 0;
 
     } else {
-        if(thread_state == FuriThreadStateRunning || thread_state == FuriThreadStateStarting) {
-            furi_thread_flags_set(instance->thread, MqttScreenStreamingThreadFlagStop);
-        }
-
-        furi_thread_join(instance->thread);
+        api_msg.type = MqttScreenStreamingApiMessageTypeStop;
     }
+
+    furi_message_queue_put(instance->api_queue, &api_msg, FuriWaitForever);
 }
 
 static void mqtt_screen_streaming_pubsub_callback(const void* message, void* context) {
@@ -64,67 +71,88 @@ static void mqtt_screen_streaming_pubsub_callback(const void* message, void* con
     if(event->type == MqttEventTypeStatusChanged) {
         if(event->status_changed.status == MqttClientStatusNotConnected) {
             FURI_LOG_E(TAG, "Connection lost");
-            furi_thread_flags_set(instance->thread, MqttScreenStreamingThreadFlagStop);
-            furi_thread_join(instance->thread);
+            const MqttScreenStreamingApiMessage api_msg = {
+                .type = MqttScreenStreamingApiMessageTypeStop,
+            };
+
+            furi_message_queue_put(instance->api_queue, &api_msg, FuriWaitForever);
         }
     }
 }
 
-static int32_t mqtt_screen_streaming_thread_callback(void* arg) {
-    furi_assert(arg);
-    MqttScreenStreaming* instance = arg;
+static void mqtt_screen_streaming_timer_callback(void* context) {
+    furi_assert(context);
+    MqttScreenStreaming* instance = context;
 
-    furi_check(instance->buffer == NULL);
-    instance->buffer = malloc(FRONT_DISPLAY_BUF_SIZE);
+    with_gui(instance->gui, {
+        const uint8_t* frame = gui_display_get_frame_buffer(instance->gui, GuiDisplayIdFront);
+        memcpy(instance->buffer, frame, FRONT_DISPLAY_BUF_SIZE);
+    });
 
-    Gui* gui = furi_record_open(RECORD_GUI);
-    MqttClient* mqtt = furi_record_open(RECORD_MQTT);
+    mqtt_client_publish(
+        instance->mqtt, PUB_QOS, PUB_TOPIC, instance->buffer, FRONT_DISPLAY_BUF_SIZE);
+}
 
-    FURI_LOG_I(TAG, "Start");
+static void mqtt_screen_streaming_api_queue_callback(FuriEventLoopObject* obj, void* context) {
+    furi_assert(context);
+    MqttScreenStreaming* instance = context;
 
-    for(;;) {
-        const uint32_t flags = furi_thread_flags_wait(
-            MqttScreenStreamingThreadFlagStop, FuriFlagWaitAny, FRAME_PERIOD_MS);
+    furi_assert(instance->api_queue == obj);
 
-        if(flags & FuriFlagError) {
-            furi_check(flags == FuriFlagErrorTimeout);
-        } else if(flags & MqttScreenStreamingThreadFlagStop) {
-            break;
+    MqttScreenStreamingApiMessage msg;
+    while(furi_message_queue_get(instance->api_queue, &msg, 0) == FuriStatusOk) {
+        if(msg.type == MqttScreenStreamingApiMessageTypeStart) {
+            if(!furi_event_loop_timer_is_running(instance->timer)) {
+                FURI_LOG_I(TAG, "Start");
+                furi_event_loop_timer_start(instance->timer, FRAME_PERIOD_MS);
+            }
+
+        } else if(msg.type == MqttScreenStreamingApiMessageTypeStop) {
+            FURI_LOG_I(TAG, "Stop");
+            furi_event_loop_timer_stop(instance->timer);
+
+        } else {
+            furi_crash("Invalid MqttScreenStreamingApiMessageType value");
         }
-
-        with_gui(gui, {
-            const uint8_t* frame = gui_display_get_frame_buffer(gui, GuiDisplayIdFront);
-            memcpy(instance->buffer, frame, FRONT_DISPLAY_BUF_SIZE);
-        });
-
-        mqtt_client_publish(mqtt, PUB_QOS, PUB_TOPIC, instance->buffer, FRONT_DISPLAY_BUF_SIZE);
     }
-
-    furi_record_close(RECORD_GUI);
-    furi_record_close(RECORD_MQTT);
-
-    free(instance->buffer);
-    instance->buffer = NULL;
-
-    FURI_LOG_I(TAG, "Stop");
-
-    return 0;
 }
 
 static MqttScreenStreaming* mqtt_screen_streaming_alloc(void) {
     MqttScreenStreaming* instance = malloc(sizeof(MqttScreenStreaming));
-    instance->thread = furi_thread_alloc_ex(
-        TAG, THREAD_STACK_SIZE, mqtt_screen_streaming_thread_callback, instance);
+
+    instance->event_loop = furi_event_loop_alloc();
+    instance->timer = furi_event_loop_timer_alloc(
+        instance->event_loop,
+        mqtt_screen_streaming_timer_callback,
+        FuriEventLoopTimerTypePeriodic,
+        instance);
+    instance->api_queue =
+        furi_message_queue_alloc(API_QUEUE_SIZE, sizeof(MqttScreenStreamingApiMessage));
+    instance->mqtt = furi_record_open(RECORD_MQTT);
+    instance->gui = furi_record_open(RECORD_GUI);
+    instance->buffer = malloc(FRONT_DISPLAY_BUF_SIZE);
+
+    furi_event_loop_subscribe_message_queue(
+        instance->event_loop,
+        instance->api_queue,
+        FuriEventLoopEventIn,
+        mqtt_screen_streaming_api_queue_callback,
+        instance);
+
+    furi_pubsub_subscribe(
+        mqtt_client_get_pubsub(instance->mqtt), mqtt_screen_streaming_pubsub_callback, instance);
+
+    mqtt_subscribe(
+        instance->mqtt, SUB_QOS, SUB_TOPIC, mqtt_screen_streaming_message_callback, instance);
+
     return instance;
 }
 
-void mqtt_screen_streaming_on_system_start(void) {
+int32_t mqtt_screen_stream_srv(void* arg) {
+    UNUSED(arg);
+
     MqttScreenStreaming* instance = mqtt_screen_streaming_alloc();
+    furi_event_loop_run(instance->event_loop);
 
-    MqttClient* mqtt = furi_record_open(RECORD_MQTT);
-
-    furi_pubsub_subscribe(
-        mqtt_client_get_pubsub(mqtt), mqtt_screen_streaming_pubsub_callback, instance);
-
-    mqtt_subscribe(mqtt, SUB_QOS, SUB_TOPIC, mqtt_screen_streaming_message_callback, instance);
+    return 0;
 }
