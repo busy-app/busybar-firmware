7c2869f1b46c6c4aa5b31c8272787fd2bcfa44de
gornekich <n.gorbadey@gmail.com>
2026-02-04 17:09:35 +0000

    settings about: fix firmware version scene

--- applications/settings/about/scenes/about_scene_firmware.c
+++ applications/settings/about/scenes/about_scene_firmware.c
@@ -25,28 +25,31 @@ static void about_scene_firmware_on_enter(void* context) {
 
     const Version* version = version_get();
     furi_string_printf(
-        scene->firmware_info_str, GREY_TEXT("Version:") " %s", version_get_version(version));
-    furi_string_printf(
-        scene->firmware_info_str, GREY_TEXT("Branch:") " %s", version_get_gitbranch(version));
-    furi_string_printf(
-        scene->firmware_info_str, GREY_TEXT("Commit hash:") " %s%%", version_get_githash(version));
-    uint8_t api_version[] = API_VERSION;
-    furi_string_printf(
+        scene->firmware_info_str, GREY_TEXT("Version:") " %s\n", version_get_version(version));
+    furi_string_cat_printf(
+        scene->firmware_info_str, GREY_TEXT("Branch:") "\n%s\n", version_get_gitbranch(version));
+    furi_string_cat_printf(
         scene->firmware_info_str,
-        GREY_TEXT("API version:") " %u.%u.%u",
+        GREY_TEXT("Commit hash:") "\n%s\n",
+        version_get_githash(version));
+
+    uint8_t api_version[] = API_VERSION;
+    furi_string_cat_printf(
+        scene->firmware_info_str,
+        GREY_TEXT("API version:") " %u.%u.%u\n",
         api_version[0],
         api_version[1],
         api_version[2]);
-    furi_string_printf(
+    furi_string_cat_printf(
         scene->firmware_info_str,
-        GREY_TEXT("Build date:") " %s\n",
+        GREY_TEXT("Build date:") "\n%s\n",
         version_get_builddate(version));
 
     uint32_t uptime_s = furi_get_tick() / furi_kernel_get_tick_frequency();
     uint32_t minutes = uptime_s / 60;
     uint32_t hours = minutes / 60;
     uint32_t days = minutes / 24;
-    furi_string_printf(
+    furi_string_cat_printf(
         scene->firmware_info_str,
         GREY_TEXT("Uptime:") " %lud %02luh %02lum\n",
         days,
