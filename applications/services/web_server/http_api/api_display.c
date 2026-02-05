79743a4218fa0589d4aaf2703b5b000c4879253c
nminaylov <nm29719@gmail.com>
2026-02-05 18:53:08 +0300

    Common brightness api for both displays

--- applications/services/web_server/http_api/api_display.c
+++ applications/services/web_server/http_api/api_display.c
@@ -14,7 +14,8 @@
 #define DISPLAY_BUILTIN_IMAGES_FORMATTER EXT_PATH("apps_assets/%s/images/%s.bin")
 #define DISPLAY_BUILTIN_ANIM_FORMATTER   EXT_PATH("apps_assets/%s/animations/%s.anim")
 
-#define DISPLAY_BRIGHTNESS_MAX (100)
+#define DISPLAY_BRIGHTNESS_MAX  (100)
+#define DISPLAY_BRIGHTNESS_AUTO (FRONT_DISPLAY_BRIGHTNESS_AUTO)
 
 static bool api_display_draw_parse_text_element(
     CanvasElement* canvas_element,
@@ -440,23 +441,20 @@ static bool api_display_get_brightness_callback(
     FuriString* json_str = furi_string_alloc();
 
     FrontDisplaySrv* front_srv = furi_record_open(RECORD_FRONT_DISPLAY);
-    uint8_t brightness_value = front_display_get_brightness_setting(front_srv);
+    uint8_t brightness_front = front_display_get_brightness_setting(front_srv);
     furi_record_close(RECORD_FRONT_DISPLAY);
 
-    if(brightness_value == FRONT_DISPLAY_BRIGHTNESS_AUTO) {
-        furi_string_cat_printf(json_str, "\"front\":\"auto\",");
-    } else {
-        furi_string_cat_printf(json_str, "\"front\":\"%u\",", brightness_value);
-    }
-
     BackDisplaySrv* back_srv = furi_record_open(RECORD_BACK_DISPLAY);
-    brightness_value = back_display_get_brightness(back_srv);
+    uint8_t brightness_back = back_display_get_brightness(back_srv);
+    if(brightness_front != brightness_back) {
+        back_display_set_brightness(back_srv, brightness_front);
+    }
     furi_record_close(RECORD_BACK_DISPLAY);
 
-    if(brightness_value == FRONT_DISPLAY_BRIGHTNESS_AUTO) {
-        furi_string_cat_printf(json_str, "\"back\":\"auto\"");
+    if(brightness_front == DISPLAY_BRIGHTNESS_AUTO) {
+        furi_string_cat_printf(json_str, "\"value\":\"auto\"");
     } else {
-        furi_string_cat_printf(json_str, "\"back\":\"%u\"", brightness_value);
+        furi_string_cat_printf(json_str, "\"value\":\"%u\"", brightness_front);
     }
 
     MG_REPLY_OK_BODY(conn, "{%s}\n", furi_string_get_cstr(json_str));
@@ -478,44 +476,29 @@ static bool api_display_set_brightness_callback(
     do {
         if(msg->query.len == 0) break;
 
-        char front_value_str[5];
-        char back_value_str[5];
+        char value_str[5];
+        int brightness_value = 0;
 
-        int front_value = 0;
-        int back_value = 0;
+        int value_len = mg_http_get_var(&msg->query, "value", value_str, sizeof(value_str));
 
-        int front_len =
-            mg_http_get_var(&msg->query, "front", front_value_str, sizeof(front_value_str));
-        int back_len =
-            mg_http_get_var(&msg->query, "back", back_value_str, sizeof(back_value_str));
+        if(value_len <= 0) break;
 
-        if((front_len <= 0) && (back_len <= 0)) break;
-
-        if(front_len > 0) {
-            if(strcmp(front_value_str, "auto") == 0) {
-                front_value = FRONT_DISPLAY_BRIGHTNESS_AUTO;
-            } else if(sscanf(front_value_str, "%u", &front_value) == 1) {
-                if(front_value > DISPLAY_BRIGHTNESS_MAX) break;
-            } else {
-                break;
-            }
-            FrontDisplaySrv* srv = furi_record_open(RECORD_FRONT_DISPLAY);
-            front_display_set_brightness(srv, front_value);
-            furi_record_close(RECORD_FRONT_DISPLAY);
+        if(strcmp(value_str, "auto") == 0) {
+            brightness_value = DISPLAY_BRIGHTNESS_AUTO;
+        } else if(sscanf(value_str, "%u", &brightness_value) == 1) {
+            if(brightness_value > DISPLAY_BRIGHTNESS_MAX) break;
+        } else {
+            break;
         }
+        FrontDisplaySrv* srv = furi_record_open(RECORD_FRONT_DISPLAY);
+        BackDisplaySrv* back_srv = furi_record_open(RECORD_BACK_DISPLAY);
 
-        if(back_len > 0) {
-            if(strcmp(back_value_str, "auto") == 0) {
-                back_value = BACK_DISPLAY_BRIGHTNESS_AUTO;
-            } else if(sscanf(back_value_str, "%u", &back_value) == 1) {
-                if(back_value > DISPLAY_BRIGHTNESS_MAX) break;
-            } else {
-                break;
-            }
-            BackDisplaySrv* srv = furi_record_open(RECORD_BACK_DISPLAY);
-            back_display_set_brightness(srv, back_value);
-            furi_record_close(RECORD_BACK_DISPLAY);
-        }
+        front_display_set_brightness(srv, brightness_value);
+        back_display_set_brightness(back_srv, brightness_value);
+
+        furi_record_close(RECORD_FRONT_DISPLAY);
+        furi_record_close(RECORD_BACK_DISPLAY);
+
         success = true;
     } while(0);
 
