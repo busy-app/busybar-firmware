9ea17e48a940c8d9b7aff3bc7ea1702984096474
Ivan Shestakov <vanyww2@gmail.com>
2026-02-20 14:41:11 +0400

    Make intercom cli break pipe on intercom death

--- applications/services/cli_u5/cli_command_sl_cli.c
+++ applications/services/cli_u5/cli_command_sl_cli.c
@@ -8,9 +8,7 @@
 #ifdef SRV_INTERCOM
 FURI_CHECK_RETURN bool
     cli_command_sl_cli_send_command_get_response(PipeSide* pipe, const char* sl_cmd) {
-    if(!furi_record_exists(RECORD_CLI_INTERCOM)) {
-        return false;
-    }
+    if(!furi_record_exists(RECORD_CLI_INTERCOM)) return false;
 
     PipeSideBundle temp_bundle = pipe_alloc(TEMP_PIPE_SZ, 1);
     PipeSide* temp_own_pipe = temp_bundle.alices_side;
@@ -18,29 +16,30 @@ FURI_CHECK_RETURN bool
 
     CliIntercom* cli_intercom = furi_record_open(RECORD_CLI_INTERCOM);
 
-    bool success = true;
+    bool is_success = false;
+
     do {
         if(cli_intercom_spawn(cli_intercom, temp_shell_pipe) != CliIntercomSpawnStatusOk) {
-            success = false;
+            pipe_free(temp_shell_pipe);
             break;
         }
 
-        furi_check(pipe_copy_until(temp_own_pipe, NULL, CLI_PROMPT));
+        if(!pipe_copy_until(temp_own_pipe, NULL, CLI_PROMPT)) break;
+        if(pipe_send(temp_own_pipe, sl_cmd, strlen(sl_cmd)) != strlen(sl_cmd)) break;
+        if(pipe_send(temp_own_pipe, "\r", 1) != 1) break;
+        if(!pipe_copy_until(temp_own_pipe, NULL, sl_cmd)) break;
+        if(!pipe_copy_until(temp_own_pipe, NULL, "\r\n")) break;
+        if(!pipe_copy_until(temp_own_pipe, pipe, CLI_PROMPT)) break;
 
-        furi_check(pipe_send(temp_own_pipe, sl_cmd, strlen(sl_cmd)) == strlen(sl_cmd));
-        furi_check(pipe_send(temp_own_pipe, "\r", 1) == 1);
-        furi_check(pipe_copy_until(temp_own_pipe, NULL, sl_cmd));
-        furi_check(pipe_copy_until(temp_own_pipe, NULL, "\r\n"));
-
-        furi_check(pipe_copy_until(temp_own_pipe, pipe, CLI_PROMPT));
+        is_success = true;
     } while(false);
 
     pipe_free(temp_own_pipe);
     cli_intercom_join(cli_intercom);
-    furi_record_close(RECORD_CLI_INTERCOM);
-    pipe_free(temp_shell_pipe);
 
-    return success;
+    furi_record_close(RECORD_CLI_INTERCOM);
+
+    return is_success;
 }
 
 void cli_command_sl_cli(PipeSide* pipe, FuriString* args, void* context) {
