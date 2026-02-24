ca4ec3e0dad85d321440643a0b8271cd7ba8a041
Ivan Shestakov <vanyww2@gmail.com>
2026-02-24 13:55:36 +0400

    Backport factory reset cli command improvements from dev

--- applications/services/cli_f20/cli_command_factory_reset.c
+++ applications/services/cli_f20/cli_command_factory_reset.c
@@ -3,6 +3,42 @@
 
 #include <furi_hal_nvm.h>
 #include <storage/storage.h>
+#include <cli/args.h>
+
+typedef struct {
+    bool shipping_mode;
+    bool help;
+} FactoryResetArgs;
+
+static bool parse_command_args(FuriString* args, FactoryResetArgs* parsed_args) {
+    parsed_args->shipping_mode = false;
+    parsed_args->help = false;
+
+    FuriString* arg = furi_string_alloc();
+
+    bool is_success = true;
+    while(args_read_string_and_trim(args, arg)) {
+        if(furi_string_equal_str(arg, "-s") || furi_string_equal_str(arg, "--shipping-mode")) {
+            parsed_args->shipping_mode = true;
+        } else if(furi_string_equal_str(arg, "-h") || furi_string_equal_str(arg, "--help")) {
+            parsed_args->help = true;
+        } else {
+            printf("Unknown argument: %s\r\n", furi_string_get_cstr(arg));
+            is_success = false;
+            break;
+        }
+    }
+
+    furi_string_free(arg);
+    return is_success;
+}
+
+static void print_command_help(void) {
+    printf("Usage: factory_reset [options]\r\n");
+    printf("Options:\r\n");
+    printf("  -s, --shipping-mode    Enter shipping mode after performing reset\r\n");
+    printf("  -h, --help             Show this help message\r\n");
+}
 
 static void cli_command_step_format_emmc() {
     Storage* storage = furi_record_open(RECORD_STORAGE);
@@ -29,8 +65,14 @@ static void cli_command_step_wifi_ble_restore_default_config() {
 }
 
 void cli_command_factory_reset(PipeSide* pipe, FuriString* args, void* context) {
-    UNUSED(args);
     UNUSED(context);
+
+    FactoryResetArgs _args;
+    if(!parse_command_args(args, &_args) || _args.help) {
+        print_command_help();
+        return;
+    }
+
     printf("Warning! This will wipe all the data from the device! Are you sure? y/n\r\n");
 
     while(true) {
@@ -45,6 +87,11 @@ void cli_command_factory_reset(PipeSide* pipe, FuriString* args, void* context) 
             cli_command_step_reset_pairing();
             cli_command_step_wifi_ble_restore_default_config();
             furi_hal_nvm_reset();
+
+            if(_args.shipping_mode) {
+                furi_hal_nvm_set_flag(FuriHalNvmFlagRebootIntoShippingMode);
+            }
+
             printf("Done\r\nRebooting...\r\n");
             furi_delay_ms(100);
             Power* pwr = furi_record_open(RECORD_POWER);
