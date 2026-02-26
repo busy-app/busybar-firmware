9fc8ec1c208bd835440631992f55613a5c76a90d
hedger <hedger@nanode.su>
2026-02-26 11:58:48 +0000

    cli_intercom_spawn: added close_pipe_on_detach for proper lifecycling of cli pipes

--- applications/services/cli_intercom/cli_intercom.h
+++ applications/services/cli_intercom/cli_intercom.h
@@ -36,12 +36,18 @@ typedef enum {
  * @brief Spawns a shell on f64. Relays data and status information in both
  *        directions using the given pipe.
  * 
- * @param[in] cli_intercom CliIntercom instance
- * @param[in] pipe         Local PipeSide instance
+ * @param[in] cli_intercom         CliIntercom instance
+ * @param[in] pipe                 Local PipeSide instance
+ * @param[in] close_pipe_on_detach If true, call pipe_close() on the pipe when the intercom
+ *                                 session ends. Set to true for programmatic callers that may be
+ *                                 blocked in pipe operations on the other side of a temp pipe;
+ *                                 set to false for interactive callers that pass an outer shell
+ *                                 pipe that must survive the session.
  * 
  * @returns Spawn status: OK or error.
  */
-CliIntercomSpawnStatus cli_intercom_spawn(CliIntercom* cli_intercom, PipeSide* pipe);
+CliIntercomSpawnStatus
+    cli_intercom_spawn(CliIntercom* cli_intercom, PipeSide* pipe, bool close_pipe_on_detach);
 
 /**
  * @brief Blocks until the shell on f64 exits.
