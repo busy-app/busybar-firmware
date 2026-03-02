461436b828c22ed2a34b18b47a032f65d63b9648
RebornedBrain <RebornedBrain@gmail.com>
2026-03-02 16:24:03 +0300

    Ble status indicator is enhanced with state logic

--- applications/services/status_bar/widgets/ble_status_indicator.h
+++ applications/services/status_bar/widgets/ble_status_indicator.h
@@ -6,6 +6,14 @@
 extern "C" {
 #endif
 
+typedef enum {
+    BleStatusIndicatorStateUnknown,
+    BleStatusIndicatorStateDisconnected,
+    BleStatusIndicatorStateConnecting,
+    BleStatusIndicatorStateConnected,
+    BleStatusIndicatorStateMax,
+} BleStatusIndicatorState;
+
 typedef struct BleStatusIndicator BleStatusIndicator;
 
 BleStatusIndicator* ble_status_indicator_alloc(Widget* parent);
@@ -13,6 +21,8 @@ BleStatusIndicator* ble_status_indicator_alloc(Widget* parent);
 void ble_status_indicator_free(BleStatusIndicator* instance);
 
 Widget* ble_status_indicator_get_base(BleStatusIndicator* instance);
+
+void Ble_status_indicator_set_state(BleStatusIndicator* instance, BleStatusIndicatorState state);
 
 #ifdef __cplusplus
 }
