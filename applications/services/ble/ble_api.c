ea376ab72b1ca948b919537dc00fd28b4d789682
RebornedBrain <RebornedBrain@gmail.com>
2026-03-02 16:28:39 +0300

    Fix duplication after merge

--- applications/services/ble/ble_api.c
+++ applications/services/ble/ble_api.c
@@ -147,19 +147,3 @@ void ble_uart_session_set_value(Ble* ble, const uint32_t session) {
     BleServiceObject* service = ble->services[BleServiceIndexNordicUart];
     ble_service_write_data(service, BleUartCharacteristicIndexSession, &session, sizeof(uint32_t));
 }
-
-void ble_uart_set_session_callback(Ble* ble, BleDataUpdatedCallback session_update_cb, void* ctx) {
-    furi_assert(ble);
-    furi_assert(session_update_cb);
-    furi_assert(ctx);
-
-    BleServiceObject* service = ble->services[BleServiceIndexNordicUart];
-    ble_service_register_update_callback(service, 2, session_update_cb, ctx);
-}
-
-void ble_uart_session_set_value(Ble* ble, const uint32_t session) {
-    furi_assert(ble);
-
-    BleServiceObject* service = ble->services[BleServiceIndexNordicUart];
-    ble_service_write_data(service, 2, &session, sizeof(uint32_t));
-}
