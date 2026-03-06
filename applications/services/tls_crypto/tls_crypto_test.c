2f5a9d2fbeb4a3c081346b3d387d905b82fd2207
Georgii Surkov <georgii.surkov@outlook.com>
2026-03-06 12:36:07 +0000

    TlsCryptoTest: cleanup the code

--- applications/services/tls_crypto/tls_crypto_test.c
+++ applications/services/tls_crypto/tls_crypto_test.c
@@ -7,6 +7,9 @@
 
 #include <tls_crypto/tls_crypto_client.h>
 
+#define INPUT_DATA_SIZE    (800)
+#define SIGNATURE_BUF_SIZE (128)
+
 void cli_command_tls_crypto_test(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
     UNUSED(args);
@@ -14,14 +17,21 @@ void cli_command_tls_crypto_test(PipeSide* pipe, FuriString* args, void* context
 
     TlsCryptoClient* tls = furi_record_open(RECORD_TLS_CRYPTO_CLIENT);
 
-    uint8_t hash[800];
-    uint8_t buf[128];
-    size_t sign_len;
+    uint8_t input_data[INPUT_DATA_SIZE];
+    uint8_t signature_buf[SIGNATURE_BUF_SIZE];
+    size_t signature_len;
 
     while(!cli_is_pipe_broken_or_is_etx_next_char(pipe)) {
-        furi_hal_random_fill_buf(hash, sizeof(hash));
+        furi_hal_random_fill_buf(input_data, sizeof(input_data));
 
-        if(!tls_crypto_client_sign(tls, 1, hash, sizeof(hash), buf, sizeof(buf), &sign_len)) {
+        if(!tls_crypto_client_sign(
+               tls,
+               1,
+               input_data,
+               sizeof(input_data),
+               signature_buf,
+               sizeof(signature_buf),
+               &signature_len)) {
             printf(ANSI_FG_RED "Failed to sign data\r\n" ANSI_RESET);
         }
     }
