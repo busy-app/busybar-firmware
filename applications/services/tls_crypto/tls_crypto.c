f8c87f75592258547377e9405c09b6621b25acec
Georgii Surkov <georgii.surkov@outlook.com>
2026-03-08 12:21:11 +0000

    TlsCrypto: correctly handle event flags

--- applications/services/tls_crypto/tls_crypto.c
+++ applications/services/tls_crypto/tls_crypto.c
@@ -21,19 +21,29 @@ typedef enum {
     TlsCryptoEventFlagResponse = 1UL << 1,
 } TlsCryptoEventFlag;
 
+static void tls_crypto_notify_response(TlsCrypto* instance) {
+    const uint32_t curr_flags = furi_event_flag_get(instance->flags);
+
+    if(curr_flags != 0) {
+        FURI_LOG_W(TAG, "Rogue response from backend");
+        return;
+    }
+
+    const uint32_t flags = furi_event_flag_set(instance->flags, TlsCryptoEventFlagResponse);
+    furi_check((flags & FuriFlagError) == 0);
+}
+
 static void tls_crypto_intercom_rx_callback(const void* data, size_t data_size, void* context) {
     furi_assert(data);
     furi_assert(context);
 
     furi_check(data_size == sizeof(TlsCryptoResponse));
-
-    TlsCrypto* instance = context;
     const TlsCryptoResponse* response = data;
 
+    TlsCrypto* instance = context;
     instance->response = *response;
-    furi_check(
-        furi_event_flag_set(instance->flags, TlsCryptoEventFlagResponse) ==
-        TlsCryptoEventFlagResponse);
+
+    tls_crypto_notify_response(instance);
 }
 
 static void tls_crypto_send_request(const TlsCrypto* instance) {
@@ -84,14 +94,14 @@ static void tls_crypto_build_sign_request(
     memcpy(sign_request->message, message, message_len);
 }
 
-static void tls_crypto_api_acquire(const TlsCrypto* instance) {
+static void tls_crypto_api_lock(const TlsCrypto* instance) {
     furi_check(
         furi_event_flag_wait(
             instance->flags, TlsCryptoEventFlagReady, FuriFlagWaitAny, FuriWaitForever) ==
         TlsCryptoEventFlagReady);
 }
 
-static void tls_crypto_api_release(const TlsCrypto* instance) {
+static void tls_crypto_api_unlock(const TlsCrypto* instance) {
     furi_check(
         furi_event_flag_set(instance->flags, TlsCryptoEventFlagReady) == TlsCryptoEventFlagReady);
 }
@@ -105,6 +115,8 @@ static TlsCrypto* tls_crypto_alloc(void) {
     instance->intercom_ch = intercom_channel_open(
         intercom, IntercomChannelIdTlsCrypto, tls_crypto_intercom_rx_callback, instance);
 
+    tls_crypto_api_unlock(instance);
+
     return instance;
 }
 
@@ -116,7 +128,7 @@ TlsCryptoStatus tls_crypto_get_certificate(
     furi_check(key_id < TlsCryptoKeyIdMax);
     furi_check(certificate);
 
-    tls_crypto_api_acquire(instance);
+    tls_crypto_api_lock(instance);
 
     tls_crypto_build_get_cert_request(instance, key_id);
     tls_crypto_send_request(instance);
@@ -128,7 +140,7 @@ TlsCryptoStatus tls_crypto_get_certificate(
         *certificate = get_cert_response->certificate;
     }
 
-    tls_crypto_api_release(instance);
+    tls_crypto_api_unlock(instance);
 
     return status;
 }
@@ -145,7 +157,7 @@ TlsCryptoStatus tls_crypto_sign(
     furi_check(message_len > 0 && message_len <= TLS_CRYPTO_DATA_LEN_MAX);
     furi_check(signature);
 
-    tls_crypto_api_acquire(instance);
+    tls_crypto_api_lock(instance);
 
     tls_crypto_build_sign_request(instance, key_id, message, message_len);
     tls_crypto_send_request(instance);
@@ -156,8 +168,8 @@ TlsCryptoStatus tls_crypto_sign(
         const TlsCryptoResponseSign* sign_response = &instance->response.sign;
         *signature = sign_response->signature;
     }
-
-    tls_crypto_api_release(instance);
+    // Unlock api requests for the first time
+    tls_crypto_api_unlock(instance);
 
     return status;
 }
