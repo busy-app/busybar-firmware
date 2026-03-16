92a26535bc77f8cc52b7883684cafa5a7d053508
Georgii Surkov <georgii.surkov@outlook.com>
2026-03-16 17:31:55 +0000

    FuriHalSerial: extract dma transaction reset to own function

--- targets/f64/furi_hal/furi_hal_serial.c
+++ targets/f64/furi_hal/furi_hal_serial.c
@@ -174,6 +174,11 @@ static void furi_hal_serial_disable_fifo(FuriHalSerialHandle* handle) {
     periph->SFE = 0;
 }
 
+static void furi_hal_serial_cancel_all_dma_transactions(FuriHalSerialHandle* handle) {
+    USART0_Type* periph = furi_hal_serial_resources[handle->id].periph;
+    periph->DMASA = DMASA_DMA_SOFTWARE_ACK;
+}
+
 void furi_hal_serial_init(FuriHalSerialHandle* handle, uint32_t baud) {
     furi_check(handle);
 
@@ -505,8 +510,7 @@ void furi_hal_serial_dma_init(FuriHalSerialHandle* handle) {
     const FuriHalSerialResources* resources = &furi_hal_serial_resources[handle->id];
 
     FURI_CRITICAL_ENTER();
-    // Abort all ongoing DMA transactions
-    resources->periph->DMASA = DMASA_DMA_SOFTWARE_ACK;
+    furi_hal_serial_cancel_all_dma_transactions(handle);
 
     furi_hal_dma_set_callback(
         resources->dma_tx_channel, furi_hal_serial_dma_tx_irq_callback, handle);
@@ -522,8 +526,7 @@ void furi_hal_serial_dma_deinit(FuriHalSerialHandle* handle) {
     const FuriHalSerialResources* resources = &furi_hal_serial_resources[handle->id];
 
     FURI_CRITICAL_ENTER();
-    // Abort all ongoing DMA transactions
-    resources->periph->DMASA = DMASA_DMA_SOFTWARE_ACK;
+    furi_hal_serial_cancel_all_dma_transactions(handle);
 
     furi_hal_dma_deinit_channel(resources->dma_tx_channel);
     furi_hal_dma_deinit_channel(resources->dma_rx_channel);
