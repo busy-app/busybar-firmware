157e75157ead578d922a3554d2d5e42f3e08a977
Evgenii Egoshin <e.egoshin@flipperdevices.com>
2026-03-20 10:31:58 +0000

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -206,6 +206,7 @@ def pytest_configure(config):
         "feature_web_frontend: Feature 5. Web Frontend",
         "connection_test: Fresh connection tests",
         "schemathesis: OpenAPI schema conformance tests (schemathesis)",
+        "cloud_link: Cloud Link tests",
     ]
 
     for marker in markers:
