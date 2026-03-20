42a812d30f8868afea3a5f502421190f0b77360a
Ivan Mikhailov <imihajlow@gmail.com>
2026-03-20 17:07:44 +0100

    Refactor status streaming

--- applications/services/web_server/http_api/api_status_streaming.c
+++ applications/services/web_server/http_api/api_status_streaming.c
@@ -17,7 +17,7 @@
 #define MAX_CLIENTS_COUNT          (4)
 #define MAX_PUBLISH_MESSAGES_COUNT (4)
 
-#define FRAME_QUEUE_TIMEOUT          (10) // TODO rename
+#define FRAME_QUEUE_TIMEOUT          (10)
 #define FRAME_INTERVAL_MS            (100)
 #define CLIENT_HEARTBEAT_INTERVAL_MS (10000)
 
@@ -27,6 +27,7 @@
 #define WEBSOCKET_TEXT(flags)            (WEBSOCKET_FLAG_TEST(flags, WEBSOCKET_OP_TEXT))
 
 typedef enum {
+    ClientStateHandshake,
     ClientStateIdle,
     ClientStateActive,
     ClientStateRequestingPing,
@@ -39,41 +40,29 @@ typedef struct DataMessage {
     size_t size;
 } DataMessage;
 
+typedef struct StatusStreaming StatusStreaming;
+
 typedef struct {
-    struct mg_connection* conn;
-    struct mg_timer* heartbeat_timer;
+    bool valid;
     ClientState state;
-    void* context;
-    StatePublisherTransportHandle transport_handle;
-    FuriMessageQueue* queue; // Queue of DataMessage
-} ClientCtx;
+    struct mg_connection* conn;
+    StatusStreaming* parent;
 
-LIST_DEF(ClientList, ClientCtx*, M_POD_OPLIST);
+    struct {
+        struct mg_timer* heartbeat_timer;
+        StatePublisherTransportHandle transport_handle;
+        FuriMessageQueue* queue; // Queue of DataMessage
+    } active;
+} Client;
 
-typedef struct {
-    FuriMutex* clients_lock;
-    ClientList_t clients;
+typedef struct StatusStreaming {
+    Client clients[MAX_CLIENTS_COUNT];
+    FuriMutex* client_alloc_mutex; // protects clients[].valid
 
     StatePublisher* state_publisher;
-} StatusStreamingCtx;
+} StatusStreaming;
 
-static ClientCtx* api_streaming_get_client_by_id(
-    StatusStreamingCtx* instance,
-    const unsigned long client_id,
-    ClientList_it_t out_iterator) {
-    ClientList_it_t it;
-    ClientCtx* client = NULL;
-    for(ClientList_it(it, instance->clients); !ClientList_end_p(it); ClientList_next(it)) {
-        ClientCtx* const* it_ptr = ClientList_cref(it);
-        client = *it_ptr;
-        if(client->conn->id != client_id) continue;
-        if(out_iterator) ClientList_it_set(out_iterator, it);
-        break;
-    }
-    return client;
-}
-
-static inline void client_set_state(ClientCtx* client, ClientState new_state) {
+static inline void client_set_state(Client* client, ClientState new_state) {
     STREAM_LOG_D("Client state %d -> %d", client->state, new_state);
     client->state = new_state;
 }
@@ -81,7 +70,7 @@ static inline void client_set_state(ClientCtx* client, ClientState new_state) {
 static void client_heartbeat_timer_callback(void* ctx) {
     STREAM_LOG_D("Heartbeat timer");
 
-    ClientCtx* client = ctx;
+    Client* client = ctx;
 
     ClientState new_state = ClientStateInvalid;
     if(client->state != ClientStateWaitingPong && client->state != ClientStateInvalid) {
@@ -95,11 +84,15 @@ static void client_heartbeat_timer_callback(void* ctx) {
 }
 
 static void client_publish_callback(SharedPtr* data, size_t data_size, void* context) {
-    ClientCtx* client = context;
+    Client* client = context;
+
+    furi_assert(client->state > ClientStateHandshake);
 
     shared_ptr_acquire(data);
     FuriStatus error = furi_message_queue_put(
-        client->queue, &(DataMessage){.data = data, .size = data_size}, FRAME_QUEUE_TIMEOUT);
+        client->active.queue,
+        &(DataMessage){.data = data, .size = data_size},
+        FRAME_QUEUE_TIMEOUT);
     if(error != FuriStatusOk) {
         FURI_LOG_E(TAG, "Queue overflow, update skipped (%u)", error);
         shared_ptr_release(data);
@@ -107,40 +100,68 @@ static void client_publish_callback(SharedPtr* data, size_t data_size, void* con
     mg_wakeup(web_srv_get_mgr(), client->conn->id, NULL, 0);
 }
 
-static inline void client_free(ClientCtx* client) {
-    mg_timer_free(&client->conn->mgr->timers, client->heartbeat_timer);
-    free(client->heartbeat_timer);
-    DataMessage msg;
-    while(furi_message_queue_get(client->queue, &msg, 0) == FuriStatusOk) {
-        shared_ptr_release(msg.data);
+static Client* client_alloc(StatusStreaming* instance, struct mg_connection* conn) {
+    Client* result = NULL;
+    furi_mutex_acquire(instance->client_alloc_mutex, FuriWaitForever);
+    for(size_t i = 0; i != COUNT_OF(instance->clients); ++i) {
+        Client* client = instance->clients + i;
+        if(!client->valid) {
+            client->valid = true;
+            client->conn = conn;
+            client->state = ClientStateHandshake;
+            client->parent = instance;
+            result = client;
+            break;
+        }
     }
-    furi_message_queue_free(client->queue);
-    free(client);
+    furi_mutex_release(instance->client_alloc_mutex);
+    return result;
+}
+
+static inline void client_free(Client* client, StatusStreaming* streaming_ctx) {
+    if(client->valid) {
+        switch(client->state) {
+        case ClientStateIdle:
+        case ClientStateActive:
+        case ClientStateRequestingPing:
+        case ClientStateWaitingPong:
+        case ClientStateInvalid: {
+            state_publisher_del_transport(
+                streaming_ctx->state_publisher, client->active.transport_handle);
+            mg_timer_free(&client->conn->mgr->timers, client->active.heartbeat_timer);
+            free(client->active.heartbeat_timer);
+            DataMessage msg;
+            while(furi_message_queue_get(client->active.queue, &msg, 0) == FuriStatusOk) {
+                shared_ptr_release(msg.data);
+            }
+            furi_message_queue_free(client->active.queue);
+        } break;
+        case ClientStateHandshake:
+            break;
+        }
+        client->valid = false;
+    }
 }
 
 static void client_connection_open(struct mg_connection* conn) {
     ConnectionContext* conn_ctx = (void*)conn->data;
-    StatusStreamingCtx* instance = conn_ctx->context;
+    Client* client = conn_ctx->context;
+    furi_assert(client);
+    StatusStreaming* instance = client->parent;
     furi_assert(instance);
 
-    ClientCtx* client = malloc(sizeof(ClientCtx));
-    client->conn = conn;
-    client->state = ClientStateActive;
-    client->context = conn->data;
-    client->heartbeat_timer = mg_timer_add(
+    furi_assert(client->state == ClientStateHandshake);
+    client_set_state(client, ClientStateActive);
+    client->active.heartbeat_timer = mg_timer_add(
         conn->mgr,
         CLIENT_HEARTBEAT_INTERVAL_MS,
         MG_TIMER_REPEAT,
         client_heartbeat_timer_callback,
         client);
-    client->queue = furi_message_queue_alloc(MAX_PUBLISH_MESSAGES_COUNT, sizeof(DataMessage));
+    client->active.queue =
+        furi_message_queue_alloc(MAX_PUBLISH_MESSAGES_COUNT, sizeof(DataMessage));
 
-    // Add connection to WebSocket clients list
-    furi_mutex_acquire(instance->clients_lock, FuriWaitForever);
-    ClientList_push_back(instance->clients, client);
-    furi_mutex_release(instance->clients_lock);
-
-    client->transport_handle = state_publisher_add_transport(
+    client->active.transport_handle = state_publisher_add_transport(
         instance->state_publisher,
         StatePublisherTransportClassWebSocket,
         FRAME_INTERVAL_MS,
@@ -151,24 +172,19 @@ static void client_connection_open(struct mg_connection* conn) {
 }
 
 static void client_connection_close(struct mg_connection* conn) {
-    // Get handler context from connection data
     ConnectionContext* conn_ctx = (void*)conn->data;
-    StatusStreamingCtx* instance = conn_ctx->context;
+    Client* client = conn_ctx->context;
+    furi_assert(client);
+    StatusStreaming* instance = client->parent;
     furi_assert(instance);
 
-    // Remove connection from WebSocket clients list
-    ClientList_it_t it;
-    ClientCtx* client = api_streaming_get_client_by_id(instance, conn->id, it);
     if(client) {
         STREAM_LOG_D("Remove client: %ld", client->conn->id);
-        furi_mutex_acquire(instance->clients_lock, FuriWaitForever);
-        ClientList_remove(instance->clients, it);
-        furi_mutex_release(instance->clients_lock);
-        state_publisher_del_transport(instance->state_publisher, client->transport_handle);
-        client_free(client);
+        furi_mutex_acquire(instance->client_alloc_mutex, FuriWaitForever);
+        client_free(client, instance);
+        furi_mutex_release(instance->client_alloc_mutex);
     }
 
-    // Clear connection callbacks
     conn_ctx->ws.on_open = NULL;
     conn_ctx->ws.on_message = NULL;
     conn_ctx->on_close = NULL;
@@ -179,12 +195,12 @@ static void client_send_frame(struct mg_connection* conn, void* data, size_t len
     furi_assert(conn);
 
     ConnectionContext* conn_ctx = (void*)conn->data;
-    StatusStreamingCtx* instance = conn_ctx->context;
+    Client* client = conn_ctx->context;
 
-    ClientCtx* client = api_streaming_get_client_by_id(instance, conn->id, NULL);
     if(client->state == ClientStateActive) {
         DataMessage msg;
-        if(furi_message_queue_get(client->queue, &msg, FRAME_QUEUE_TIMEOUT) == FuriStatusOk) {
+        if(furi_message_queue_get(client->active.queue, &msg, FRAME_QUEUE_TIMEOUT) ==
+           FuriStatusOk) {
             mg_ws_send(conn, msg.data->inner, msg.size, WEBSOCKET_OP_BINARY);
             shared_ptr_release(msg.data);
         } else {
@@ -203,17 +219,16 @@ static void client_on_message(struct mg_connection* conn, struct mg_ws_message* 
     furi_assert(conn);
 
     ConnectionContext* conn_ctx = (void*)conn->data;
-    StatusStreamingCtx* instance = conn_ctx->context;
-    ClientCtx* const client = api_streaming_get_client_by_id(instance, conn->id, NULL);
+    Client* client = conn_ctx->context;
 
     if(WEBSOCKET_PING(ws_msg->flags)) {
         STREAM_LOG_D("PING");
         client_set_state(client, ClientStateActive);
-        client->heartbeat_timer->expire = mg_now() + CLIENT_HEARTBEAT_INTERVAL_MS;
+        client->active.heartbeat_timer->expire = mg_now() + CLIENT_HEARTBEAT_INTERVAL_MS;
     } else if(WEBSOCKET_PONG(ws_msg->flags)) {
         STREAM_LOG_D("PONG");
         client_set_state(client, ClientStateActive);
-        client->heartbeat_timer->expire = mg_now() + CLIENT_HEARTBEAT_INTERVAL_MS;
+        client->active.heartbeat_timer->expire = mg_now() + CLIENT_HEARTBEAT_INTERVAL_MS;
     } else if(WEBSOCKET_TEXT(ws_msg->flags)) {
         STREAM_LOG_D("MSG");
     }
@@ -228,27 +243,27 @@ bool http_api_status_ws_callback(
 
     if(!IS_HTTP_ENDPOINT(path)) return false;
 
-    StatusStreamingCtx* instance = ctx;
+    StatusStreaming* instance = ctx;
 
     bool success = false;
     do {
         bool is_ws_upgrade = (mg_http_get_header(msg, "Sec-WebSocket-Key") != NULL);
         if(!is_ws_upgrade) break;
 
-        if(ClientList_size(instance->clients) >= MAX_CLIENTS_COUNT) {
-            MG_REPLY_ERROR(conn, 400, "Exceed max clients count");
+        Client* client = client_alloc(instance, conn);
+
+        if(!client) {
+            MG_REPLY_ERROR(conn, 400, "Exceeded max clients count");
             break;
         }
 
-        // Assign connection callbacks
         ConnectionContext* conn_ctx = (void*)conn->data;
         conn_ctx->ws.on_open = client_connection_open;
         conn_ctx->on_close = client_connection_close;
         conn_ctx->ws.on_message = client_on_message;
         conn_ctx->on_wakeup = client_send_frame;
-        conn_ctx->context = instance;
+        conn_ctx->context = client;
 
-        // Upgrade connection to WebSocket
         mg_ws_upgrade(conn, msg, NULL);
 
         success = true;
@@ -258,9 +273,11 @@ bool http_api_status_ws_callback(
 }
 
 void* http_api_status_ws_alloc(void) {
-    StatusStreamingCtx* instance = malloc(sizeof(StatusStreamingCtx));
-    instance->clients_lock = furi_mutex_alloc(FuriMutexTypeNormal);
-    ClientList_init(instance->clients);
+    StatusStreaming* instance = malloc(sizeof(StatusStreaming));
+    instance->client_alloc_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    for(size_t i = 0; i != COUNT_OF(instance->clients); ++i) {
+        instance->clients[i].valid = false;
+    }
 
     instance->state_publisher = furi_record_open(RECORD_STATE_PUBLISHER);
 
@@ -269,17 +286,15 @@ void* http_api_status_ws_alloc(void) {
 
 void http_api_status_ws_free(void* ctx) {
     furi_assert(ctx);
-    StatusStreamingCtx* instance = ctx;
-    ClientList_it_t it;
-    for(ClientList_it(it, instance->clients); !ClientList_end_p(it); ClientList_next(it)) {
-        ClientCtx* const* it_ptr = ClientList_cref(it);
-        ClientCtx* client = *it_ptr;
-        state_publisher_del_transport(instance->state_publisher, client->transport_handle);
-        client_free(client);
+    StatusStreaming* instance = ctx;
+    furi_mutex_acquire(instance->client_alloc_mutex, FuriWaitForever);
+    for(size_t i = 0; i != COUNT_OF(instance->clients); ++i) {
+        Client* client = instance->clients + i;
+        client_free(client, instance);
     }
+    furi_mutex_release(instance->client_alloc_mutex);
     furi_record_close(RECORD_STATE_PUBLISHER);
 
-    ClientList_clear(instance->clients);
-    furi_mutex_free(instance->clients_lock);
+    furi_mutex_free(instance->client_alloc_mutex);
     free(instance);
 }
