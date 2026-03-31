c0e2b200a495ca8be29ef94a544d096220417ab1
Georgii Surkov <georgii.surkov@outlook.com>
2026-03-31 17:14:25 +0100

    Matter: update comments

--- applications/services/matter/matter.h
+++ applications/services/matter/matter.h
@@ -57,91 +57,112 @@ typedef struct {
 } MatterCommissionedFabrics;
 
 /**
- * @brief Gets a PubSub to listen to Matter events
+ * @brief Get a PubSub to listen to Matter events.
+ *
+ * The returned FuriPubSub object witll have an underlying type of MatterEvent.
+ * Use furi_pubsub_subscribe() to get notifications about new events.
  * 
- * @param[in] matter Service instance
- * 
- * @returns FuriPubSub handle. Events are of type `MatterEvent`.
+ * @param[in,out] instance pointer to the service instance
+ *
+ * @returns pointer to a FuriPubSub object
  */
-FuriPubSub* matter_get_pubsub(MatterSrv* matter);
+FuriPubSub* matter_get_pubsub(MatterSrv* instance);
 
 /**
- * TODO: description
+ * @brief Get the switch state object that supports change notifications.
+ *
+ * The returned FuriState object will have an underlying type of MatterSwitchState.
+ * Use furi_state_subscribe() to get notifications about changes in the current switch state.
+ *
+ * This function never blocks.
+ *
+ * @param[in,out] instance pointer to the service instance
+ *
+ * @returns pointer to a FuriState object
  */
-FuriState* matter_get_switch_state(MatterSrv* matter);
+FuriState* matter_get_switch_state(MatterSrv* instance);
 
 /**
- * @brief Sets the state of the Matter switch
+ * @brief Set the state of the Matter switch.
+ *
+ * @note Passing @c MatterSwitchStateUnknown as @c switch_state does nothing.
+ *
+ * @param[in,out] instance pointer to the service instance
+ * @param[in] switch_state required Matter switch state
+ *
+ * @returns @c MatterStatusOk on success, any other value from @c MatterStatus enum on error
  */
-MatterStatus matter_set_switch_state(MatterSrv* matter, MatterSwitchState switch_state);
+MatterStatus matter_set_switch_state(MatterSrv* instance, MatterSwitchState switch_state);
 
 /**
  * @brief Set the startup mode of the Matter switch
  *
- * @param[in] matter Service instance
- * @param[in] mode Desired Matter switch startup mode
- * 
- * @returns true on success
- */
-MatterStatus matter_set_switch_startup_mode(MatterSrv* matter, MatterSwitchStartupMode mode);
-
-/**
- * @brief Enables Matter commissioning
- * 
- * @param[in] matter Service instance
+ * @param[in,out] instance pointer to the service instance
+ * @param[in] mode required Matter switch startup mode
  *
- * TODO: Complete description
+ * @returns @c MatterStatusOk on success, any other value from @c MatterStatus enum on error
  */
-MatterStatus matter_enable_commissioning(MatterSrv* matter, MatterCommissioningInfo* info);
+MatterStatus matter_set_switch_startup_mode(MatterSrv* instance, MatterSwitchStartupMode mode);
 
 /**
- * @brief Brief information about the commissioning status
+ * @brief Enable Matter commissioning (open the commissioning window).
  * 
- * @param[in] matter Service instance
+ * @param[in,out] instance pointer to the service instance
+ * @param[out] info pointer to the object to contain the information about the current commissioning window
+ *
+ * @returns @c MatterStatusOk on success, any other value from @c MatterStatus enum on error
+ */
+MatterStatus matter_enable_commissioning(MatterSrv* instance, MatterCommissioningInfo* info);
+
+/**
+ * @brief Get the brief information about the commissioning status.
  * 
- * TODO: Complete description
+ * @param[in,out] instance pointer to the service instance
+ * @param[out] fabrics pointer to the object to contain the information about commissioned fabrics
+ * 
+ * @returns @c MatterStatusOk on success, any other value from @c MatterStatus enum on error
  */
 MatterStatus
-    matter_get_commissioned_fabrics(MatterSrv* matter, MatterCommissionedFabrics* fabrics);
+    matter_get_commissioned_fabrics(MatterSrv* instance, MatterCommissionedFabrics* fabrics);
 
 /**
- * @brief Deletes all Matter data
+ * @brief Delete all Matter pairing data.
  *
- * @param[in] matter Service instance
+ * @param[in,out] instance pointer to the service instance
  *
- * @returns true on success
+ * @returns @c MatterStatusOk on success, any other value from @c MatterStatus enum on error
  */
-MatterStatus matter_factory_reset(MatterSrv* matter);
+MatterStatus matter_factory_reset(MatterSrv* instance);
 
 /**
- * @brief Gets the currently selected Certification Declaration
+ * @brief Get the currently selected Certification Declaration.
  * 
- * @param[in] matter Service instance
+ * @param[in] instance Service instance
  * 
  * @returns Name of the Certification Declaration variant currently marked as "wanted"
  */
-const char* matter_get_wanted_cd_selection(MatterSrv* matter);
+const char* matter_get_wanted_cd_selection(MatterSrv* instance);
 
 /**
- * @brief Sets the currently selected Certification Declaration
+ * @brief Set the currently selected Certification Declaration.
  * 
- * @param[in] matter Service instance
+ * @param[in,out] instance pointer to the service instance
  * @param[in] selection Wanted selection
  * 
  * @warning Changes will only apply after a reboot
  * 
  * @returns true on success
  */
-MatterStatus matter_set_wanted_cd_selection(MatterSrv* matter, const char* selection);
+MatterStatus matter_set_wanted_cd_selection(MatterSrv* instance, const char* selection);
 
 /**
- * @brief Gets the currently de-facto active Certification Declaration
+ * @brief Get the currently de-facto active Certification Declaration.
  * 
- * @param[in] matter Service instance
+ * @param[in,out] instance pointer to the service instance
  * 
  * @returns name of the Certification Declaration variant that Matter is currently using
  */
-const char* matter_get_de_facto_cd_selection(MatterSrv* matter);
+const char* matter_get_de_facto_cd_selection(MatterSrv* instance);
 
 #ifdef __cplusplus
 }
