a16aa493faccd5dbf9e2d8072d91908e73b454aa
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2026-04-07 17:35:24 +0300

    fix: Post-update modal

--- assets/frontend/components/Tab/Firmware/CardFirmware.vue
+++ assets/frontend/components/Tab/Firmware/CardFirmware.vue
@@ -121,6 +121,9 @@ watch(() => firmwareStore.fileUpdate.stage, newStage => {
     if (newStage === UpdateStage.SUCCESS) {
       if (updatePollingInterval.value) {
         clearInterval(updatePollingInterval.value);
+
+        localStorage.setItem('successfulUpdate', 'true');
+        window.location.reload();
       }
       return;
     }
@@ -139,9 +142,6 @@ watch(() => firmwareStore.fileUpdate.stage, newStage => {
         clearInterval(updatePollingInterval.value!);
         firmwareStore.fileUpdate.stage = UpdateStage.SUCCESS;
         firmwareStore.fileUpdate.progress = 0;
-
-        localStorage.setItem('successfulUpdate', 'true');
-        window.location.reload();
       })
       .catch(() => {
         // ignore the error
