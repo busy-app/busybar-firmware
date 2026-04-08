d382cd6f5f2a6021798891369e85d6639b20c8bd
Georgii Surkov <georgii.surkov@outlook.com>
2026-04-08 18:57:01 +0100

    StatusLights: refactor cli

--- applications/services/cli_u5/cli_command_status_lights.c
+++ applications/services/cli_u5/cli_command_status_lights.c
@@ -10,27 +10,52 @@ static void cli_command_status_lights_print_usage(void) {
     printf("status_lights <0-255> <0-255> <0-255>\r\n");
 }
 
+#ifdef SRV_STATUS_LIGHTS
+static bool cli_command_status_lights_parse_color(FuriString* args, Color* color) {
+    bool success = true;
+
+    int value[3];
+
+    for(uint32_t i = 0; i < COUNT_OF(value); i++) {
+        if(!args_read_int_and_trim(args, &value[i])) {
+            success = false;
+            break;
+        }
+
+        if((value[i] < 0) || (value[i] > UINT8_MAX)) {
+            success = false;
+            break;
+        };
+    }
+
+    if(success) {
+        *color = (Color)COLOR_MAKE_RGB(value[0], value[1], value[2]);
+    }
+
+    return success;
+}
+#endif // SRV_STATUS_LIGHTS
+
 void cli_command_status_lights(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
     UNUSED(context);
 
 #ifdef SRV_STATUS_LIGHTS
-    int value[3] = {};
+    Color color;
 
-    size_t i = 0;
-    for(i = 0; i < COUNT_OF(value); i++) {
-        if(!args_read_int_and_trim(args, &value[i])) break;
-        if((value[i] < 0) || (value[i] > 255)) break;
-    }
-    if(i != COUNT_OF(value)) {
+    if(!cli_command_status_lights_parse_color(args, &color)) {
         cli_command_status_lights_print_usage();
         return;
     }
 
-    Color color = COLOR_MAKE_RGB(value[0], value[1], value[2]);
     StatusLights* status_lights = furi_record_open(RECORD_STATUS_LIGHTS);
-    status_lights_run_preset(status_lights, StatusLightsPresetStaticColor, color);
+    const StatusLightsStatus status =
+        status_lights_run_preset(status_lights, StatusLightsPresetStaticColor, color);
     furi_record_close(RECORD_STATUS_LIGHTS);
+
+    if(status != StatusLightsStatusOk) {
+        printf("Failed to set status lights color\r\n");
+    }
 #else // SRV_STATUS_LIGHTS
     UNUSED(args);
     UNUSED(cli_command_status_lights_print_usage);
