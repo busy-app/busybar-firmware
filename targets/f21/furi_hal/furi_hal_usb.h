3ac71f10f9660039c26244297320e32d379015af
Georgii Surkov <georgii.surkov@outlook.com>
2026-04-14 16:54:22 +0100

    FuriHal: minor styling fixes

--- targets/f21/furi_hal/furi_hal_usb.h
+++ targets/f21/furi_hal/furi_hal_usb.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <furi_hal_interrupt.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void furi_hal_usb_init(void);
 
 void furi_hal_usb_set_irq(FuriHalInterruptISR usb_isr, void* isr_ctx);
+
+#ifdef __cplusplus
+}
+#endif
