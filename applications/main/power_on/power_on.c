1f810a2d91af07e286095fc11b6f6502b925cd1e
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-06 12:24:22 +0100

    PowerOn: break up everything to functions

--- applications/main/power_on/power_on.c
+++ applications/main/power_on/power_on.c
@@ -9,8 +9,8 @@
 
 #define TAG "PowerOn"
 
-#define POWER_ON_START_TIMEOUT_TICKS furi_ms_to_ticks(500)
-#define POWER_ON_APP_TIMEOUT_MIN     (15)
+#define POWER_ON_START_TIMEOUT_MS (500)
+#define POWER_ON_APP_TIMEOUT_MIN  (15)
 
 #define MIN_TO_MS(minutes)    (minutes * 60U * 1000U)
 #define POWER_ON_LOOP_SECTION "loop"
@@ -19,21 +19,20 @@
 #define POWER_ON_DONE_PATH       APP_DATA_PATH("done.txt")
 
 typedef enum {
-    PowerOnAppThreadFlagExitToMenu = 1 << 0,
-    PowerOnAppThreadFlagExitToTransportMode = 1 << 1,
+    PowerOnAppThreadFlagExit = 1 << 0,
+    PowerOnAppThreadFlagShutdown = 1 << 1,
     PowerOnAppThreadFlagDeviceStarted = 1 << 2,
 } PowerOnAppThreadFlag;
 
-#define POWER_ON_APP_ANIMATION_FLAGS                                            \
-    (PowerOnAppThreadFlagExitToMenu | PowerOnAppThreadFlagExitToTransportMode | \
-     PowerOnAppThreadFlagDeviceStarted)
+#define POWER_ON_APP_ANIMATION_FLAGS \
+    (PowerOnAppThreadFlagExit | PowerOnAppThreadFlagShutdown | PowerOnAppThreadFlagDeviceStarted)
 
 typedef struct {
     Gui* gui;
     Power* power;
     Storage* storage;
     FuriThreadId thread_id;
-    FuriTimer* back_to_transport_timer;
+    FuriTimer* shutdown_timer;
     Label* labels[GuiDisplayIdMax];
     AnimPlayer* anims[GuiDisplayIdMax];
 } PowerOnApp;
@@ -54,7 +53,7 @@ static bool power_on_input_callback(const InputEvent* event, void* context) {
         case InputKeyOk:
         case InputKeyBack:
         case InputKeyStart:
-            furi_thread_flags_set(instance->thread_id, PowerOnAppThreadFlagExitToMenu);
+            furi_thread_flags_set(instance->thread_id, PowerOnAppThreadFlagExit);
             consumed = true;
             break;
         default:
@@ -65,9 +64,9 @@ static bool power_on_input_callback(const InputEvent* event, void* context) {
     return consumed;
 }
 
-static void back_to_transport_timer_callback(void* ctx) {
+static void power_on_shutdown_timer_callback(void* ctx) {
     PowerOnApp* instance = ctx;
-    furi_thread_flags_set(instance->thread_id, PowerOnAppThreadFlagExitToTransportMode);
+    furi_thread_flags_set(instance->thread_id, PowerOnAppThreadFlagShutdown);
 }
 
 static bool power_on_thread_signal(uint32_t signal, void* arg, void* context) {
@@ -95,10 +94,10 @@ static PowerOnApp* power_on_app_alloc(void) {
     instance->storage = furi_record_open(RECORD_STORAGE);
 
     instance->thread_id = furi_thread_get_current_id();
-    instance->back_to_transport_timer =
-        furi_timer_alloc(back_to_transport_timer_callback, FuriTimerTypeOnce, instance);
+    instance->shutdown_timer =
+        furi_timer_alloc(power_on_shutdown_timer_callback, FuriTimerTypeOnce, instance);
     furi_timer_start(
-        instance->back_to_transport_timer, furi_ms_to_ticks(MIN_TO_MS(POWER_ON_APP_TIMEOUT_MIN)));
+        instance->shutdown_timer, furi_ms_to_ticks(MIN_TO_MS(POWER_ON_APP_TIMEOUT_MIN)));
 
     furi_thread_set_signal_callback(furi_thread_get_current(), power_on_thread_signal, instance);
 
@@ -106,95 +105,7 @@ static PowerOnApp* power_on_app_alloc(void) {
 }
 
 static void power_on_app_free(PowerOnApp* instance) {
-    furi_record_close(RECORD_STORAGE);
-    furi_record_close(RECORD_POWER);
-    furi_record_close(RECORD_GUI);
-
-    furi_timer_free(instance->back_to_transport_timer);
-    free(instance);
-}
-
-static inline bool power_on_done_flag_present(PowerOnApp* instance) {
-    return storage_file_exists(instance->storage, POWER_ON_DONE_PATH);
-}
-
-static inline void power_on_done_flag_create(PowerOnApp* instance) {
-    File* file = storage_file_alloc(instance->storage);
-
-    if(!storage_file_open(file, POWER_ON_DONE_PATH, FSAM_WRITE, FSOM_CREATE_ALWAYS))
-        FURI_LOG_W(TAG, "Failed to create file");
-
-    storage_file_close(file);
-    storage_file_free(file);
-}
-
-static AnimPlayer* power_on_animation_alloc(Widget* widget, GuiDisplayId display_id) {
-    AnimPlayer* anim = anim_player_alloc(widget);
-
-    if(anim_player_set_source(anim, power_on_anim_paths[display_id])) {
-        anim_player_set_section(
-            anim, AnimFilePlayFlagFinishCurrent | AnimFilePlayFlagLoop, POWER_ON_LOOP_SECTION);
-    }
-
-    return anim;
-}
-
-int32_t power_on_app(void* arg) {
-    UNUSED(arg);
-
-    PowerOnApp* instance = power_on_app_alloc();
-
-    // avoid showing text for <500ms
-    uint32_t wanted_flags = PowerOnAppThreadFlagDeviceStarted;
-    uint32_t flags =
-        furi_thread_flags_wait(wanted_flags, FuriFlagWaitAny, POWER_ON_START_TIMEOUT_TICKS);
-
-    if(flags == FuriFlagErrorTimeout) {
-        with_gui(instance->gui, {
-            GuiLayer* layer_main = gui_get_layer(instance->gui, GuiLayerIdMain);
-
-            for(GuiDisplayId id = 0; id < GuiDisplayIdMax; ++id) {
-                Widget* root = gui_layer_get_root_widget(layer_main, id);
-
-                Label* label = label_alloc(root);
-                label_set_text(label, "Starting...");
-                widget_set_align(label_get_base(label), AlignCenter);
-
-                instance->labels[id] = label;
-            }
-        });
-
-        furi_thread_flags_wait(wanted_flags, FuriFlagWaitAny, FuriWaitForever);
-
-    } else {
-        furi_check(!(flags & FuriFlagError));
-    }
-
-    do {
-        if(power_on_done_flag_present(instance)) break;
-
-        with_gui(instance->gui, {
-            GuiLayer* layer_main = gui_get_layer(instance->gui, GuiLayerIdMain);
-            gui_layer_add_input_callback(layer_main, power_on_input_callback, instance);
-
-            for(GuiDisplayId id = 0; id < GuiDisplayIdMax; ++id) {
-                Widget* root = gui_layer_get_root_widget(layer_main, id);
-                instance->anims[id] = power_on_animation_alloc(root, id);
-            }
-        });
-
-        uint32_t flags =
-            furi_thread_flags_wait(POWER_ON_APP_ANIMATION_FLAGS, FuriFlagWaitAny, FuriWaitForever);
-
-        if(flags & PowerOnAppThreadFlagExitToTransportMode) {
-            power_off(instance->power);
-        }
-
-        if(flags & PowerOnAppThreadFlagExitToMenu) {
-            furi_timer_stop(instance->back_to_transport_timer);
-            power_on_done_flag_create(instance);
-        }
-    } while(0);
+    furi_timer_free(instance->shutdown_timer);
 
     with_gui(instance->gui, {
         for(GuiDisplayId id = 0; id < GuiDisplayIdMax; ++id) {
@@ -211,6 +122,108 @@ int32_t power_on_app(void* arg) {
         gui_layer_remove_input_callback(layer_main, power_on_input_callback);
     });
 
+    furi_record_close(RECORD_STORAGE);
+    furi_record_close(RECORD_POWER);
+    furi_record_close(RECORD_GUI);
+
+    free(instance);
+}
+
+static inline bool power_on_done_flag_present(PowerOnApp* instance) {
+    return storage_file_exists(instance->storage, POWER_ON_DONE_PATH);
+}
+
+static inline void power_on_done_flag_create(PowerOnApp* instance) {
+    File* file = storage_file_alloc(instance->storage);
+
+    if(!storage_file_open(file, POWER_ON_DONE_PATH, FSAM_WRITE, FSOM_CREATE_ALWAYS)) {
+        FURI_LOG_W(TAG, "Failed to create file");
+    }
+
+    storage_file_close(file);
+    storage_file_free(file);
+}
+
+static AnimPlayer* power_on_animation_alloc(Widget* widget, GuiDisplayId display_id) {
+    AnimPlayer* anim = anim_player_alloc(widget);
+
+    if(anim_player_set_source(anim, power_on_anim_paths[display_id])) {
+        anim_player_set_section(
+            anim, AnimFilePlayFlagFinishCurrent | AnimFilePlayFlagLoop, POWER_ON_LOOP_SECTION);
+    }
+
+    return anim;
+}
+
+static void power_on_show_startup_message(PowerOnApp* instance) {
+    with_gui(instance->gui, {
+        GuiLayer* layer_main = gui_get_layer(instance->gui, GuiLayerIdMain);
+
+        for(GuiDisplayId id = 0; id < GuiDisplayIdMax; ++id) {
+            Widget* root = gui_layer_get_root_widget(layer_main, id);
+
+            Label* label = label_alloc(root);
+            label_set_text(label, "Starting...");
+            widget_set_align(label_get_base(label), AlignCenter);
+
+            instance->labels[id] = label;
+        }
+    });
+}
+
+static void power_on_show_first_boot_animation(PowerOnApp* instance) {
+    with_gui(instance->gui, {
+        GuiLayer* layer_main = gui_get_layer(instance->gui, GuiLayerIdMain);
+        gui_layer_add_input_callback(layer_main, power_on_input_callback, instance);
+
+        for(GuiDisplayId id = 0; id < GuiDisplayIdMax; ++id) {
+            Widget* root = gui_layer_get_root_widget(layer_main, id);
+            instance->anims[id] = power_on_animation_alloc(root, id);
+        }
+    });
+}
+
+static void power_on_wait_for_start_condition(PowerOnApp* instance) {
+    // avoid showing text for < 500ms
+    const uint32_t wanted_flags = PowerOnAppThreadFlagDeviceStarted;
+    const uint32_t flags = furi_thread_flags_wait(
+        wanted_flags, FuriFlagWaitAny, furi_ms_to_ticks(POWER_ON_START_TIMEOUT_MS));
+
+    if(flags == FuriFlagErrorTimeout) {
+        power_on_show_startup_message(instance);
+        furi_thread_flags_wait(wanted_flags, FuriFlagWaitAny, FuriWaitForever);
+    } else {
+        furi_check((flags & FuriFlagError) == 0);
+    }
+}
+
+static void power_on_wait_for_exit_condition(PowerOnApp* instance) {
+    const uint32_t flags =
+        furi_thread_flags_wait(POWER_ON_APP_ANIMATION_FLAGS, FuriFlagWaitAny, FuriWaitForever);
+
+    if(flags & PowerOnAppThreadFlagShutdown) {
+        power_off(instance->power);
+    }
+
+    if(flags & PowerOnAppThreadFlagExit) {
+        furi_timer_stop(instance->shutdown_timer);
+        power_on_done_flag_create(instance);
+    }
+}
+
+int32_t power_on_app(void* arg) {
+    UNUSED(arg);
+
+    PowerOnApp* instance = power_on_app_alloc();
+
+    power_on_wait_for_start_condition(instance);
+
+    if(!power_on_done_flag_present(instance)) {
+        power_on_show_first_boot_animation(instance);
+        power_on_wait_for_exit_condition(instance);
+    }
+
     power_on_app_free(instance);
+
     return 0;
 }
