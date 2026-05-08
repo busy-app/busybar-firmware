0c590761477731971c28975dcf37364f7e280305
Ivan Shestakov <vanyww2@gmail.com>
2026-05-08 20:08:07 +0400

    Update some icons (remove outdated ones), used mostly by status view

--- applications/settings/about/scenes/about_scene_compliance.c
+++ applications/settings/about/scenes/about_scene_compliance.c
@@ -50,7 +50,7 @@ static void about_scene_compliance_on_enter(void* context) {
 
     with_gui(instance->gui, {
         scene->front_status_view = status_view_alloc(instance->front_scene_window);
-        status_view_set_icon(scene->front_status_view, SETTINGS_IMG_PATH("info_front_7x7.image"));
+        status_view_set_icon(scene->front_status_view, SETTINGS_IMG_PATH("info_front_8x8.image"));
         status_view_set_header(scene->front_status_view, "Look at back\nscreen");
 
         scene->info_flex = flex_layout_alloc(instance->back_scene_window, FlexLayoutTypeColumn);
