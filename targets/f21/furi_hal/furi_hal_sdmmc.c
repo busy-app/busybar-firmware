a4943bc92c6bc1be9cb9c0931cd7c9126f8884e9
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-08 15:24:51 +0100

    FuriHalSdmmc: fix timeout calculation

--- targets/f21/furi_hal/furi_hal_sdmmc.c
+++ targets/f21/furi_hal/furi_hal_sdmmc.c
@@ -1116,7 +1116,7 @@ SdCardState sdmmc_get_card_state(void) {
 }
 
 static bool sdmmc_wait_for_transfer_state(size_t timeout_ms) {
-    PreciseTimer timer = precise_timer_create(timeout_ms);
+    PreciseTimer timer = precise_timer_create(timeout_ms * 1000);
     SdCardState card_state = sdmmc_get_card_state();
     while(card_state != SdCardStateTransfer) {
         if(precise_timer_is_expired(timer)) {
