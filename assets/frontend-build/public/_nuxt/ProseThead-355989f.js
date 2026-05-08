4d3cfb2e6e7451a9bbe7527480a85cf2001061de
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2026-05-08 20:58:20 +0300

    frontend: build

---
+++ assets/frontend-build/public/_nuxt/ProseThead-355989f.js
@@ -0,0 +1,1 @@
+import{_ as r,c as o,o as t,r as s}from"./entry-355989f.js";const a={};function c(e,n){return t(),o("thead",null,[s(e.$slots,"default")])}const _=r(a,[["render",c]]);export{_ as default};
