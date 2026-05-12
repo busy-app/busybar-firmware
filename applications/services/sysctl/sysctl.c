6fe3f9b1f3d2ebe9b0f54b14a9ec0ed3df4bada2
hedger <hedger@nanode.su>
2026-05-12 13:24:57 +0300

    sysctl: thread safety

--- applications/services/sysctl/sysctl.c
+++ applications/services/sysctl/sysctl.c
@@ -1,26 +1,43 @@
 #include "sysctl.h"
 #include "settings/sysctl_settings.h"
+#include <furi.h>
 
-static SysctlSettings s_settings;
+typedef struct {
+    SysctlSettings settings;
+    FuriMutex* mutex;
+} SysctlState;
+
+static SysctlState s_state;
 
 bool sysctl_get_cli_wifi_enabled(void) {
-    return s_settings.cli_wifi_enabled;
+    furi_mutex_acquire(s_state.mutex, FuriWaitForever);
+    bool value = s_state.settings.cli_wifi_enabled;
+    furi_mutex_release(s_state.mutex);
+    return value;
 }
 
 void sysctl_set_cli_wifi_enabled(bool enabled) {
-    s_settings.cli_wifi_enabled = enabled;
-    sysctl_settings_save(&s_settings);
+    furi_mutex_acquire(s_state.mutex, FuriWaitForever);
+    s_state.settings.cli_wifi_enabled = enabled;
+    sysctl_settings_save(&s_state.settings);
+    furi_mutex_release(s_state.mutex);
 }
 
 int sysctl_get_websrv_accesslog_level(void) {
-    return s_settings.websrv_accesslog_level;
+    furi_mutex_acquire(s_state.mutex, FuriWaitForever);
+    int value = s_state.settings.websrv_accesslog_level;
+    furi_mutex_release(s_state.mutex);
+    return value;
 }
 
 void sysctl_set_websrv_accesslog_level(int level) {
-    s_settings.websrv_accesslog_level = level;
-    sysctl_settings_save(&s_settings);
+    furi_mutex_acquire(s_state.mutex, FuriWaitForever);
+    s_state.settings.websrv_accesslog_level = level;
+    sysctl_settings_save(&s_state.settings);
+    furi_mutex_release(s_state.mutex);
 }
 
 void sysctl_on_system_start(void) {
-    sysctl_settings_load(&s_settings);
+    s_state.mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    sysctl_settings_load(&s_state.settings);
 }
