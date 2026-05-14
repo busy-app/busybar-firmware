bf0d375db3de02f9c94294c5c16e86cec61daaf2
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-05-14 16:56:26 +0400

    netstat: better pool name checking

--- lib/netstat/netstat.c
+++ lib/netstat/netstat.c
@@ -6,7 +6,22 @@
 #define TAG                 "Netstat"
 #define USAGE_PERCENT_LIMIT 75UL
 
-static const char* monitored_pools = "NETCONN,TCP_PCB,SYS_TIMEOUT,PBUF_POOL";
+static const char* const monitored_pools[] = {
+    "NETCONN",
+    "TCP_PCB",
+    "SYS_TIMEOUT",
+    "PBUF_POOL",
+};
+
+static bool netstat_is_pool_monitored(const char* pool_name) {
+    furi_assert(pool_name);
+
+    for(size_t i = 0; i < COUNT_OF(monitored_pools); i++) {
+        if(strcmp(monitored_pools[i], pool_name) == 0) return true;
+    }
+
+    return false;
+}
 
 bool netstat_is_overloaded(NetstatLog log) {
     furi_check(log < NetstatLogMAX);
@@ -20,11 +35,10 @@ bool netstat_is_overloaded(NetstatLog log) {
         const struct memp_desc* pool_desc = memp_pools[i];
         struct stats_mem* pool_stats = lwip_stats.memp[i];
 
+        if(!netstat_is_pool_monitored(pool_desc->desc)) continue;
+
         u32_t percent_used =
             (pool_stats->avail > 0) ? ((pool_stats->used * 100) / pool_stats->avail) : 0;
-
-        bool is_monitored = !!strstr(monitored_pools, pool_desc->desc);
-        if(!is_monitored) continue;
 
         if(percent_used >= USAGE_PERCENT_LIMIT) {
             is_overloaded = true;
