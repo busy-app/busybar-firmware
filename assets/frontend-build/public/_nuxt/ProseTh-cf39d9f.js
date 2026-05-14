eb76b29caca4c1256b393841531d4c81cf3bbadc
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2026-05-14 19:39:39 +0300

    frontend: build

---
+++ assets/frontend-build/public/_nuxt/ProseTh-cf39d9f.js
@@ -0,0 +1,1 @@
+import{_ as r,c as o,o as t,r as s}from"./entry-cf39d9f.js";const c={};function n(e,a){return t(),o("th",null,[s(e.$slots,"default")])}const _=r(c,[["render",n]]);export{_ as default};
