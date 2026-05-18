963d389a87670ddf8f9ed8f0179a5de03ea6b980
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-18 16:25:59 +0100

    Busy: add the ability to look up a theme by name

--- applications/main/busy/helpers/theme_picker_model.c
+++ applications/main/busy/helpers/theme_picker_model.c
@@ -58,6 +58,24 @@ uint32_t
     return index;
 }
 
+uint32_t
+    theme_picker_model_get_item_index_by_name(const ThemePickerModel* instance, const char* name) {
+    furi_assert(instance);
+    furi_assert(name);
+
+    uint32_t index = THEME_PICKER_MODEL_INVALID_INDEX;
+
+    for(uint32_t i = 0; i < BusyThemeArray_size(instance->items); ++i) {
+        const BusyTheme* theme = *BusyThemeArray_cget(instance->items, i);
+        if(strcmp(busy_theme_get_name(theme), name) == 0) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
+
 void theme_picker_model_sort(ThemePickerModel* instance) {
     furi_assert(instance);
     BusyThemeArray_special_stable_sort(instance->items);
--- applications/main/busy/helpers/theme_picker_model.h
+++ applications/main/busy/helpers/theme_picker_model.h
@@ -19,4 +19,7 @@ uint32_t theme_picker_model_get_item_count(const ThemePickerModel* instance);
 uint32_t
     theme_picker_model_get_item_index(const ThemePickerModel* instance, const BusyTheme* item);
 
+uint32_t
+    theme_picker_model_get_item_index_by_name(const ThemePickerModel* instance, const char* name);
+
 void theme_picker_model_sort(ThemePickerModel* instance);
