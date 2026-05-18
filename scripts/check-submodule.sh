7591875643cde113a36aa28f4ca4642633687b40
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-05-18 07:42:00 +0400

    ci: infer branch manually

---
+++ scripts/check-submodule.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+EXPECTATION_TABLE=$1
+REPO_NAME=$2
+
+COMMIT="$(git rev-parse HEAD)"
+BRANCHES_W_COMMIT="$(git branch -r --contains $COMMIT)"
+
+EXPECTED_BRANCH=$(sed -n "s#^$REPO_NAME ##p" <(cat $EXPECTATION_TABLE))
+
+if grep "$EXPECTED_BRANCH" <(echo $BRANCHES_W_COMMIT) > /dev/null; then
+    echo "  Correct branch ($EXPECTED_BRANCH)"
+else
+    echo "  Incorrect branch (expected $EXPECTED_BRANCH)"
+    exit 1
+fi
--- scripts/check-submodules.sh
+++ scripts/check-submodules.sh
@@ -2,40 +2,36 @@
 
 set -e
 
-EXPECTED_BRANCHES="assets/proto (remotes/origin/HEAD)
-fbt_layers/core_libs (heads/bsb-2-g29c0c15)
-fbt_layers/fbtng (remotes/origin/bsb)
-fbt_layers/freertos (heads/dev)
-lib/cjson (v1.7.15-44-g12c4bf1)
-lib/heatshrink (heads/master)
-lib/lvgl (remotes/origin/flipper_tweaks)
-lib/lwip (remotes/origin/bsb)
-lib/matter_ext (v1.0.5-16-gc880f40e1)
-lib/matter_zap (heads/dev)
-lib/mbedtls (v3.6.4)
-lib/microtar (v0.1.0-48-g1e92136)
-lib/mongoose (7.20-187-gb1c2ffe1)
-lib/nanopb (nanopb-0.4.9.1)
-lib/simplicity_sdk (heads/sisdk-2025.12.2)
-lib/stb/stb_repo (f1c79c0)
-lib/thorvg (v1.0-pre32-63-g9a5112d8)
-lib/tinyusb (0.16.0-3142-g405dadecc)
-lib/wiseconnect (heads/bsb)
-lib/zlib (v1.3.1.2-41-gedd8895)
-scripts/toolchain (heads/dev)"
+EXPECTED_BRANCHES="assets/proto origin/dev
+fbt_layers/core_libs origin/bsb
+fbt_layers/fbtng origin/bsb
+fbt_layers/freertos origin/dev
+lib/cjson origin/master
+lib/heatshrink origin/master
+lib/lvgl origin/flipper_tweaks
+lib/lwip origin/bsb
+lib/matter_ext origin/main
+lib/matter_zap origin/dev
+lib/mbedtls origin/mbedtls-3.6
+lib/microtar origin/master
+lib/mongoose origin/master
+lib/nanopb origin/maintenance_0.4
+lib/simplicity_sdk origin/sisdk-2025.12.3
+lib/stb/stb_repo origin/master
+lib/thorvg origin/main
+lib/tinyusb origin/ncm_packet_filter
+lib/wiseconnect origin/bsb
+lib/zlib origin/master
+scripts/toolchain origin/dev"
 
-ACTUAL_BRANCHES=$(git submodule status | sed "s/^ *//" | cut -d ' ' -f 2,3)
+TABLE_FILE=$(mktemp "/tmp/flipper-bsb-chk-submodules.XXXXXXXX")
+echo "$EXPECTED_BRANCHES" > $TABLE_FILE
 
-if [ "$EXPECTED_BRANCHES" == "$ACTUAL_BRANCHES" ]; then
-    echo "All submodules are on the correct branch"
+if git submodule foreach 'bash $toplevel/scripts/check-submodule.sh '"$TABLE_FILE"' $sm_path'; then
+    echo "All submodules are on the correct branch."
 else
-    echo "Not all modules are on the correct branch:"
-    diff --color <(echo "$ACTUAL_BRANCHES") <(echo "$EXPECTED_BRANCHES") || true
-    echo ""
-    echo "Please point these submodules to the correct branches:"
-    echo ""
-    echo "    cd [submodule path here]"
-    echo "    git reset --hard HEAD"
-    echo "    git switch --detach [branch name here]"
+    echo "Not all modules are on the correct branch. See output above."
     exit 1
 fi
+
+rm $TABLE_FILE
