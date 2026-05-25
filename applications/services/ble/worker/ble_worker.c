05ae3181583408a7ce8ab5149c8c2e7ed05dca91
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-25 13:27:40 +0100

    Docs: fix more issues in ble_worker.c

--- applications/services/ble/worker/ble_worker.c
+++ applications/services/ble/worker/ble_worker.c
@@ -184,7 +184,6 @@ static BleWorker* ble_worker_instance = NULL;
  * @brief      invoked when advertise report event is received
  * @param[in]  adv_report, pointer to the received advertising report
  * @return     none.
- * @section description
  * This callback function updates the scanned remote devices list
  */
 static void ble_worker_on_adv_report_event(rsi_ble_event_adv_report_t* adv_report) {
@@ -229,7 +228,6 @@ static void ble_worker_on_adv_report_event(rsi_ble_event_adv_report_t* adv_repor
  * @param[in]  resp_disconnect, disconnected remote device information
  * @param[in]  reason, reason for disconnection.
  * @return     none.
- * @section description
  * This callback function indicates disconnected device information and status
  */
 static void
@@ -249,7 +247,6 @@ static void
  * @param[in]  resp_disconnect, disconnected remote device information
  * @param[in]  reason, reason for disconnection.
  * @return     none.
- * @section description
  * This Callback function indicates disconnected device information and status
  */
 static void ble_worker_phy_update_complete_event(
@@ -265,7 +262,6 @@ static void ble_worker_phy_update_complete_event(
 
 /**
  * @brief      invoked when data length is set
- * @section description
  * This Callback function indicates data length is set
  */
 static void ble_worker_data_length_change_event(
@@ -283,7 +279,6 @@ static void ble_worker_data_length_change_event(
  * @brief      invoked when enhanced connection complete event is received
  * @param[out] resp_conn, connected remote device information
  * @return     none.
- * @section description
  * This callback function indicates the status of the connection
  */
 static void
@@ -325,7 +320,6 @@ static void rsi_ble_on_directed_adv_report_event(
  * @brief      invoked when LE remote features event is received.
  * @param[in] resp_conn, connected remote device information
  * @return     none.
- * @section description
  * This callback function indicates the status of the connection
  */
 void ble_worker_simple_peripheral_on_remote_features_event(
@@ -354,7 +348,6 @@ static void ble_worker_more_data_req_event(rsi_ble_event_le_dev_buf_ind_t* rsi_b
  * @param[in]  event_id, it indicates write/notification event id.
  * @param[in]  rsi_ble_write, write event parameters.
  * @return     none.
- * @section description
  * This callback function is invoked when write/notify/indication events are received
  */
 static void
@@ -981,7 +974,6 @@ static int32_t ble_worker_thread_callback(void* context) {
  * @param[in]  temp_service,received 128-bit service.
  * @param[out] temp_uuid,formed 128-bit service structure.
  * @return     none.
- * @section description
  * This function prepares the 128bit UUID
  */
 static void
@@ -1009,7 +1001,6 @@ static void
  * @param[in]  att_val_handle, characteristic value handle
  * @param[in]  att_val_uuid, characteristic value uuid
  * @return     none.
- * @section description
  * This function is used at application to add characteristic attribute
  */
 static uint16_t ble_worker_add_char_serv_att(
