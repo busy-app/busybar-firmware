3681877a2f8f1904e89f8ce9e357082009888e54
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-05-25 12:04:08 +0400

    fix clanker review suggestions

--- scripts/check-submodule.sh
+++ scripts/check-submodule.sh
@@ -8,15 +8,15 @@ REPO_NAME=$2
 COMMIT="$(git rev-parse HEAD)"
 BRANCHES_W_COMMIT="$(git branch -r --contains $COMMIT)"
 
-EXPECTED_BRANCH=$(sed -n "s#^$REPO_NAME ##p" <(cat $EXPECTATION_TABLE))
+EXPECTED_BRANCH=$(sed -n "s#^$REPO_NAME ##p" "$EXPECTATION_TABLE")
 
 if [ "$EXPECTED_BRANCH" == "" ]; then
-    echo "  This module does not have an expected branch declared in \"check-submodules.sh\"."
+    echo "  This module does not have an expected branch declared in \"scripts/check-submodules.sh\"."
     echo "  Please declare the correct branch."
     exit 1
 fi
 
-if grep "$EXPECTED_BRANCH" <(echo $BRANCHES_W_COMMIT) > /dev/null; then
+if grep -P "$EXPECTED_BRANCH"'( |$)' <(echo $BRANCHES_W_COMMIT) > /dev/null; then
     echo "  Correct branch ($EXPECTED_BRANCH)"
 else
     echo "  Incorrect branch (expected $EXPECTED_BRANCH)"
