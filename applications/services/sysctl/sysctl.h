96554f0f670b09bf95443c25351d01c057ac0f7c
nminaylov <nm29719@gmail.com>
2026-05-28 19:09:52 +0300

    UI debug overlay

--- applications/services/sysctl/sysctl.h
+++ applications/services/sysctl/sysctl.h
@@ -10,4 +10,7 @@ void sysctl_set_cli_wifi_enabled(bool enabled);
 int sysctl_get_websrv_accesslog_level(void);
 void sysctl_set_websrv_accesslog_level(int level);
 
+int sysctl_get_ui_debug_mode(void);
+void sysctl_set_ui_debug_mode(int mode);
+
 void sysctl_set_debug_enabled(bool enabled);
