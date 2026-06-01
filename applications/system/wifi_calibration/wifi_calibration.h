3a6485e6781c5d635456d3721280b843c768cef5
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-06-01 11:28:33 +0400

    clean up debug apps

--- applications/system/wifi_calibration/wifi_calibration.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cli/cli_command.h>
-
-void wifi_calibration_command(PipeSide* pipe, FuriString* args, void* context);
--- applications/system/wifi_test/helpers/wifi_scan.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <furi.h>
-#include <sl_status.h>
-#include "../wifi_test.h"
-
-sl_status_t wifi_scan(void);
