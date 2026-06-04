924598caa0153567ea45fd998829170c87c388ec
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-06-04 06:53:34 +0400

    setting_provider: add dynamic default value support to custom type

--- lib/setting_provider/types/type_custom.c
+++ lib/setting_provider/types/type_custom.c
@@ -41,8 +41,22 @@ SETTING_RESET_DECLARATION(type_custom, json_node, setting, value) {
     furi_check(interface->default_value_size > 0);
     furi_check(interface->serialize_callback);
 
-    const void* default_value = interface->default_value;
-    furi_check(default_value);
+    uint8_t default_value[interface->default_value_size];
+    bool use_static_default = !!interface->default_value;
+    bool use_dynamic_default = !!interface->default_value_callback;
+
+    if(!(use_static_default ^ use_dynamic_default)) {
+        furi_crash(
+            "Exactly one of `default_value` or `default_value_callback` must be non-NULL in `SettingProviderCustomInterface`.");
+    }
+
+    if(use_static_default) memcpy(default_value, interface->default_value, sizeof(default_value));
+    if(use_dynamic_default) {
+        if(!interface->default_value_callback(default_value)) {
+            furi_crash(
+                "`default_value_callback` of `SettingProviderCustomInterface` returned `false`.");
+        }
+    }
 
     FuriString* default_string_value = furi_string_alloc();
     furi_check(interface->serialize_callback(setting, default_value, default_string_value));
