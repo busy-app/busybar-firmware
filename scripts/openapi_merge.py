393d0d95164b4aca5c1525152bd0f5f6b0853033
Anna porta Antonenko <portasynthinca3@gmail.com>
2026-06-04 07:31:13 +0400

    scripts: rewrite openapi_merge.py for flipper.app

--- scripts/openapi_merge.py
+++ scripts/openapi_merge.py
@@ -1,78 +1,103 @@
 #!/usr/bin/env python3
+#
+# How to use:
+#   openapi_merge.py path/to/destination.yaml path/to/template/openapi.yaml segment1.yaml segment2.yaml ...
+#                    \______________________/ \___________________________________________________________/
+#                          merged file                           source files to combine
+#                                             \___________________________/ \_____________________________/
+#                                                template (distinguished             segment files
+#                                                     by basename)
 
-import sys
+from flipper.app import App
 from pathlib import Path
+from dataclasses import dataclass
 
-def error(txt):
-    print(txt, file=sys.stderr)
-    exit(1)
+@dataclass
+class SubstitutionDescriptor:
+    section: str
+    placeholder: str
+    indentation_change: int
 
-if len(sys.argv) < 2:
-    error(f"Usage: {sys.argv[0]} destination source source source...")
-
-dst_file = sys.argv[1]
-src_files = sys.argv[2:]
-
-TEMPLATE_FILE_NAME = "openapi.yaml"
-template_files = []
-segment_files = []
-
-for path in src_files:
-    basename = Path(path).name
-    extension = Path(path).suffix
-
-    if extension != ".yaml":
-        error(f"All source files must have the `.yaml` extension.\nThis one didn't:\n  {path}")
-
-    if basename == TEMPLATE_FILE_NAME:
-        template_files.append(path)
-    else:
-        segment_files.append(path)
-
-if len(template_files) > 1:
-    names_for_printing = "\n".join(f"  - {path}" for path in template_files)
-    error(f"There must only be one template file.\nThe template file is distinguished by its `{TEMPLATE_FILE_NAME}` name.\nThe following sources files have this name:\n{names_for_printing}")
-
-if not template_files:
-    error(f"There must be a template file.\nThe template file is distinguished by its `{TEMPLATE_FILE_NAME}` name.\nNone of the provided source files had this name.")
-
-template_file = template_files[0]
-substitutions = {}
-
-for path in segment_files:
-    with open(path, "r") as file:
-        lines = file.readlines()
-        current_section = ""
-
-        for line in lines:
-            line = line.strip("\r\n")
-            if line and not line.startswith(" "):
-                current_section = line
-                if current_section not in substitutions:
-                    substitutions[current_section] = []
-                continue
-
-            substitutions[current_section].append(line)
-
+EXTENSION = ".yaml"
+TEMPLATE_FILE_NAME = f"openapi{EXTENSION}"
 SUBSTITUTION_DESCRIPTORS = [
-    { "section": "paths:", "placeholder": "# PATHS_CONTENT_PLACEHOLDER #", "indentation_change": 0 },
-    { "section": "schemas:", "placeholder": "# SCHEMAS_CONTENT_PLACEHOLDER #", "indentation_change": +2 },
+    SubstitutionDescriptor(section="paths:", placeholder="# PATHS_CONTENT_PLACEHOLDER #", indentation_change=0),
+    SubstitutionDescriptor(section="schemas:", placeholder="# SCHEMAS_CONTENT_PLACEHOLDER #", indentation_change=+2),
 ]
 
-with open(template_file, "r") as file:
-    template = file.read()
+class Main(App):
+    def init(self):
+        self.parser.add_argument("destination", help="Destination file")
+        self.parser.add_argument("source", help="Source files (template and segments)", nargs="+")
+        self.parser.set_defaults(func=self.main)
 
-def format_line(string, indentation_change):
-    contents = string.lstrip(" ")
-    original_indentation = len(string) - len(contents)
-    new_indentation = " " * (original_indentation + indentation_change)
-    return f"\n{new_indentation}{contents}"
+        self.args = self.parser.parse_args()
 
-for descriptor in SUBSTITUTION_DESCRIPTORS:
-    lines = substitutions[descriptor["section"]]
-    lines = [format_line(line, descriptor["indentation_change"]) for line in lines]
-    text = "".join(lines)
-    template = template.replace(descriptor["placeholder"], text)
+    @staticmethod
+    def change_indentation(string, delta):
+        contents = string.lstrip(" ")
+        original_indentation = len(string) - len(contents)
+        new_indentation = " " * (original_indentation + delta)
+        return f"\n{new_indentation}{contents}"
 
-with open(dst_file, "w") as file:
-    file.write(template)
+    def read_segments(self, segment_files):
+        substitutions = {}
+
+        for path in segment_files:
+            with open(path, "r") as file:
+                lines = file.readlines()
+                current_section = ""
+
+                for line in lines:
+                    line = line.strip("\r\n")
+                    if line and not line.startswith(" "):
+                        current_section = line
+                        if current_section not in substitutions:
+                            substitutions[current_section] = []
+                        continue
+
+                    substitutions[current_section].append(line)
+
+        return substitutions
+
+    def main(self):
+        args = self.args
+
+        template_files = []
+        segment_files = []
+
+        for path in args.source:
+            if Path(path).suffix != EXTENSION:
+                self.logger.error(f"All source files must have the `{EXTENSION}` extension.\nThis one didn't:\n  {path}")
+                return 1
+
+            if Path(path).name == TEMPLATE_FILE_NAME:
+                template_files.append(path)
+            else:
+                segment_files.append(path)
+
+        if len(template_files) > 1:
+            names_for_printing = "\n".join(f"  - {path}" for path in template_files)
+            self.logger.error(f"There must only be one template file.\nThe template file is distinguished by its `{TEMPLATE_FILE_NAME}` name.\nThe following sources files have this name:\n{names_for_printing}")
+            return 1
+
+        if not template_files:
+            self.logger.error(f"There must be a template file.\nThe template file is distinguished by its `{TEMPLATE_FILE_NAME}` name.\nNone of the provided source files had this name.")
+            return 1
+
+        template = Path(template_files[0]).read_text()
+        substitutions = self.read_segments(segment_files)
+
+        for descriptor in SUBSTITUTION_DESCRIPTORS:
+            lines = substitutions[descriptor.section]
+            lines = [self.change_indentation(line, descriptor.indentation_change) for line in lines]
+            text = "".join(lines)
+            template = template.replace(descriptor.placeholder, text)
+
+        with open(args.destination, "w") as file:
+            file.write(template)
+
+        return 0
+
+if __name__ == "__main__":
+    Main()()
