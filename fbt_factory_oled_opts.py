994324d7a438240371bed1daaca1bdcef7a80366
Lesha Lomalkin <flp16@lomalkin.org>
2026-06-09 18:02:36 +0100

    factory_oled: build in CI via extra_fbt_args

---
+++ fbt_factory_oled_opts.py
@@ -0,0 +1,6 @@
+
+# ./fbt TARGET_HW=21 FIRMWARE_APP_SET="factory_testing" LOADER_AUTOSTART=BackDisplayFactory flash
+
+# U5_TARGET_HW=21
+FIRMWARE_APP_SET="factory_testing"
+LOADER_AUTOSTART="BackDisplayFactory"
