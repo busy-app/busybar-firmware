f2589b5a4e94cf0c7b996a4aec86148e98158204
Georgii Surkov <georgii.surkov@outlook.com>
2026-06-16 23:20:05 +0100

    Busy: implement conditional record opening

--- applications/services/busy_timer/busy_timer_util.c
+++ applications/services/busy_timer/busy_timer_util.c
@@ -5,25 +5,8 @@
 
 #define BUSY_APP_WAIT_TIME_TICKS (500)
 
-static bool busy_timer_app_record_exists(void) {
-    bool record_exists = false;
-
-    for(const uint32_t start_tick = furi_get_tick();
-        furi_get_tick() - start_tick < BUSY_APP_WAIT_TIME_TICKS;
-        furi_delay_tick(2)) {
-        if(furi_record_exists(RECORD_BUSY_APP)) {
-            record_exists = true;
-            break;
-        }
-    }
-
-    return record_exists;
-}
-
-static bool busy_timer_app_show_timer(const BusyAppConfig* app_config) {
+static bool busy_timer_app_show_timer(BusyApp* busy_app, const BusyAppConfig* app_config) {
     bool success = false;
-
-    BusyApp* busy_app = furi_record_open(RECORD_BUSY_APP);
 
     do {
         if(busy_set_config(busy_app, app_config) != BusyStatusOk) {
@@ -37,33 +20,45 @@ static bool busy_timer_app_show_timer(const BusyAppConfig* app_config) {
         success = true;
     } while(false);
 
-    furi_record_close(RECORD_BUSY_APP);
-
     return success;
+}
+
+static void busy_timer_app_lauch(Desktop* desktop) {
+    while(!desktop_replace_current_app(desktop, "busy", BUSY_APP_TIMER_MODE)) {
+        furi_delay_tick(2);
+    }
 }
 
 void busy_timer_start_app(const BusyAppConfig* app_config) {
     // NOTE: Waiting for Desktop before attempting to look for the app record
     Desktop* desktop = furi_record_open(RECORD_DESKTOP);
 
+    BusyApp* busy_app;
+
     for(;;) {
-        if(!busy_timer_app_record_exists()) {
-            while(!desktop_replace_current_app(desktop, "busy", BUSY_APP_TIMER_MODE)) {
-                furi_delay_tick(2);
-            }
+        busy_app = furi_record_open_ex(RECORD_BUSY_APP, BUSY_APP_WAIT_TIME_TICKS);
+
+        if(busy_app == NULL) {
+            busy_timer_app_lauch(desktop);
+            continue;
         }
 
-        if(busy_timer_app_show_timer(app_config)) {
+        if(busy_timer_app_show_timer(busy_app, app_config)) {
             break;
         }
+    }
+
+    if(busy_app) {
+        furi_record_close(RECORD_BUSY_APP);
     }
 
     furi_record_close(RECORD_DESKTOP);
 }
 
 void busy_timer_exit_app(void) {
-    if(busy_timer_app_record_exists()) {
-        BusyApp* busy_app = furi_record_open(RECORD_BUSY_APP);
+    BusyApp* busy_app = furi_record_open_ex(RECORD_BUSY_APP, 0);
+
+    if(busy_app != NULL) {
         busy_request_exit(busy_app);
         furi_record_close(RECORD_BUSY_APP);
     }
