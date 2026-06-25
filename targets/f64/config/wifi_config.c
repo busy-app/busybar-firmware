b932491fcd1a886b589f4a55c1bc1318374f1410
RebornedBrain <RebornedBrain@gmail.com>
2026-06-25 22:00:46 +0300

    Enabled extended advertising with 1 set

--- targets/f64/config/wifi_config.c
+++ targets/f64/config/wifi_config.c
@@ -79,7 +79,13 @@ const sl_wifi_device_configuration_t wifi_config_client = {
 #if BLE_SIMPLE_GATT
                  | SL_SI91X_BLE_GATT_INIT
 #endif
-                 ),
+#if RSI_BLE_ENABLE_ADV_EXTN
+                 | SL_SI91X_BLE_ENABLE_ADV_EXTN
+#endif
+#if RSI_BLE_AE_MAX_ADV_SETS
+                 | SL_SI91X_BLE_AE_MAX_ADV_SETS(RSI_BLE_AE_MAX_ADV_SETS)
+#endif
+                     ),
             .config_feature_bit_map = SL_SI91X_FEAT_SLEEP_GPIO_SEL_BITMAP,
         },
     .ta_pool =
