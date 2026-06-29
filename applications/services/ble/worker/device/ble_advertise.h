4ee00afe57bb17cfc93c51617f49a39fac5cae6a
RebornedBrain <RebornedBrain@gmail.com>
2026-06-29 13:03:01 +0300

    Obsolete comment removed

--- applications/services/ble/worker/device/ble_advertise.h
+++ applications/services/ble/worker/device/ble_advertise.h
@@ -24,9 +24,7 @@ void ble_advertise_free(BleAdvertiseContext* instance);
  * @brief Set new name, which will be used for advertise
  *
  * @param instance of advertise context
- * @param new_name which will be used for advertise. Max_length = Advertise_pack_max_length - Used_space.
- * If name exceeds max_length it will be trimmed like "Name..." 
- * Now Max_length = 31 - 17 = 14
+ * @param new_name which will be used for advertise.
  */
 void ble_advertise_set_name(BleAdvertiseContext* instance, const char* new_name);
 
