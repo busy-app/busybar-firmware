201f12ef353fc9635a96bd75f2e0c4e57a26691e
Georgii Surkov <georgii.surkov@outlook.com>
2026-07-04 10:57:14 +0100

    Fetch: port to EventLoop

--- applications/system/fetch/fetch.c
+++ applications/system/fetch/fetch.c
@@ -17,24 +17,24 @@ typedef struct {
 } FetchParams;
 
 typedef struct {
+    FuriEventLoop* event_loop;
     FuriStreamBuffer* buffer_rx;
     FetchClient* fetch_client;
     FuriMessageQueue* status_queue;
     FetchFileSave* file_save;
-    bool error;
+    bool is_error;
 } Fetch;
 
 static void fetch_client_callback_raw_data(uint8_t* data, size_t data_size, void* context) {
     furi_assert(context);
     Fetch* instance = context;
-    furi_assert(instance);
     furi_stream_buffer_send(instance->buffer_rx, data, data_size, FuriWaitForever);
 }
 
 static void fetch_client_callback_file_write_data(uint8_t* data, size_t data_size, void* context) {
     furi_assert(context);
     Fetch* instance = context;
-    furi_assert(instance->file_save);
+
     if(data_size > 0) {
         fetch_file_save_write(instance->file_save, data, data_size);
     } else {
@@ -51,52 +51,168 @@ static void fetch_client_callback_header(uint8_t* data, size_t data_size, void* 
 static void fetch_client_callback_error(const char* error, void* context) {
     furi_assert(context);
     Fetch* instance = context;
-    furi_assert(instance);
+
     FuriString* error_str =
         furi_string_alloc_printf(ANSI_FG_RED "Error: " ANSI_RESET "%s\r\n", error);
+
     furi_stream_buffer_send(
         instance->buffer_rx,
         (uint8_t*)furi_string_get_cstr(error_str),
         furi_string_size(error_str),
         FuriWaitForever);
+
     furi_string_free(error_str);
-    instance->error = true;
+    instance->is_error = true;
 }
 
 static void fetch_client_callback_status(FetchClientStatus status, void* context) {
     furi_assert(context);
     Fetch* instance = context;
-    furi_assert(instance);
+
     furi_message_queue_put(instance->status_queue, &status, FuriWaitForever);
+}
+
+static void fetch_client_callback_done(void* context) {
+    furi_assert(context);
+    Fetch* instance = context;
+
+    furi_event_loop_stop(instance->event_loop);
+}
+
+static void fetch_status_queue_callback(FuriEventLoopObject* obj, void* context) {
+    furi_assert(context);
+
+    Fetch* instance = context;
+    furi_assert(obj == instance->status_queue);
+
+    FetchClientStatus status;
+    while(furi_message_queue_get(instance->status_queue, &status, 0) == FuriStatusOk) {
+        if(status.total_download_size) {
+            const char* units_str;
+
+            if(status.total_download_size > 2048) {
+                status.received_download_size /= 1024;
+                status.total_download_size /= 1024;
+                units_str = "KiB";
+
+            } else {
+                units_str = "B";
+            }
+
+            const size_t download_percent =
+                (status.received_download_size * 100) / status.total_download_size;
+
+            printf(
+                ANSI_BG_GREEN ANSI_FG_BLACK "\rDownloaded: [%3zu%%]" ANSI_RESET " [",
+                download_percent);
+
+            const size_t bars = (status.received_download_size * 20) / status.total_download_size;
+
+            for(size_t i = 0; i < bars; i++) {
+                printf("=");
+            }
+
+            for(size_t i = bars; i < 20; i++) {
+                printf(" ");
+            }
+
+            const float download_speed = (float)status.speed_bytes_per_sec / 1024.0f;
+
+            printf(
+                "] %8.2f KiB/s, %zu%s/%zu%s        ",
+                download_speed,
+                status.received_download_size,
+                units_str,
+                status.total_download_size,
+                units_str);
+
+        } else {
+            const float download_speed = (float)status.speed_bytes_per_sec / 1024.0f;
+            const size_t total_size = status.received_download_size / 1024;
+
+            printf(
+                ANSI_BG_GREEN ANSI_FG_BLACK "\rDownloaded: " ANSI_RESET
+                                            "%8.2fKiB/s, Total: %zuKiB        ",
+                download_speed,
+                total_size);
+        }
+
+        fflush(stdout);
+    }
+}
+
+static void fetch_stream_buffer_rx_callback(FuriEventLoopObject* obj, void* context) {
+    furi_assert(context);
+
+    Fetch* instance = context;
+    furi_assert(obj == instance->buffer_rx);
+
+    size_t bytes_read;
+    uint8_t buffer[256];
+
+    do {
+        bytes_read = furi_stream_buffer_receive(instance->buffer_rx, buffer, sizeof(buffer), 0);
+
+        for(size_t i = 0; i < bytes_read; i++) {
+            const char c = buffer[i];
+
+            if(!iscntrl(c) || isspace(c)) {
+                printf("%c", c);
+            } else {
+                printf(" [%02x] ", c);
+            }
+
+            fflush(stdout);
+        }
+
+    } while(bytes_read != 0);
 }
 
 static Fetch* fetch_alloc() {
     Fetch* instance = malloc(sizeof(Fetch));
 
-    instance->error = false;
+    instance->event_loop = furi_event_loop_alloc();
     instance->buffer_rx = furi_stream_buffer_alloc(1024 * 4, 1);
     instance->status_queue = furi_message_queue_alloc(10, sizeof(FetchClientStatus));
     instance->fetch_client = fetch_client_alloc();
     instance->file_save = fetch_file_save_alloc();
+    instance->is_error = false;
+
     fetch_client_set_context(instance->fetch_client, instance);
+    fetch_client_set_callback_error(instance->fetch_client, fetch_client_callback_error);
+    fetch_client_set_callback_done(instance->fetch_client, fetch_client_callback_done);
+
+    furi_event_loop_subscribe_message_queue(
+        instance->event_loop,
+        instance->status_queue,
+        FuriEventLoopEventIn,
+        fetch_status_queue_callback,
+        instance);
+
+    furi_event_loop_subscribe_stream_buffer(
+        instance->event_loop,
+        instance->buffer_rx,
+        FuriEventLoopEventIn,
+        fetch_stream_buffer_rx_callback,
+        instance);
 
     return instance;
 }
 
 static void fetch_free(Fetch* instance) {
+    furi_event_loop_unsubscribe(instance->event_loop, instance->status_queue);
+    furi_event_loop_unsubscribe(instance->event_loop, instance->buffer_rx);
+
     fetch_file_save_free(instance->file_save);
     fetch_client_free(instance->fetch_client);
     furi_stream_buffer_free(instance->buffer_rx);
     furi_message_queue_free(instance->status_queue);
+    furi_event_loop_free(instance->event_loop);
 
     free(instance);
 }
 
-static bool fetch_url(PipeSide* pipe, const FetchParams* params) {
-    UNUSED(pipe);
-
-    bool ret = false;
-
+static void fetch_run(const FetchParams* params) {
     Fetch* instance = fetch_alloc();
 
     const char* output_path = params->output_path;
@@ -105,12 +221,11 @@ static bool fetch_url(PipeSide* pipe, const FetchParams* params) {
         if(!fetch_file_save_open(instance->file_save, FetchFileSaveFlagNone, output_path)) {
             printf(ANSI_FG_RED "Error: Failed to open file %s\r\n" ANSI_RESET, output_path);
             fetch_free(instance);
-            return ret;
+            return;
         }
 
         fetch_client_set_callback_raw_data(
             instance->fetch_client, fetch_client_callback_file_write_data);
-
         fetch_client_set_callback_status(instance->fetch_client, fetch_client_callback_status);
 
     } else {
@@ -121,90 +236,12 @@ static bool fetch_url(PipeSide* pipe, const FetchParams* params) {
         fetch_client_set_callback_header(instance->fetch_client, fetch_client_callback_header);
     }
 
-    fetch_client_set_callback_error(instance->fetch_client, fetch_client_callback_error);
-
     fetch_client_run(instance->fetch_client, params->url);
+    furi_event_loop_run(instance->event_loop);
 
-    const char spin_chars[] = "|/-\\";
-    uint8_t spin_chars_index = 0;
-    bool flag_waiting_receive_data = true;
-    FetchClientStatus status;
-
-    while(!fetch_client_is_processing_done(instance->fetch_client) ||
-          furi_stream_buffer_bytes_available(instance->buffer_rx)) {
-        if(furi_message_queue_get(instance->status_queue, &status, 200) == FuriStatusOk) {
-            flag_waiting_receive_data = false;
-            if(status.total_download_size) {
-                char* dimension = "B";
-                if(status.total_download_size > 2048) {
-                    status.received_download_size /= 1024;
-                    status.total_download_size /= 1024;
-                    dimension = "kB";
-                }
-                printf(
-                    ANSI_BG_GREEN ANSI_FG_BLACK "\rDownloaded: [%3d%%]" ANSI_RESET " [",
-                    (int)((status.received_download_size * 100) / status.total_download_size));
-                size_t bars = (status.received_download_size * 20) / status.total_download_size;
-                for(size_t i = 0; i < bars; i++) {
-                    printf("=");
-                }
-                for(size_t i = bars; i < 20; i++) {
-                    printf(" ");
-                }
-                printf(
-                    "] %8.2f kB/s, %zu%s/%zu%s        ",
-                    (float)status.speed_bytes_per_sec / 1024.0f,
-                    status.received_download_size,
-                    dimension,
-                    status.total_download_size,
-                    dimension);
-                fflush(stdout);
-            } else {
-                printf(
-                    ANSI_BG_GREEN ANSI_FG_BLACK "\rDownloaded: " ANSI_RESET
-                                                "%8.2fkB/s, Total: %zukB        ",
-                    (float)status.speed_bytes_per_sec / 1024.0f,
-                    status.received_download_size / 1024);
-                fflush(stdout);
-            }
-            continue;
-        }
-
-        if(!furi_stream_buffer_bytes_available(instance->buffer_rx) && flag_waiting_receive_data) {
-            printf("\rWaiting... %c", spin_chars[spin_chars_index]);
-            fflush(stdout);
-            spin_chars_index = (spin_chars_index + 1) % 4;
-            continue;
-        }
-
-        if(flag_waiting_receive_data) {
-            printf("\r                      \r");
-            flag_waiting_receive_data = false;
-        }
-
-        while(furi_stream_buffer_bytes_available(instance->buffer_rx)) {
-            uint8_t buffer[256];
-            size_t bytes_read =
-                furi_stream_buffer_receive(instance->buffer_rx, buffer, sizeof(buffer), 0);
-            if(bytes_read > 0) {
-                for(size_t i = 0; i < bytes_read; i++) {
-                    if(buffer[i] < 0x10 && (buffer[i] != '\r') && (buffer[i] != '\n')) {
-                        printf(" [%02x] ", buffer[i]);
-                    } else {
-                        printf("%c", buffer[i]);
-                    }
-                    fflush(stdout);
-                }
-            }
-        }
-    }
-    printf("\r\n");
-
-    // If saving to file, close and report
-    if(params->output_path) {
-        if(!instance->error) {
+    if(output_path != NULL) {
+        if(!instance->is_error) {
             printf(ANSI_FG_GREEN "File successfully saved to %s\r\n" ANSI_RESET, output_path);
-            ret = true;
         } else {
             fetch_file_save_remove(instance->file_save);
             printf(ANSI_FG_RED "Error: Failed to save file to %s\r\n" ANSI_RESET, output_path);
@@ -212,8 +249,6 @@ static bool fetch_url(PipeSide* pipe, const FetchParams* params) {
     }
 
     fetch_free(instance);
-
-    return ret;
 }
 
 static void fetch_command_print_usage(void) {
@@ -263,14 +298,29 @@ static bool fetch_validate_params(const FetchParams* params) {
 }
 
 void fetch_command(PipeSide* pipe, FuriString* args, void* context) {
+    UNUSED(pipe);
     UNUSED(context);
 
-    FetchParams params = {0};
+    bool success = false;
 
-    if(parse_args(args, "o:d:H:X:v", fetch_option_callback, &params) &&
-       fetch_validate_params(&params)) {
-        fetch_url(pipe, &params);
-    } else {
+    do {
+        FetchParams params = {0};
+
+        if(!parse_args(args, "o:d:H:X:v", fetch_option_callback, &params)) {
+            // TODO: Error message
+            break;
+        }
+        if(!fetch_validate_params(&params)) {
+            // TODO: Error message
+            break;
+        }
+
+        fetch_run(&params);
+        success = true;
+
+    } while(false);
+
+    if(!success) {
         fetch_command_print_usage();
     }
 }
