e3d03314dde66b8642fc4b1339fc7bd29af54ffd
Georgii Surkov <georgii.surkov@outlook.com>
2026-07-23 17:52:06 +0100

    ApiAssets: refactor request handling

--- applications/services/web_server/http_api/api_assets.c
+++ applications/services/web_server/http_api/api_assets.c
@@ -7,28 +7,66 @@
 #define TAG "HttpAssets"
 
 #define ASSETS_UPLOAD_DIR EXT_PATH("user_assets")
-#define FILE_NAME_LEN_MAX 32
 
-static bool api_assets_upload_parse_parameters(struct mg_str* params_str, FuriString* file_path) {
-    if(params_str->len == 0) {
-        return false;
+#define APP_NAME_LEN_MAX  32
+#define FILE_NAME_LEN_MAX 64
+
+#define APP_NAME_PARAM_KEY  "application_name"
+#define FILE_NAME_PARAM_KEY "file"
+
+static bool api_assets_get_app_directory_path(const struct mg_str* params, FuriString* out_path) {
+    bool success = false;
+
+    char app_name[APP_NAME_LEN_MAX];
+    const int app_name_len =
+        mg_http_get_var(params, APP_NAME_PARAM_KEY, app_name, sizeof(app_name));
+
+    if(app_name_len > 0) {
+        furi_string_printf(out_path, "%s/%.*s", ASSETS_UPLOAD_DIR, app_name_len, app_name);
+        success = true;
     }
 
-    char temp_str[FILE_NAME_LEN_MAX];
+    return success;
+}
 
-    int var_len = mg_http_get_var(params_str, "application_name", temp_str, sizeof(temp_str));
-    if(var_len <= 0) {
-        return false;
+static bool
+    api_assets_append_target_file_subpath(const struct mg_str* params, FuriString* out_path) {
+    bool success = false;
+
+    char file_name[FILE_NAME_LEN_MAX];
+    const int file_name_len =
+        mg_http_get_var(params, FILE_NAME_PARAM_KEY, file_name, sizeof(file_name));
+
+    if(file_name_len > 0) {
+        furi_string_cat_printf(out_path, "/%.*s", file_name_len, file_name);
+        success = true;
     }
-    furi_string_printf(file_path, "%s/%.*s/", ASSETS_UPLOAD_DIR, var_len, temp_str);
 
-    var_len = mg_http_get_var(params_str, "file", temp_str, sizeof(temp_str));
-    if(var_len <= 0) {
-        return false;
-    }
-    furi_string_cat_printf(file_path, "%.*s", var_len, temp_str);
+    return success;
+}
 
-    return mg_path_is_sane(mg_str(furi_string_get_cstr(file_path)));
+static bool api_assets_get_target_file_path(const struct mg_str* params, FuriString* out_path) {
+    bool success = false;
+
+    do {
+        if(params->len == 0) {
+            break;
+        }
+        if(!api_assets_get_app_directory_path(params, out_path)) {
+            break;
+        }
+        if(!api_assets_append_target_file_subpath(params, out_path)) {
+            break;
+        }
+        if(!mg_path_is_sane(mg_str(furi_string_get_cstr(out_path)))) {
+            break;
+        }
+
+        success = true;
+
+    } while(false);
+
+    return success;
 }
 
 static bool api_assets_upload_headers_callback(
@@ -43,7 +81,8 @@ static bool api_assets_upload_headers_callback(
     if(method != HttpMethodPost) return false;
 
     FuriString* file_path = furi_string_alloc();
-    if(api_assets_upload_parse_parameters(&msg->query, file_path)) {
+
+    if(api_assets_get_target_file_path(&msg->query, file_path)) {
         http_upload_start(conn, msg, furi_string_get_cstr(file_path));
     } else {
         MG_REPLY_ERROR_CLOSE(conn, 400, "Bad Request");
@@ -66,40 +105,40 @@ static bool api_assets_delete_callback(
     if(method != HttpMethodDelete) return false;
 
     FuriString* dir_path = furi_string_alloc();
-    bool success = false;
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+
     do {
-        if(msg->query.len == 0) {
+        const struct mg_str* params = &msg->query;
+
+        if(params->len == 0) {
             MG_REPLY_BAD_REQUEST(conn);
             break;
         }
 
-        char app_name_str[FILE_NAME_LEN_MAX];
-
-        int var_len =
-            mg_http_get_var(&msg->query, "application_name", app_name_str, sizeof(app_name_str));
-        if(var_len <= 0) {
+        if(!api_assets_get_app_directory_path(params, dir_path)) {
             MG_REPLY_BAD_REQUEST(conn);
             break;
         }
-        furi_string_printf(dir_path, "%s/%.*s", ASSETS_UPLOAD_DIR, var_len, app_name_str);
 
-        Storage* fs_api = furi_record_open(RECORD_STORAGE);
-        if(!storage_dir_exists(fs_api, furi_string_get_cstr(dir_path))) {
+        const char* dir_path_str = furi_string_get_cstr(dir_path);
+
+        FURI_LOG_W(TAG, "!!!!!!!!!!!! %s", dir_path_str);
+
+        if(!storage_dir_exists(storage, dir_path_str)) {
             MG_REPLY_ERROR(conn, 404, "Assets not found");
-            furi_record_close(RECORD_STORAGE);
             break;
         }
 
-        success = storage_simply_remove_recursive(fs_api, furi_string_get_cstr(dir_path));
-        furi_record_close(RECORD_STORAGE);
-
-        if(success) {
-            MG_REPLY_OK(conn);
-        } else {
+        if(!storage_simply_remove_recursive(storage, dir_path_str)) {
             MG_REPLY_SERVICE_UNAVAILABLE(conn, "File delete failed");
+            break;
         }
+
+        MG_REPLY_OK(conn);
+
     } while(0);
 
+    furi_record_close(RECORD_STORAGE);
     furi_string_free(dir_path);
 
     return true;
@@ -119,6 +158,16 @@ typedef struct {
     HttpHandlersList_t handlers;
 } ApiAssetsCtx;
 
+static void api_assets_create_base_directory(void) {
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+
+    if(!storage_simply_mkdir(storage, ASSETS_UPLOAD_DIR)) {
+        FURI_LOG_E(TAG, "Failed to create base directory");
+    }
+
+    furi_record_close(RECORD_STORAGE);
+}
+
 void* http_api_assets_alloc(void) {
     ApiAssetsCtx* context = malloc(sizeof(ApiAssetsCtx));
     HttpHandlersList_init(context->handlers);
@@ -127,8 +176,7 @@ void* http_api_assets_alloc(void) {
         http_handler_add(context->handlers, &handlers_assets[i - 1]);
     }
 
-    // Create assets directory
-    http_fs_get()->mkd(ASSETS_UPLOAD_DIR);
+    api_assets_create_base_directory();
     return context;
 }
 
