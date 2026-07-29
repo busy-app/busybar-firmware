6496238d08c3cf6137ccb3da7bfa568fc1a6c073
Georgii Surkov <georgii.surkov@outlook.com>
2026-07-29 20:19:40 +0100

    JsAppLauncher: implement error scene

--- applications/system/js_app_launcher/scenes/js_app_launcher_scene_error.c
+++ applications/system/js_app_launcher/scenes/js_app_launcher_scene_error.c
@@ -1,22 +1,46 @@
 #include "../js_app_launcher_i.h"
 #include "js_app_launcher_scenes.h"
 
+#include <storage/storage.h>
+#include <gui/modules/status_view.h>
+
 typedef struct {
-    bool dummy;
+    StatusView* front_status;
+    StatusView* back_status;
 } JsAppLauncherSceneError;
 
 static void js_app_launcher_scene_error_on_enter(void* context) {
     furi_assert(context);
     JsAppLauncher* instance = context;
 
-    UNUSED(instance);
+    JsAppLauncherSceneError* data =
+        scene_manager_get_scene_data(instance->scene_manager, JsAppLauncherSceneIdError);
+
+    with_gui(instance->gui, {
+        widget_set_visible(nav_bar_get_base(instance->nav_bar), false);
+
+        data->front_status = status_view_alloc(instance->front_window);
+        status_view_set_primary_text(data->front_status, "Failed to start app");
+        status_view_set_icon(data->front_status, SHARED_IMG_PATH("error_front_8x8.image"), false);
+
+        data->back_status = status_view_alloc(instance->back_window);
+        status_view_set_primary_text(data->back_status, "Error");
+        status_view_set_auxiliary_text(data->back_status, "Failed to start app");
+        status_view_set_icon(data->back_status, SHARED_IMG_PATH("error_back_11x11.image"), false);
+    });
 }
 
 static void js_app_launcher_scene_error_on_exit(void* context) {
     furi_assert(context);
     JsAppLauncher* instance = context;
 
-    UNUSED(instance);
+    JsAppLauncherSceneError* data =
+        scene_manager_get_scene_data(instance->scene_manager, JsAppLauncherSceneIdError);
+
+    with_gui(instance->gui, {
+        status_view_free(data->front_status);
+        status_view_free(data->back_status);
+    });
 }
 
 static bool js_app_launcher_scene_error_on_event(const SceneManagerEvent* event, void* context) {
@@ -24,10 +48,6 @@ static bool js_app_launcher_scene_error_on_event(const SceneManagerEvent* event,
     furi_assert(context);
 
     bool consumed = false;
-
-    JsAppLauncher* instance = context;
-    UNUSED(instance);
-
     return consumed;
 }
 
