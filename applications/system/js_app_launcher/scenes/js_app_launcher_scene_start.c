031ee23836db2c94e192a7ae8a81b60f99ac1a5d
Georgii Surkov <georgii.surkov@outlook.com>
2026-07-29 12:42:12 +0100

    JsAppLauncher: add icons and label

--- applications/system/js_app_launcher/scenes/js_app_launcher_scene_start.c
+++ applications/system/js_app_launcher/scenes/js_app_launcher_scene_start.c
@@ -1,8 +1,11 @@
 #include "../js_app_launcher_i.h"
 #include "js_app_launcher_scenes.h"
 
-#include <gui/modules/menu.h>
 #include <gui/modules/anim_menu.h>
+#include <gui/modules/flex_box.h>
+#include <gui/modules/image.h>
+#include <gui/modules/label.h>
+#include <gui/modules/menu.h>
 
 typedef enum {
     JsAppLauncherSceneStartMenuIdxStart,
@@ -11,6 +14,9 @@ typedef enum {
 } JsAppLauncherSceneStartMenuIdx;
 
 typedef struct {
+    FlexBox* front_flex;
+    Image* front_icon;
+    Label* front_label;
     AnimMenu* front_menu;
     Menu* back_menu;
 } JsAppLauncherSceneStart;
@@ -30,7 +36,22 @@ static void js_app_launcher_scene_start_on_enter(void* context) {
     JsAppLauncherSceneStart* data =
         scene_manager_get_scene_data(instance->scene_manager, JsAppLauncherSceneIdStart);
 
+    JsAppInfo info;
+    furi_check(js_app_get_info(instance->js_app, &info));
+
     with_gui(instance->gui, {
+        data->front_flex = flex_box_alloc(instance->front_window);
+        widget_set_align(flex_box_get_base(data->front_flex), AlignLeftMid);
+        flex_box_set_flow(data->front_flex, FlexBoxFlowRow);
+        flex_box_set_align(data->front_flex, FlexBoxAlignStart, FlexBoxAlignCenter);
+        flex_box_set_spacing(data->front_flex, 2);
+
+        data->front_icon = image_alloc(flex_box_get_base(data->front_flex));
+        image_set_source(data->front_icon, info.path.icon.front);
+
+        data->front_label = label_alloc(flex_box_get_base(data->front_flex));
+        label_set_text(data->front_label, info.manifest.name);
+
         data->front_menu = anim_menu_alloc(instance->front_window);
         widget_set_align(anim_menu_get_base(data->front_menu), AlignRightMid);
         anim_menu_set_source(
@@ -56,6 +77,7 @@ static void js_app_launcher_scene_start_on_exit(void* context) {
         scene_manager_get_scene_data(instance->scene_manager, JsAppLauncherSceneIdStart);
 
     with_gui(instance->gui, {
+        flex_box_free(data->front_flex);
         anim_menu_free(data->front_menu);
         menu_free(data->back_menu);
     });
