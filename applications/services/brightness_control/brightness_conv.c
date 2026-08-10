2503854ea0a7148662367b6acee6c01983c691a6
Georgii Surkov <georgii.surkov@outlook.com>
2026-08-10 18:51:29 +0100

    BrightnessControl: use float as internal value

--- applications/services/brightness_control/brightness_conv.c
+++ applications/services/brightness_control/brightness_conv.c
@@ -4,7 +4,7 @@
 
 #include <core/core_defines.h>
 
-#define INTERNAL_BRIGHTNESS_MAX UINT8_MAX
+#define BRIGHTNESS_CONV_POWER_MIN (1E-2f)
 
 typedef struct {
     uint8_t min;
@@ -28,19 +28,18 @@ static const BrightnessCurve back_curve = {
 static const BrightnessCurve lights_curve = {
     .min = 5,
     .max = 90,
+    .power = 0.f,
 };
 #endif
 
 static uint8_t brightness_conv_map_curve(const BrightnessCurve* curve, InternalBrightness v) {
-    const float val_norm = (float)v.val / INTERNAL_BRIGHTNESS_MAX;
-
     float val_exp;
     const float power = curve->power;
 
-    if(fabsf(power) < 1e-2f) {
-        val_exp = val_norm;
+    if(fabsf(power) < BRIGHTNESS_CONV_POWER_MIN) {
+        val_exp = v.val;
     } else {
-        val_exp = (expf(power * val_norm) - 1.f) / (expf(power) - 1.f);
+        val_exp = (expf(power * v.val) - 1.f) / (expf(power) - 1.f);
     }
 
     return roundf(curve->min + (curve->max - curve->min) * val_exp);
@@ -52,16 +51,15 @@ UserBrightness brightness_conv_int_to_user_clamped(int brightness) {
 }
 
 InternalBrightness brightness_conv_user_to_internal(UserBrightness v) {
-    return (InternalBrightness){roundf(((float)INTERNAL_BRIGHTNESS_MAX / BRIGHTNESS_MAX) * v.val)};
+    return (InternalBrightness){(float)v.val / BRIGHTNESS_MAX};
 }
 
 UserBrightness brightness_conv_internal_to_user(InternalBrightness v) {
-    return (UserBrightness){roundf(v.val / ((float)INTERNAL_BRIGHTNESS_MAX / BRIGHTNESS_MAX))};
+    return (UserBrightness){roundf(v.val * BRIGHTNESS_MAX)};
 }
 
 InternalBrightness brightness_conv_light_sensor_to_internal(LightSensorLevel v) {
-    return (InternalBrightness){
-        roundf(v.val * ((float)INTERNAL_BRIGHTNESS_MAX / LIGHT_SENSOR_LIGHT_LEVEL_MAX))};
+    return (InternalBrightness){(float)v.val / LIGHT_SENSOR_LIGHT_LEVEL_MAX};
 }
 
 FrontDisplayBrightness brightness_conv_internal_to_front(InternalBrightness v) {
