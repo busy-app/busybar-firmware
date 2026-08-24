a4590da2d18b485731e939c0249c09734d74bd62
Nikita Zinin <zininnikita309@gmail.com>
2026-08-24 14:01:11 +0400

    fix: use location.origin if barUrl is empty

--- assets/frontend/components/Tab/Network/CardHttpApi.vue
+++ assets/frontend/components/Tab/Network/CardHttpApi.vue
@@ -10,10 +10,10 @@
       <UButton
         variant="link"
         class="p-0 gap-0.5"
-        :href="`${runtimeConfig.public.barUrl || 'http://10.0.4.20'}/docs`"
+        :href="`${barUrl}/docs`"
         target="_blank"
       >
-        <span class="underline">{{ runtimeConfig.public.barUrl || 'http://10.0.4.20' }}/docs</span>
+        <span class="underline">{{ barUrl }}/docs</span>
         <UIcon
           name="i-bi-open-in-new"
           class="size-4"
@@ -201,6 +201,8 @@ const httpApiSwitchModel = ref(false);
 const showEnableHttpApiModal = ref(false);
 
 const connected = computed(() => wifiStore.wifi?.state === 'connected');
+
+const barUrl = computed(() => runtimeConfig.public.barUrl || window.location.origin);
 
 watch(() => deviceStore.httpAPIAccess, access => {
   if (!access) {
