7f3d0d9ef12d26f3e035a83ce96660469f37c614
Georgii Surkov <georgii.surkov@outlook.com>
2026-09-07 14:44:05 +0100

    MbedTls: enable RSA SSA_PSS support

--- targets/f21/mbedtls/mbedtls_cfg.h
+++ targets/f21/mbedtls/mbedtls_cfg.h
@@ -135,6 +135,7 @@
 //Additional verification options
 #define MBEDTLS_X509_CRL_PARSE_C // For checking certificate revocation lists
 #define MBEDTLS_X509_CSR_PARSE_C // For parsing certificate requests
+#define MBEDTLS_X509_RSASSA_PSS_SUPPORT // Required for some sites
 //Enable SNI (Server Name Indication)
 #define MBEDTLS_SSL_SERVER_NAME_INDICATION
 
