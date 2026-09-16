95f8065c5daaf0ef8e2cbf0861f186296ccb637e
Ivan Mikhailov <imihajlow@gmail.com>
2026-09-16 21:10:52 +0200

    Move JsAppInstaller folder

--- applications/system/js_app_installer/js_app_installer_paths.h
+++ applications/system/js_app_installer/js_app_installer_paths.h
@@ -2,7 +2,7 @@
 
 #include <storage/storage.h>
 
-#define JS_APP_INSTALLER_ROOT_PATH EXT_PATH("js_app_installer")
+#define JS_APP_INSTALLER_ROOT_PATH APP_DATA_PATH("js_app_installer")
 
 #define JS_APP_DOWNLOAD_PATH JS_APP_INSTALLER_ROOT_PATH "/app.tgz"
 #define JS_APP_STAGING_PATH  JS_APP_INSTALLER_ROOT_PATH "/staging"
