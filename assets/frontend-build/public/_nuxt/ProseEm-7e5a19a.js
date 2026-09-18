a1944c792e6ccb859a3dbbd7d90288ee0149a80c
Nikita Zinin <zininnikita309@gmail.com>
2026-09-18 17:16:19 +0400

    frontend: build

---
+++ assets/frontend-build/public/_nuxt/ProseEm-7e5a19a.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as s,q as t}from"./entry-7e5a19a.js";const c={};function n(e,a){return s(),r("em",null,[t(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
