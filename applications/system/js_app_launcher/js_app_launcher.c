c6a5e8c0cd17c87229684dbdb795661b1eebd2d1
Georgii Surkov <georgii.surkov@outlook.com>
2026-09-21 18:49:03 +0100

    JsAppLauncher: refactor code

--- applications/system/js_app_launcher/js_app_launcher.c
+++ applications/system/js_app_launcher/js_app_launcher.c
@@ -85,23 +85,81 @@ static void js_app_launcher_set_navbar_text(const JsAppLauncher* instance) {
 }
 
 static void js_app_launcher_init_app(JsAppLauncher* instance, const char* app_id) {
-    const size_t app_id_len = strlen(app_id);
-    if((app_id_len == 0) ||
-       (app_id_len > (JS_APP_ID_LEN_MAX + strlen(JS_APP_LAUNCHER_FLAG_SKIP_MENU)))) {
-        return;
+    JsAppLauncherMode mode = JsAppLauncherModeNormal;
+
+    do {
+        const size_t app_id_len = strlen(app_id);
+        if((app_id_len == 0) ||
+           (app_id_len > (JS_APP_ID_LEN_MAX + strlen(JS_APP_LAUNCHER_FLAG_SKIP_MENU)))) {
+            break;
+        }
+
+        char app_id_tmp[app_id_len + 1];
+        strcpy(app_id_tmp, app_id);
+
+        const size_t flag_idx = app_id_len - strlen(JS_APP_LAUNCHER_FLAG_SKIP_MENU);
+
+        if(strcmp(&app_id_tmp[flag_idx], JS_APP_LAUNCHER_FLAG_SKIP_MENU) == 0) {
+            app_id_tmp[flag_idx] = '\0';
+            mode = JsAppLauncherModeSkipMenu;
+        }
+
+        instance->js_app = js_app_registry_get_app(app_id_tmp);
+
+    } while(false);
+
+    instance->mode = mode;
+}
+
+static void js_app_launcher_init_settings_storage(JsAppLauncher* instance) {
+    JsAppLauncherError error;
+
+    do {
+        if(instance->js_app == NULL) {
+            error = JsAppLauncherErrorLoadFailed;
+            break;
+        }
+
+        JsAppInfo app_info;
+        if(!js_app_get_info(instance->js_app, &app_info)) {
+            error = JsAppLauncherErrorLoadFailed;
+            break;
+        }
+
+        JsAppSettingsStorageStatus status;
+        instance->settings_storage = js_app_settings_storage_alloc(app_info.manifest.id, &status);
+
+        if((instance->settings_storage == NULL) &&
+           (status != JsAppSettingsStorageStatusSchemaMissing)) {
+            error = js_app_launcher_settings_storage_error_map[status];
+            break;
+        }
+
+        error = JsAppLauncherErrorNone;
+    } while(false);
+
+    instance->error = error;
+}
+
+static void js_app_launcher_go_to_next_scene(const JsAppLauncher* instance) {
+    uint32_t scene_ids[2];
+    size_t scene_ids_count;
+
+    if(instance->error == JsAppLauncherErrorNone) {
+        scene_ids[0] = JsAppLauncherSceneIdStart;
+        scene_ids_count = 1;
+
+        if(instance->mode == JsAppLauncherModeSkipMenu) {
+            scene_ids[1] = JsAppLauncherSceneIdRun;
+            scene_ids_count = 2;
+        }
+
+    } else {
+        scene_ids[0] = JsAppLauncherSceneIdError;
+        scene_ids_count = 1;
     }
 
-    char app_id_tmp[app_id_len + 1];
-    strcpy(app_id_tmp, app_id);
-
-    const size_t flag_idx = app_id_len - strlen(JS_APP_LAUNCHER_FLAG_SKIP_MENU);
-
-    if(strcmp(&app_id_tmp[flag_idx], JS_APP_LAUNCHER_FLAG_SKIP_MENU) == 0) {
-        app_id_tmp[flag_idx] = '\0';
-        instance->mode = JsAppLauncherModeSkipMenu;
-    }
-
-    instance->js_app = js_app_registry_get_app(app_id_tmp);
+    scene_manager_next_scenes(instance->scene_manager, scene_ids, scene_ids_count);
 }
 
 static JsAppLauncher* js_app_launcher_alloc(const char* app_id) {
@@ -115,6 +173,7 @@ static JsAppLauncher* js_app_launcher_alloc(const char* app_id) {
     instance->gui = furi_record_open(RECORD_GUI);
 
     js_app_launcher_init_app(instance, app_id);
+    js_app_launcher_init_settings_storage(instance);
 
     with_gui(instance->gui, {
         GuiLayer* layer = gui_get_layer(instance->gui, GuiLayerIdMain);
@@ -153,28 +212,7 @@ static JsAppLauncher* js_app_launcher_alloc(const char* app_id) {
         js_app_launcher_event_queue_callback,
         instance);
 
-    uint32_t scene_ids[2] = {JsAppLauncherSceneIdError, JsAppLauncherSceneIdMax};
-    size_t scene_ids_count = 1;
-
-    if(instance->js_app) {
-        JsAppSettingsStorageStatus status;
-        instance->settings_storage = js_app_settings_storage_alloc(app_id, &status);
-
-        if(instance->settings_storage || status == JsAppSettingsStorageStatusSchemaMissing) {
-            scene_ids[0] = JsAppLauncherSceneIdStart;
-            if(instance->mode == JsAppLauncherModeSkipMenu) {
-                scene_ids[1] = JsAppLauncherSceneIdRun;
-                scene_ids_count = 2;
-            }
-        } else {
-            instance->error = js_app_launcher_settings_storage_error_map[status];
-        }
-    } else {
-        instance->settings_storage = NULL;
-        instance->error = JsAppLauncherErrorLoadFailed;
-    }
-
-    scene_manager_next_scenes(instance->scene_manager, scene_ids, scene_ids_count);
+    js_app_launcher_go_to_next_scene(instance);
 
     return instance;
 }
